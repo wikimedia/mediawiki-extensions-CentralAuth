81bd1387c5d452742fb83d03963d1524feb0b0d6
Brion Vibber <brion@users.mediawiki.org>
2006-11-10 16:00:31 +0000

    prelim ui strings

---
+++ CentralAuth.i18n.php
@@ -0,0 +1,60 @@
+<?php
+
+$wgCentralAuthMessages = array();
+$wgCentralAuthMessages['en'] = array(
+	// Big text on completion
+	'centralauth-complete' =>
+		'Login unification complete!',
+	'centralauth-incomplete' =>
+		'Login unification not complete!',
+	
+	// Wheeee
+	'centralauth-complete-text' =>
+		'You can now log in to any Wikimedia wiki site without creating ' .
+		'a new account; the same username and password will work everywhere.',
+	'centralauth-incomplete-text' =>
+		'Once your login is unified, you will be able to log in ' .
+		'to any Wikimedia wiki site without creating a new account; ' .
+		'the same username and password will work everywhere.',
+	'centralauth-not-owner-text' =>
+		'The username "$1" was automatically assigned to the owner ' .
+		"of the account on $2.\n" .
+		"\n" .
+		"If this is you, you can finish the login unification process " .
+		"simply by typing the master password for that account here:",
+	
+	// Appended to various messages above
+	'centralauth-readmore-text' =>
+		'(Read more about [[meta:Help:Unified login|unified login]]...)',
+	
+	// For lists of wikis/accounts:
+	'centralauth-list-merged' =>
+		'The accounts named "$1" on the following Wikimedia sites ' .
+		'have been automatically merged:',
+	'centralauth-list-unmerged' =>
+		'Some accounts could not be automatically confirmed ' .
+		'as belonging to you; most likely they have a different ' .
+		'password from your primary account.',
+	
+	// When not complete, offer to finish...
+	'centralauth-finish-text' =>
+		'If these accounts do belong to you, you can finish ' .
+		'the login unification process simply by typing the passwords ' .
+		'for the other accounts here:',
+	'centralauth-finish-password' =>
+		'Password:',
+	'centralauth-finish-login' =>
+		'Login',
+	'centralauth-finish-forgot' =>
+		'Forgot the password?',
+	'centralauth-finish-send-confirmation' =>
+		'Send a confirmation mail',
+	'centralauth-finish-notmine' =>
+		'Not your accounts? [[How to deal with this bla bla]]',
+
+);
+
+
+
+
+?>
