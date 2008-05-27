f67364e6f0e30f8b71ef4eda3544cddab9918825
Andrew Garrett <werdna@users.mediawiki.org>
2008-05-27 08:20:38 +0000

    Revert r35383. Oops.

--- CentralAuthHooks.php
+++ CentralAuthHooks.php
@@ -78,7 +78,6 @@ class CentralAuthHooks {
 	
 	static function onAbortNewAccount( $user, &$abortError ) {
 		$centralUser = CentralAuthUser::getInstance( $user );
-		die( print_r( $centralUser->exists(), true ) );
 		if ( $centralUser->exists() ) {
 			wfLoadExtensionMessages('SpecialCentralAuth');
 			$abortError = wfMsg( 'centralauth-account-exists' );
