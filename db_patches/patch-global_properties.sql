f39b35ec137092ba1a6c8237feb54ba01df38a1e
Andrew Garrett <werdna@users.mediawiki.org>
2009-04-27 05:35:10 +0000

    Remove (11) after int(11) -- useless

--- db_patches/patch-global_properties.sql
+++ db_patches/patch-global_properties.sql
@@ -1,6 +1,6 @@
 -- Allow users to share preferences across wikis
 CREATE TABLE global_user_properties (
-	gp_user int(11) NOT NULL,
+	gp_user int NOT NULL,
 	gp_property varbinary(255) NOT NULL,
 	gp_value BLOB,
 	
