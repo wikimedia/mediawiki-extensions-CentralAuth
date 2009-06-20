6ab68c109d56bb9c6e16729dd14442e2be21096a
Andrew Garrett <werdna@users.mediawiki.org>
2009-06-20 10:43:04 +0000

    (bug 19158) For sanity, check that the session matches the User when automatically logging users in. Avoids session collisions causing users to be randomly logged-in as another user.

--- CentralAuthHooks.php
+++ CentralAuthHooks.php
@@ -182,6 +182,17 @@ class CentralAuthHooks {
 			wfDebug( __METHOD__.": no token or session\n" );
 			return true;
 		}
+		
+		// Sanity check to avoid session ID collisions, as reported on bug 19158
+		if ( !isset($_COOKIE["{$prefix}User"]) ) {
+			wfDebug( __METHOD__.": no User cookie, so unable to check for session mismatch\n" );
+			return;
+		} elseif ( $_COOKIE["{$prefix}User"] != $userName ) {
+			wfDebug( __METHOD__.": Session ID/User mismatch. Possible session collision. ".
+					"Expected: $userName; actual: ".
+					$_COOKIE["{$prefix}User"]."\n" );
+			return;
+		}
 
 		// Clean up username
 		$title = Title::makeTitleSafe( NS_USER, $userName );
