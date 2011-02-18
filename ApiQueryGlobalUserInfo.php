0292e41dffdfe31693792c9764b5f482d6bf9b77
Sam Reed <reedy@users.mediawiki.org>
2011-02-18 18:54:21 +0000

    * (bug 25508) Poor API behavior when the SUL account does not exist
    Original patch by Brad Jorsch, modified to fit/work. Minor addition
    
    Rough idea of patch tested on fenari, and when global user didn't exist, list of unattached accounts was still available (aim of the patch)

--- ApiQueryGlobalUserInfo.php
+++ ApiQueryGlobalUserInfo.php
@@ -42,37 +42,40 @@ class ApiQueryGlobalUserInfo extends ApiQueryBase {
 			$params['user'] = $wgUser->getName();
 		}
 		$user = new CentralAuthUser( $params['user'] );
-		if ( !$user->exists() ) {
-			$this->dieUsageMsg( array( 'nosuchuser', $params['user'] ) );
-		}
 
 		// Add basic info
 		$result = $this->getResult();
-		$data = array(
-			'home' => $user->getHomeWiki(),
-			'id' => $user->getId(),
-			'registration' => wfTimestamp( TS_ISO_8601, $user->getRegistration() )
-		);
-		if ( $user->isLocked() ) {
-			$data['locked'] = '';
-		}
-		if ( $user->isHidden() ) {
-			$data['hidden'] = '';
+		$data = array();
+		$userExists = $user->exists();
+
+		if ( $userExists ) {
+			$data['home'] = $user->getHomeWiki();
+			$data['id'] = $user->getId();
+			$data['registration'] = wfTimestamp( TS_ISO_8601, $user->getRegistration() );
+
+			if ( $user->isLocked() ) {
+				$data['locked'] = '';
+			}
+			if ( $user->isHidden() ) {
+				$data['hidden'] = '';
+			}
+		} else {
+			$data['missing'] = '';
 		}
 		$result->addValue( 'query', $this->getModuleName(), $data );
 
 		// Add requested info
-		if ( isset( $prop['groups'] ) ) {
+		if ( $userExists && isset( $prop['groups'] ) ) {
 			$groups = $user->getGlobalGroups();
 			$result->setIndexedTagName( $groups, 'g' );
 			$result->addValue( array( 'query', $this->getModuleName() ), 'groups', $groups );
 		}
-		if ( isset( $prop['rights'] ) ) {
+		if ( $userExists && isset( $prop['rights'] ) ) {
 			$rights = $user->getGlobalRights();
 			$result->setIndexedTagName( $rights, 'r' );
 			$result->addValue( array( 'query', $this->getModuleName() ), 'rights', $rights );
 		}
-		if ( isset( $prop['merged'] ) ) {
+		if ( $userExists && isset( $prop['merged'] ) ) {
 			$accounts = $user->queryAttached();
 			foreach ( $accounts as $account ) {
 				$a = array(
@@ -150,12 +153,6 @@ class ApiQueryGlobalUserInfo extends ApiQueryBase {
 
 	public function getDescription() {
 		return 'Show information about a global user.';
-	}
-
-	public function getPossibleErrors() {
-		return array_merge( parent::getPossibleErrors(), array(
-			array( 'nosuchuser', 'user' ),
-		) );
 	}
 
 	protected function getExamples() {
