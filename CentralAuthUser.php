8a4d3d4de5da3313da06864741c40a95dcfc77b5
Sam Reed <reedy@users.mediawiki.org>
2011-11-19 13:36:45 +0000

    4 PHP Warning:  array_merge() [<a href='function.array-merge'>function.array-merge</a>]: Argument #2 is not an array in /usr/local/apache/common-local/php-1.18/extensions/CentralAuth/CentralAuthUser.php on line 1518
    In trunk at 1578

--- CentralAuthUser.php
+++ CentralAuthUser.php
@@ -1573,10 +1573,15 @@ class CentralAuthUser extends AuthPluginUser {
 				'attachedMethod' => $row->lu_attached_method,
 			);
 
+			$localUser = $this->localUserData( $row->lu_wiki );
+
+			if ( $localUser === false ) {
+				continue;
+			}
 			// Just for fun, add local user data.
 			// Displayed in the steward interface.
 			$wikis[$row->lu_wiki] = array_merge( $wikis[$row->lu_wiki],
-												$this->localUserData( $row->lu_wiki ) );
+				$localUser );
 		}
 
 		return $wikis;
@@ -1608,7 +1613,7 @@ class CentralAuthUser extends AuthPluginUser {
 	 * Fetch a row of user data needed for migration.
 	 *
 	 * @param $wikiID String
-	 * @return Array
+	 * @return Array|bool
 	 */
 	protected function localUserData( $wikiID ) {
 		$lb = wfGetLB( $wikiID );
