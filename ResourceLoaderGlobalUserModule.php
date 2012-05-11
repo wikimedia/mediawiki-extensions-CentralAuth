e00a624925d0be3169741ebd1b9a78dd07c1c310
Szymon Świerkosz <beau@adres.pl>
2012-05-11 10:09:42 +0200

    (bug 13953) Allow global user CSS/JS.
    This change creates a new user module, which is loaded when
    - user has account attached on a local wiki
    - user has account attached on a global wiki
    
    It uses RL ability to load modules from non-local sources.
    
    NOTE! When enabling this feature make sure the global wiki
    has this feature enabled first.
    
    Change-Id: Ie0358ea13a4dda1cc13714af4683bd6e396b8e4e

---
+++ ResourceLoaderGlobalUserModule.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Resource loader module for global user customizations.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @author Szymon Świerkosz
+ */
+
+/**
+ * Module for user customizations - runs on central wiki.
+ */
+class ResourceLoaderGlobalUserModule extends ResourceLoaderWikiModule {
+
+	/* Protected Methods */
+	protected $origin = self::ORIGIN_USER_INDIVIDUAL;
+
+	/**
+	 * @param $context ResourceLoaderContext
+	 * @return array
+	 */
+	protected function getPages( ResourceLoaderContext $context ) {
+		$username = $context->getUser();
+
+		if ( $username === null ) {
+			return array();
+		}
+
+		// Get the normalized title of the user's user page
+		$userpageTitle = Title::makeTitleSafe( NS_USER, $username );
+
+		if ( !$userpageTitle instanceof Title ) {
+			return array();
+		}
+
+		$userpage = $userpageTitle->getPrefixedDBkey();
+
+		$pages = array(
+			"$userpage/global.js" => array( 'type' => 'script' ),
+			"$userpage/global.css" => array( 'type' => 'style' ),
+		);
+
+		return $pages;
+	}
+
+	/* Methods */
+
+	/**
+	 * @return string
+	 */
+	public function getGroup() {
+		return 'user';
+	}
+}
