9b15d4e57c815dab4009a05f06d185754c2dc75a
Krenair <krenair@gmail.com>
2012-12-16 02:24:43 +0000

    (bug 20850) Add global_user_groups variable to AbuseFilter
    Relies on I647a5296
    
    Change-Id: I79a625e3c3fac659ae42673a7b8b0a9fb04502ad

--- CentralAuthHooks.php
+++ CentralAuthHooks.php
@@ -815,4 +815,46 @@ class CentralAuthHooks {
 		}
 		return true;
 	}
+
+	/**
+	 * Computes the global_user_groups variable
+	 * @param string $method
+	 * @param AbuseFilterVariableHolder $vars
+	 * @param array $parameters
+	 * @param null &$result
+	 */
+	static function abuseFilterComputeVariable( $method, $vars, $parameters, &$result ) {
+		if ( $method == 'global-user-groups' ) {
+			$user = new CentralAuthUser( $parameters['userName'] );
+			if ( $user->exists() && $user->isAttached() ) {
+				$result = $user->getGlobalGroups();
+			} else {
+				$result = array();
+			}
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * Load our global_user_groups variable
+	 * @param AbuseFilterVariableHolder $vars
+	 * @param Title $title
+	 */
+	static function abuseFilterFilterAction( $vars, $title ) {
+		$vars->setLazyLoadVar( 'global_user_groups', 'global-user-groups', array(
+			'userName' => $vars->getVar( 'user_name' )->toString()
+		) );
+		return true;
+	}
+
+	/**
+	 * Tell AbuseFilter about our global_user_groups variable
+	 * @param array &$builderValues
+	 */
+	static function abuseFilterBuilder( &$builderValues ) {
+		$builderValues['vars']['global_user_groups'] = 'global-user-groups';
+		return true;
+	}
 }
