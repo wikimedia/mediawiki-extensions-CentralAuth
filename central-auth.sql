7e11e480d14ee0a3ca94d13f3a362fde7de77e35
techman224 <dev@wiki.techman224.ca>
2012-12-16 16:40:37 -0600

    Adds the spoofuser table to the centralauth database. If it is not added, you have problems creating users if the wiki also has the AntiSpoof extension.
    Change-Id: I3511c3ec7843ee0219a599c99bfefe234afb3649

--- central-auth.sql
+++ central-auth.sql
@@ -167,3 +167,27 @@ CREATE TABLE global_group_restrictions (
   PRIMARY KEY (ggr_group),
   KEY (ggr_set)
 ) /*$wgDBTableOptions*/;
+
+-- AntiSpoof (for making the AntiSpoof extension work on the global users)
+-- Copied from AntiSpoof's patch-antispoof.mysql.sql
+CREATE TABLE spoofuser (
+  -- Equivalent to user_name, but not guaranteed to be in sync.
+  -- Do a join against user_name to confirm that an account hasn't
+  -- been renamed or deleted away.
+  su_name VARCHAR(255),
+
+  -- Normalized form of name for similarity-spoofing checks
+  su_normalized VARCHAR(255),
+
+  -- ok/not-ok according to the looks-like-a-valid-name check
+  su_legal BOOL,
+
+  -- error message that came out of the unicode check, if any
+  su_error TEXT,
+
+  -- unique record per username
+  PRIMARY KEY (su_name),
+
+  -- for checking matching possible spoofs
+  KEY(su_normalized, su_name)
+  ) /*$wgDBTableOptions*/;
