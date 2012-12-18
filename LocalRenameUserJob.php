585a43de4a5c073de95eebc42b071cbcbe995970
Krenair <krenair@gmail.com>
2012-12-18 01:10:24 +0000

    (bug 14862) WIP - Global user renaming
    Change-Id: I6f8584f28e6f4ea6e5664c925cc464dd4674899f

---
+++ LocalRenameUserJob.php
@@ -0,0 +1,80 @@
+<?php
+// This is inserted into the job queue of each wiki which needs renaming done, and actually just calls RenameuserSQL::rename() which may start a job of it's own...
+class CentralAuthLocalRenameUserJob extends Job {
+	public function __construct( $title, $params, $id = 0 ) {
+		parent::__construct( 'startLocalRenaming', $title, $params, $id );
+	}
+
+	public function run() {
+		global $wgMemc, $wgDBname;
+		AutoLoader::autoload( 'SpecialRenameuser' );
+
+		$from = $this->params['from'];
+		$to = $this->params['to'];
+		$rus = new RenameuserSQL(
+			$from,
+			$to,
+			User::newFromName( $from )->getId(),
+			array( 'checkIfUserExists' => false ) /* We do this update in SpecialCentralAuth::doSubmit, don't want RenameUser to fail when it notices this */
+		);
+
+		if ( $rus->rename() ) {
+			// TODO: For renames of more than RENAMEUSER_CONTRIBJOB (default: 5000), this might need to take place in CentralAuthHooks::onRenameUserComplete
+			$wikiListKey = CentralAuthUser::memcKey( 'globalrename', $from );
+			$wikiList = $wgMemc->get( $wikiListKey ); // TODO: Find a way to completely disable global renaming if memcache isn't available on all wikis. We can't just quit here if we see that there's no memcached - other wikis will be waiting for us (with no way for us to cancel) and the next bit of code will never get run.
+			$wikiList = array_diff( $wikiList, array( $wgDBname ) ); // Remove this wiki from the list.
+			if ( count( $wikiList ) == 0 ) {
+				$wgMemc->delete( $wikiListKey );
+
+				// Unlock the user
+				if ( !$this->params['startedLocked'] ) { // If the user was locked before we started renaming, we need to make sure we don't unlock them silently at the end.
+					$globalUser = new CentralAuthUser( $to );
+					$globalUser->adminUnlock();
+				}
+
+				// Make log entry on the wiki which this rename job was started on.
+				$db = wfGetLB( $this->params['startedFrom'] )->getConnection( DB_MASTER, array(), $this->params['startedFrom'] );
+				$title = Title::newFromText( "User:{$from}@global" );
+				$db->insert(
+					'logging',
+					array(
+						'log_type' => 'renameuser',
+						'log_action' => 'globalrenameuser',
+						'log_timestamp' => $db->timestamp( wfTimestampNow() ),
+						'log_user' => $this->params['startedById'],
+						'log_user_text' => $this->params['startedByName'],
+						'log_namespace' => $title->getNamespace(),
+						'log_title' => $title->getDBkey(),
+						'log_page' => $title->getArticleID(),
+						'log_comment' => $this->params['reason'],
+						'log_params' => $to
+					),
+					__METHOD__
+				);
+				$db->insert(
+					'recentchanges',
+					array(
+						'rc_timestamp' => $db->timestamp( wfTimestampNow() ),
+						'rc_cur_time' => $db->timestamp( wfTimestampNow() ),
+						'rc_user' => $this->params['startedById'],
+						'rc_user_text' => $this->params['startedByName'],
+						'rc_namespace' => $title->getNamespace(),
+						'rc_title' => $title->getDBkey(),
+						'rc_comment' => $this->params['reason'],
+						'rc_type' => RC_LOG,
+						'rc_ip' => $this->params['startedByIP'],
+						'rc_logid' => $db->insertId(),
+						'rc_log_type' => 'renameuser',
+						'rc_log_action' => 'globalrenameuser',
+						'rc_params' => $to
+					),
+					__METHOD__
+				);
+				CentralAuthUser::getCentralDB()->unlock( "centralauth-globalrename:" . $this->params['to'], __METHOD__ );
+			} else {
+				$wgMemc->set( $wikiListKey, $wikiList );
+			}
+			return true;
+		}
+	}
+}
