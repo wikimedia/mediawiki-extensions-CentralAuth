66979fe8fabc6fde6500cafa0635d7da71b014a6
Brad Jorsch <bjorsch@wikimedia.org>
2013-04-10 13:07:46 -0400

    Add JavaScript login check against the central wiki
    The ability for CentralAuth to log the user in everywhere via a "push"
    model is going away, as browsers tighten their security on third-party
    cookies.
    
    This changes things to a "pull" model instead. If the user is logged
    out, a request is sent to the central wiki, which puts the necessary
    data in memcache so we can set the necessary cookies as first-party
    instead. To avoid doing this with every page view, it is instead done
    just once and a cookie or localStorage token is set to remember this.
    
    Note that, to allow Internet Explorer to be able to send and receive the
    cookies via CORS or the iframe, a P3P header must be included when the
    cookies are set on the central domain. The default policy used is
    clearly invalid: it has no valid tokens at all, while a real policy must
    contain at least an "access" token and one statement, which would
    contain either the NID token or at least one "purpose" token, one
    "recipient" token, and one "retention" token.
    
    Change-Id: Ib6e9cce4fa4c5f1482c59bfac28087f558786efe

---
+++ modules/ext.centralauth.centralautologin.js
@@ -0,0 +1,169 @@
+( function ( mw, $ ) {
+	// Are we already logged in?
+	if ( mw.config.get( 'wgUserName' ) !== null ) {
+		return;
+	}
+
+	// Do we already know we're logged out centrally?
+	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) !== 'Userlogin' ) {
+		if ( 'localStorage' in window && +localStorage.getItem( 'CentralAuthAnon' ) > new Date().getTime() ) {
+			return;
+		}
+
+		// Can't use $.cookie(), because we want to check this at the top of
+		// the page and that isn't loaded until the bottom.
+		if ( /(^|; )CentralAuthAnon=1/.test( document.cookie ) ) {
+			return;
+		}
+	}
+
+	var centralEndpoint = mw.config.get( 'wgCentralAuthCentralAutoLoginEndpoint' ),
+		localEndpoint = mw.config.get( 'wgArticlePath' ).replace( '$1', 'Special:CentralAutoLogin/$1' ),
+		wikiId = mw.config.get( 'wgCentralAuthWikiID' );
+
+	function getReturnToParams() {
+		var params = location.search.slice( 1 ).split( '&' ),
+			len = params.length,
+			param, i,
+			ret = [];
+		for ( i = 0; i < len; i++ ) {
+			param = params[i].split( '=' );
+			param = decodeURIComponent( param[0] );
+			if ( param === 'returnto' || param === 'returntoquery' ) {
+				ret.push( params[i] );
+			}
+		}
+		return ret.join( '&' );
+	}
+
+	// These functions should be declared in the "if ( jQuery.support.cors )"
+	// where they're actually needed, but jshint insists they be placed up here
+	// instead.
+	// Documentation: [[mw:Auth systems/SUL2#Local wiki Javascript Auth check]]
+
+	// State C1
+	function fetchCentralUserId() {
+		// Also, disable this brain-damaged warning. We're not going to rewrite
+		// all the PHP code because it doesn't like "ret.gu_id".
+		/*jshint camelcase: false */
+		$.ajax( {
+			url: centralEndpoint.replace( '$1', 'C1' ),
+			dataType: 'json',
+			type: 'POST',
+			data: { wikiid: wikiId },
+			xhrFields: {
+				withCredentials: true
+			}
+		} ).done( function ( ret ) {
+			/*jshint camelcase: false */
+			if ( ret.status === 'ok' ) {
+				if ( +ret.gu_id <= 0 ) {
+					var t = new Date();
+					t.setTime( t.getTime() + 86400000 );
+					if ( 'localStorage' in window ) {
+						localStorage.setItem( 'CentralAuthAnon', t.getTime() );
+					} else {
+						document.cookie = 'CentralAuthAnon=1; expires=' + t.toGMTString() + '; path=/';
+					}
+				} else {
+					if ( 'localStorage' in window ) {
+						localStorage.removeItem( 'CentralAuthAnon' );
+					}
+					if ( /(^|; )CentralAuthAnon=/.test( document.cookie ) ) {
+						document.cookie = 'CentralAuthAnon=0; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
+					}
+					createLocalSessionForCentralLogin( ret.gu_id );
+				}
+			}
+		} );
+	}
+
+	// State L1
+	function createLocalSessionForCentralLogin( guid ) {
+		/*jshint camelcase: false */
+		$.ajax( {
+			url: localEndpoint.replace( '$1', 'L1' ),
+			dataType: 'json',
+			type: 'POST',
+			data: { gu_id: guid }
+		} ).done( function ( ret ) {
+			if ( ret.status === 'ok' ) {
+				haveCentralWikiPutLoginDataInMemcached( ret.token );
+			}
+		} );
+	}
+
+	// State C2
+	function haveCentralWikiPutLoginDataInMemcached( token ) {
+		$.ajax( {
+			url: centralEndpoint.replace( '$1', 'C2' ),
+			dataType: 'json',
+			type: 'POST',
+			data: { token: token, wikiid: wikiId },
+			xhrFields: {
+				withCredentials: true
+			}
+		} ).done( function ( ret ) {
+			if ( ret.status === 'ok' ) {
+				checkMemcachedDataAndSetCentralAuthCookies();
+			}
+		} );
+	}
+
+	// State L2
+	function checkMemcachedDataAndSetCentralAuthCookies() {
+		$.ajax( {
+			url: localEndpoint.replace( '$1', 'L2' ),
+			dataType: 'json',
+			type: 'POST',
+			data: {}
+		} ).done( function ( ret ) {
+			if ( ret.status === 'ok' ) {
+				if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Userlogin' ) {
+					var url = mw.config.get( 'wgArticlePath' ).replace( '$1', 'Special:CentralLogin/Status' );
+					url += ( url.indexOf( '?' ) < 0 ? '?' : '&' ) + getReturnToParams();
+					location.href = url;
+				} else {
+					mw.notify(
+						mw.message( 'centralauth-centralautologin-logged-in',
+							ret.userName, ret.userGender
+						), {
+							title: mw.message( 'centralautologin' ).plain(),
+							autoHide: false,
+							tag: 'CentralAutoLogin'
+						}
+					);
+				}
+			}
+		} );
+	}
+
+	if ( jQuery.support.cors ) {
+		// We can do AJAX calls using CORS.
+
+		// Start by calling state C1
+		fetchCentralUserId();
+	} else {
+		// We have to do it with an iframe. State "L0" returns the first
+		// auto-posting form for us, and the protocol proceeds from there
+		// without our intervention.
+		$( function () {
+			var url = localEndpoint.replace( '$1', 'L0' );
+			if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Userlogin' ) {
+				url += ( url.indexOf( '?' ) < 0 ? '?' : '&' ) + 'oncomplete=status';
+				url += '&' + getReturnToParams();
+			}
+			$( '<iframe>' )
+				.css( {
+					position: 'absolute',
+					top: 0,
+					left: '-10px',
+					width: '1px',
+					height: '1px'
+				} )
+				.attr( 'src', url )
+				.appendTo( document.body );
+		} );
+	}
+
+}( mediaWiki, jQuery ) );
