b44509d03243e41ba0fa44258e253c918e8b9bfe
Marius Hoch <hoo@online.de>
2013-05-09 04:17:22 +0200

    Add .jshintrc for CentralAuth and make it pass
    Change-Id: I4cc36b8d78984b875ee1b28aac69f493ac7c0cc0

--- modules/ext.centralauth.js
+++ modules/ext.centralauth.js
@@ -14,15 +14,6 @@
 			mw.util.$content.append( $methodHint );
 		}
 
-		// Give grep a chance to find the usages:
-		// centralauth-merge-method-primary, centralauth-merge-method-empty,
-		// centralauth-merge-method-mail, centralauth-merge-method-password,
-		// centralauth-merge-method-admin, centralauth-merge-method-new,
-		// centralauth-merge-method-login,
-		// centralauth-merge-method-primary-desc, centralauth-merge-method-empty-desc,
-		// centralauth-merge-method-mail-desc, centralauth-merge-method-password-desc,
-		// centralauth-merge-method-admin-desc, centralauth-merge-method-new-desc,
-		// centralauth-merge-method-login-desc
 		hintHtml = mw.html.element(
 			'p', {
 				'class': 'merge-method-help-name'
@@ -41,20 +32,12 @@
 	}
 
 	$( document ).ready( function () {
-		// Automatic form submission on CentralLogin/start
-		$( '#mw-centralloginform' ).submit();
-		// Back link for CentralLogin/startDW
-		$( '#centralauth-backlink-section' ).append(
-			$( '<a href="javascript:void()">' + mw.msg( 'centralauth-completelogin-back' ) + '</a>' )
-				.click( function() {
-					parent.history.back();
-					return false;
-				})
-		);
+		/* global confirm */
 		// OnClick event listener for the "(?)" tooltips on Special:CentralAuth
 		$( '.mw-centralauth-wikislist' ).on( 'click', '.merge-method-help', function ( event ) {
 			showMethodHint( $(this).data( 'centralauth-mergemethod' ), event );
 		} );
+
 		// Confirm account deletions
 		$( '#mw-centralauth-delete input[type="submit"]' ).on( 'click', function () {
 			return confirm( mw.msg( 'centralauth-admin-delete-confirm' ) );
