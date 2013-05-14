343348596c31dad34efdca132bbaa1dfc6e157ec
Peter Gehres <pgehres@wikimedia.org>
2013-05-14 13:30:41 -0700

    Adding i18n msg for merge request email
    Change-Id: I1d1d2b693b663e4e3219e52d4a325b55c30232c5

--- CentralAuth.i18n.php
+++ CentralAuth.i18n.php
@@ -419,6 +419,26 @@ Someone might be trying to log you in under a malicious account.
 If you are just trying to log in as another user, please log out first.',
 	'centralauth-warning-notloggedin' => 'You are not currently logged in.',
 	'centralauth-warning-notattached' => 'The local account is not attached to a global one.',
+
+	// Final unification
+	'centralauth-finishglobaliseemail_subject' => '{{SITENAME}} account confirmation',
+	'centralauth-finishglobaliseemail_body' => 'Your email address has been set as associated
+with the account "$2" on all Wikimedia projects, with your main account being
+on {{SITENAME}}.
+
+To confirm that this account really does belong to you, merge in any of your
+accounts that we could not do automatically, and allow you to recover your
+account if you lose your password, please open this link in your browser:
+
+$3
+
+If the account does *not* belong to you, follow this link to cancel the email
+address confirmation:
+
+$5
+
+This confirmation code will expire at $4.'
+
 );
 
 /** Message documentation (Message documentation)
@@ -902,6 +922,17 @@ Followed by the Submit button text {{msg-mw|Centralauth-completelogin-submit}}.'
 	'centralauth-completelogin-submit' => 'Used as label for the Submit button.',
 	'centralauth-completelogin-back' => 'Used as action link text.',
 	'centralauth-warning-notloggedin' => 'Used as error message.',
+
+	// Final unification
+	'centralauth-finishglobaliseemail_subject' => 'Subject field for account confirmation emails',
+	'centralauth-finishglobaliseemail_body' => 'Body text for account confirmation emails
+* $1 is the IP address of the user that changed the e-mail address
+* $2 is the name of the user
+* $3 is a URL to Special:ConfirmEmail
+* $4 is a time and date (duplicated by $6 and $7)
+* $5 is a URL to Special:InvalidateEmail
+* $6 is a date
+* $7 is a time',
 );
 
 /** Afrikaans (Afrikaans)
