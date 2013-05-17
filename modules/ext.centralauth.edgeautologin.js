1d4e3fed9ce5bf8cfbc462f240eaaafc71bc8b1c
Brad Jorsch <bjorsch@wikimedia.org>
2013-05-16 22:24:06 -0400

    Make CentralAuth login more silent
    Change the redirect-through-central-domain to a 302 instead of a
    JavaScript post (I'm told Safari 5 users aren't a concern) and make
    the iframes load silently on the pageview after the login rather
    than on a login success screen.
    
    Change-Id: Ibf6e271f4d018be2a53289f2c0092e88bbcc01ee

--- modules/ext.centralauth.edgeautologin.js
+++ modules/ext.centralauth.edgeautologin.js
@@ -1,66 +1,9 @@
 ( function ( mw, $ ) {
-	var max = 0, count = 0, $progressbar, $tick;
-
-	function edgeLoginDone() {
-		$( '#centralauth-edge-login-info' )
-			.text( mw.message( 'centralauth-edge-logged-in', mw.user ) )
-			.css( 'display', 'block' );
-		$tick.css( 'visibility', 'visible' );
-	}
-
-	mw.CentralAuth = {
-		edgeLoginComplete: function ( frame ) {
-			if ( frame ) {
-				$( frame ).trigger( 'ca-done' );
-			}
-			$progressbar.progressbar( 'value', ++count );
-			if ( count >= max ) {
-				edgeLoginDone();
-			}
-		}
-	};
-
 	$( function () {
-		var $urlSpans, $progressbarcontainer;
+		var i, l, urls = mw.config.get( 'wgCentralAuthEdgeAutoLoginEndpoints' );
 
-		$urlSpans = $( '.centralauth-edge-login-url' );
-		if ( $urlSpans.length <= 0 ) {
-			edgeLoginDone();
-			return;
-		}
-
-		max = $urlSpans.length;
-
-		$progressbarcontainer = $( '<div>' )
-			.attr( 'id', 'centralauth-edge-progressbar' );
-
-		$progressbar = $( '<div>' )
-			.appendTo( $progressbarcontainer )
-			.progressbar( { max: max, value: count } );
-		$tick = $( '<img>' )
-			.attr( {
-				src: mw.config.get( 'stylepath' ) + '/common/images/tick-32.png',
-				width: 32,
-				height: 32
-			} )
-			.css( 'visibility', 'hidden' )
-			.appendTo( $progressbarcontainer );
-
-		$( '#centralauth-edge-login-info' )
-			.text( mw.message( 'centralauth-edge-logging-in', mw.user ) )
-			.after( $progressbarcontainer )
-			.css( 'display', 'block' );
-
-		$urlSpans.each( function () {
-			var timer, $iframe;
-
-			function giveUp() {
-				timer = null;
-				mw.CentralAuth.edgeLoginComplete( $iframe, null, [ 'timed-out' ] );
-			}
-			timer = setTimeout( giveUp, 10000 );
-
-			$iframe = $( '<iframe>' )
+		for ( i = 0, l = urls.length; i < l; i++ ) {
+			$( '<iframe>' )
 				.css( {
 					position: 'absolute',
 					top: 0,
@@ -68,21 +11,8 @@
 					width: '1px',
 					height: '1px'
 				} )
-				.on( 'load', function () {
-					// Reset timeout
-					if ( timer ) {
-						clearTimeout( timer );
-						timer = setTimeout( giveUp, 10000 );
-					}
-				} )
-				.on( 'ca-done', function () {
-					if ( timer ) {
-						clearTimeout( timer );
-						timer = null;
-					}
-				} )
-				.attr( 'src', $( this ).data( 'src' ) )
+				.attr( 'src', urls[i] )
 				.appendTo( document.body );
-		} );
+		}
 	} );
 }( mediaWiki, jQuery ) );
