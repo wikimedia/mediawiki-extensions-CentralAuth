07495fcfbc155e8cff685f4769d57e23887f639e
Kunal Mehta <legoktm@gmail.com>
2013-10-28 17:24:16 -0700

    Allow for global renaming of users
    * Adds Special:GlobalRenameUser for trusted users to rename
      global accounts
    * Assumes that the user performing the rename has the ability
      to move whatever pages are necessary on the local wikis,
      including suppressing redirects
    * Uses a new database table to store status and hopefully
      avoid race conditions
    * Added hooks to prevent users from logging in or creating
      a new account if a rename to or from that username is
      in progress
    * If a user is currently being renamed and tries accessing the
      wiki, their session is cleared.
    * Adds Special:RenameProgress for users to check on the
      status of a rename, and which wikis are still pending
    * Visibility of CentralAuthUser::getCacheKey was changed
      to public
    
    Bug: 14862
    Change-Id: Id78577e95d73fbcdcb72e80ef25a34106ac91aa2

--- central-auth.sql
+++ central-auth.sql
@@ -167,3 +167,17 @@ CREATE TABLE global_group_restrictions (
   PRIMARY KEY (ggr_group),
   KEY (ggr_set)
 ) /*$wgDBTableOptions*/;
+
+-- Table for global rename status
+CREATE TABLE renameuser_status (
+  -- Old name being renamed from
+  ru_oldname varchar(255) binary not null,
+  -- New name being renamed to
+  ru_newname varchar(255) binary not null,
+  -- WikiID
+  ru_wiki varchar(255) binary not null,
+  -- current state of the renaming
+  ru_status enum ('queued', 'inprogress', 'failed'),
+
+  KEY (ru_oldname, ru_wiki)
+) /*$wgDBTableOptions*/;
---
+++ db_patches/patch-renameuser.sql
@@ -0,0 +1,12 @@
+CREATE TABLE renameuser_status (
+  -- Old name being renamed from
+  ru_oldname varchar(255) binary not null,
+  -- New name being renamed to
+  ru_newname varchar(255) binary not null,
+  -- WikiID
+  ru_wiki varchar(255) binary not null,
+  -- current state of the renaming
+  ru_status enum ('queued', 'inprogress', 'failed'),
+
+  KEY (ru_oldname, ru_wiki)
+) /*$wgDBTableOptions*/;
