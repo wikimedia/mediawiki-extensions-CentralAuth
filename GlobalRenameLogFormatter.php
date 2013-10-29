0a271bb4bceedbe2fda0f6bae919ec7ce17dbdf8
Kunal Mehta <legoktm@gmail.com>
2013-10-28 17:24:16 -0700

    Allow for global renaming of users
    * Adds Special:GlobalRenameUser for trusted users to rename
      global accounts
    * Assumes that the user performing the rename has the ability
      to move whatever pages are necessary on the local wikis,
      including suppressing redirects
    * Uses a new database table to store status and hopefully
      avoid race conditions
    * Added hooks to prevent users from logging in or creating
      a new account if a rename to or from that username is
      in progress
    * If a user is currently being renamed and tries accessing the
      wiki, their session is cleared.
    * Adds Special:GlobalRenameProgress for users to check on the
      status of a rename, and which wikis are still pending
    
    Bug: 14862
    Change-Id: Id78577e95d73fbcdcb72e80ef25a34106ac91aa2

---
+++ GlobalRenameLogFormatter.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Make the renameuser/global log entry look pretty
+ */
+class GlobalRenameLogFormatter extends LogFormatter {
+	protected function getMessageParameters() {
+		parent::getMessageParameters();
+		$params = $this->extractParameters();
+
+		$this->parsedParameters[3] = $this->getCentralAuthLink( $params[3] );
+		$this->parsedParameters[4] = $this->getCentralAuthLink( $params[4] );
+
+		ksort( $this->parsedParameters );
+		return $this->parsedParameters;
+	}
+
+	/**
+	 * @param string $name
+	 * @return array
+	 */
+	protected function getCentralAuthLink( $name ) {
+		return Message::rawParam( Linker::link(
+				Title::makeTitleSafe( NS_SPECIAL, 'CentralAuth/' . $name ),
+				htmlspecialchars( $name )
+		) );
+	}
+}
