330f81afcde8622d3d233c2fa495002af0bcafc6
Brad Jorsch <bjorsch@wikimedia.org>
2013-12-01 17:49:03 -0500

    Don't localize Special:CentralAutoLogin
    Localization of Special:CentralAutoLogin causes problems for log
    analysis and for dealing with the large number of hits that can come
    into this page (e.g. bug 54195 comment 20). At the same time, it gives
    miniscule benefit to users, as it is never exposed in the public user
    interface.
    
    So let's just remove the localization of it. To really make it work, we
    also need to prevent the redirect from "Special:CentralAutoLogin" to the
    localized version of the "Special" namespace.
    
    Bug: 54195
    Change-Id: I820370de3afa0bdfce360cba1aef651ac084fd06

---
+++ CentralAuth.notranslate-alias.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Aliases for special pages of CentralAuth extension that should not be
+ * translated.
+ *
+ * Do not add this file to translatewiki.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+// @codingStandardsIgnoreFile
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	// Localizing Special:CentralAutoLogin causes issues (bug 54195) and is of
+	// miniscule benefit to users, so don't do so.
+	'CentralAutoLogin' => array( 'CentralAutoLogin' ),
+);
