ed498c2c9ac772ad802a3783ae61035246ecdaae
Timo Tijhof <krinklemail@gmail.com>
2014-03-13 05:33:50 +0100

    SpecialCentralAutoLogin: Move javascript to separate files for linting
    * To make this stuff easier to edit, maintain, use and lint; move
      them to actual javascript files. They'll be automatically
      picked up by the jshint check for this repository from now on.
    
    * Set up logic to serve these minified. Based on ResourceLoader's
      internals (this should be abstracted by MediaWiki core ideally,
      but kept it as simple as possible for now).
    
    * Added a few line breaks to separate the different concatenated
      scripts from each another.
    
    Change-Id: I132ea06f82ad74ba51186e698dbb917e931534a2

---
+++ modules/inline/autologin.js
@@ -0,0 +1,52 @@
+( function ( mw, $ ) {
+	mw.loader.using( 'mediawiki.Uri', function () {
+		var current, login;
+
+		// Set returnto and returntoquery so the logout link in the returned
+		// html is correct.
+		current = new mw.Uri();
+		delete current.query.title;
+		delete current.query.returnto;
+		delete current.query.returntoquery;
+
+		login = new mw.Uri(
+			mw.config.get( 'wgArticlePath' ).replace( '$1', 'Special:CentralAutoLogin/toolslist' )
+		);
+		login.query.returnto = mw.config.get( 'wgPageName' );
+		login.query.returntoquery = current.getQueryString();
+
+		$.getJSON( login.toString() )
+		.done( function ( data ) {
+			if ( data.toolslist ) {
+				$( '#p-personal ul' ).html( data.toolslist );
+				$( '#p-personal' ).addClass( 'centralAuthPPersonalAnimation' );
+				mw.hook( 'centralauth-p-personal-reset' ).fire();
+			} else if ( data.notify ) {
+				mw.notify(
+					mw.message(
+						'centralauth-centralautologin-logged-in',
+						data.notify.username,
+						data.notify.gender
+					),
+					{
+						title: mw.message( 'centralautologin' ),
+						autoHide: false,
+						tag: 'CentralAutoLogin'
+					}
+				);
+			}
+		} )
+		.fail( function () {
+			// This happens if the user is logged in securely,
+			// while also auto-loggedin from an http page.
+			mw.notify(
+				mw.message( 'centralauth-centralautologin-logged-in-nouser' ),
+				{
+					title: mw.message( 'centralautologin' ),
+					autoHide: false,
+					tag: 'CentralAutoLogin'
+				}
+			);
+		} );
+	} );
+}( mediaWiki, jQuery ) );
