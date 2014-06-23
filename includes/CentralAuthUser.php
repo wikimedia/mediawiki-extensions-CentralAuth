95b4eb2787ad1f89cc6f579aca11c78116d494e8
Sam Smith <git@samsmith.io>
2014-06-23 11:54:22 +0100

    Use $wgLoginCookieExpiration when setting login cookies
    Bug: 66699
    Change-Id: Id043ee70d5b45cc71e23c24918acff0ff186b684
    Depends-On: I0cc24524e4d7d9d1d21c9fa8a28c7c76b677b96c

--- includes/CentralAuthUser.php
+++ includes/CentralAuthUser.php
@@ -2316,7 +2316,7 @@ class CentralAuthUser extends AuthPluginUser {
 	 */
 	static function setCookie( $name, $value, $exp = -1, $secure = null, $prefix = false ) {
 		global $wgCentralAuthCookiePrefix, $wgCentralAuthCookieDomain,
-			$wgCookieExpiration, $wgCentralAuthCookiePath;
+			$wgCookieExpiration, $wgCentralAuthCookiePath, $wgLoginCookieExpiration
 
 		if ( CentralAuthHooks::hasApiToken() ) {
 			throw new Exception( "Cannot set cookies when API 'centralauthtoken' parameter is given" );
@@ -2325,7 +2325,13 @@ class CentralAuthUser extends AuthPluginUser {
 		self::setP3P();
 
 		if ( $exp == -1 ) {
-			$exp = time() + $wgCookieExpiration;
+			$exp = time();
+
+			if ( $wgLoginCookieExpiration ) {
+				$exp += $wgLoginCookieExpiration;
+			} else {
+				$exp += $wgCookieExpiration;
+			}
 		} elseif ( $exp == 0 ) {
 			// Session cookie
 			$exp = null;
