7c5926b33e88f23869654316c2dc4d25870e7ae6
Kunal Mehta <legoktm@gmail.com>
2014-07-08 00:06:16 -0700

    Global merge tool
    Depends on I50f63d88 in UserMerge (merged)
    
    Bug: 47918
    Change-Id: Ie76c28234b24e66c708ad0dad03251a7e80ea531

---
+++ GlobalRename/GlobalUserMergeLogFormatter.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Format the gblrename/merge log entries
+ */
+class GlobalUserMergeLogFormatter extends LogFormatter {
+	protected function getMessageParameters() {
+		parent::getMessageParameters();
+		$params = $this->extractParameters();
+		$lang = $this->context->getLanguage();
+
+		$this->parsedParameters[3] = Message::rawParam( $lang->commaList(
+			array_map(
+				array( $this, 'getCentralAuthLink'),
+				explode( '|', $params[3] )
+			)
+		) );
+		$this->parsedParameters[4] = Message::rawParam( $this->getCentralAuthLink( $params[4] ) );
+
+		ksort( $this->parsedParameters );
+		return $this->parsedParameters;
+	}
+
+	/**
+	 * @param string $name
+	 * @return array
+	 */
+	protected function getCentralAuthLink( $name ) {
+		return Linker::linkKnown(
+			SpecialPage::getTitleFor( 'CentralAuth', $name ),
+			htmlspecialchars( $name )
+		);
+	}
+}
---
+++ GlobalRename/GlobalUserMergeLogger.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Log a global merge into the local log
+ *
+ * @license GNU GPL v2+
+ * @author Kunal Mehta
+ */
+
+class GlobalUserMergeLogger {
+	/**
+	 * @var User
+	 */
+	private $performingUser;
+
+	/**
+	 * @param User $performingUser
+	 */
+	public function __construct( User $performingUser ) {
+		$this->performingUser = $performingUser;
+	}
+
+	/**
+	 * @param CentralAuthUser[] $oldNames
+	 * @param string $newName
+	 * @param string $reason
+	 */
+	public function log( array $oldNames, $newName, $reason ) {
+		$logEntry = new ManualLogEntry( 'gblrename', 'merge' );
+		$logEntry->setPerformer( $this->performingUser );
+
+		$logEntry->setTarget(
+			Title::makeTitleSafe( NS_SPECIAL, 'CentralAuth/' . $newName )
+		);
+
+		$imploded = implode( '|',
+			array_map( function( CentralAuthUser $user ) {
+				return $user->getName();
+			}, $oldNames )
+		);
+
+		$logEntry->setComment( $reason );
+		$logEntry->setParameters( array(
+			'4::olduser' => $imploded,
+			'5::newuser' => $newName,
+		) );
+		$logid = $logEntry->insert();
+		$logEntry->publish( $logid );
+	}
+}
