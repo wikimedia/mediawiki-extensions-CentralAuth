c1843a0d8ecde5dba45d49133fe8a5f6544b1874
Kunal Mehta <legoktm@gmail.com>
2014-07-08 00:06:16 -0700

    Global merge tool
    Allows for a list of global users to be merged
    into one "final user". This will help after
    SUL finalisation where one user may end up with
    multiple global accounts.
    
    This follows a similar structure and re-uses
    code from GlobalRename.
    
    Introduces Special:GlobalUserMerge for stewards
    to use, and re-uses Special:GlobalRenameProgress
    for users to use.
    
    We inject LocalUserMergeJobs into local wikis
    which uses SpecialUserMerge to update the
    relevant local tables.
    
    Depends on I2bfcecad9 in UserMerge (merged)
    
    Bug: 47918
    Change-Id: Ie76c28234b24e66c708ad0dad03251a7e80ea531

---
+++ GlobalRename/GlobalUserMergeLogFormatter.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Format the gblrename/merge log entries
+ */
+class GlobalUserMergeLogFormatter extends LogFormatter {
+	protected function getMessageParameters() {
+		parent::getMessageParameters();
+		$params = $this->extractParameters();
+		$lang = $this->context->getLanguage();
+
+		$this->parsedParameters[3] = Message::rawParam( $lang->commaList(
+			array_map(
+				array( $this, 'getCentralAuthLink'),
+				explode( '|', $params[3] )
+			)
+		) );
+		$this->parsedParameters[4] = Message::rawParam( $this->getCentralAuthLink( $params[4] ) );
+
+		ksort( $this->parsedParameters );
+		return $this->parsedParameters;
+	}
+
+	/**
+	 * @param string $name
+	 * @return array
+	 */
+	protected function getCentralAuthLink( $name ) {
+		if ( $this->plaintext ) {
+			return Title::newFromText( 'CentralAuth/' . $name, NS_SPECIAL );
+		} else {
+			return Linker::linkKnown(
+				SpecialPage::getTitleFor( 'CentralAuth', $name ),
+				htmlspecialchars( $name )
+			);
+		}
+	}
+}
