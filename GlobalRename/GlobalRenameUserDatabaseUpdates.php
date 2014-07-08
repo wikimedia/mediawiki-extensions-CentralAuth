6652ffeab1a9ec664a1480ee3d16ade59f5ac2c3
Kunal Mehta <legoktm@gmail.com>
2014-07-08 00:06:16 -0700

    Global merge tool
    Allows for a list of global users to be merged
    into one "final user". This will help after
    SUL finalisation where one user may end up with
    multiple global accounts.
    
    This follows a similar structure and re-uses
    code from GlobalRename.
    
    Introduces Special:GlobalUserMerge for stewards
    to use, and re-uses Special:GlobalRenameProgress
    for users to use. This special page can be
    disabled through $wgCentralAuthEnableUserMerge.
    
    We inject LocalUserMergeJobs into local wikis
    which uses SpecialUserMerge to update the
    relevant local tables.
    
    Depends on I2bfcecad9 in UserMerge (merged)
    
    Bug: 47918
    Change-Id: Ie76c28234b24e66c708ad0dad03251a7e80ea531

--- GlobalRename/GlobalRenameUserDatabaseUpdates.php
+++ GlobalRename/GlobalRenameUserDatabaseUpdates.php
@@ -16,6 +16,47 @@ class GlobalRenameUserDatabaseUpdates {
 	}
 
 	/**
+	 * Merge a global user's rows into
+	 * another global user's ones.
+	 *
+	 * @param string $oldname
+	 * @param string $newname
+	 */
+	public function merge( $oldname, $newname ) {
+		$dbw = $this->getDB();
+
+		$dbw->begin( __METHOD__ );
+		// Delete the old user's globaluser row
+		$dbw->delete(
+			'globaluser',
+			array( 'gu_name' => $oldname ),
+			__METHOD__
+		);
+
+		// Move localuser rows to the new name
+		// if there is no row already there
+		// Note that in some cases, an account
+		// may not exist at the new name on
+		// that wiki yet.
+		$dbw->update(
+			'localuser',
+			array( 'lu_name' => $newname ),
+			array( 'lu_name' => $oldname ),
+			__METHOD__,
+			array( 'IGNORE' )
+		);
+
+		// Delete the ones that are duplicates,
+		// we'll use the existing rows
+		$dbw->delete(
+			'localuser',
+			array( 'lu_name' => $oldname )
+		);
+
+		$dbw->commit( __METHOD__ );
+	}
+
+	/**
 	 * @param string $oldname
 	 * @param string $newname
 	 */
