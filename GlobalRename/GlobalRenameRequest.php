7224b92da8449d1357c792e97f719aea82014332
Bryan Davis <bd808@wikimedia.org>
2014-08-25 18:36:06 -0600

    [WIP] Request a global account rename
    As a user who is going to be forcibly renamed
    I would like the opportunity to choose a new name that I like
    So that I'm not stuck with OLDNAME~wiki as my new name.
    
    Provides a new Special:GlobalRenameRequest page that can be used to
    submit a rename request to the global rename queue. Also adds a check
    for pending rename requests to the existing AbortNewAccount hook.
    
    The feature is guarded using the $wgCentralAuthEnableGlobalRenameRequest
    global flag.
    
    TODO:
    * Ajax check for name availability
    * Add UI note about why email is required
    
    Bug: 68886
    Change-Id: I5a657934453f99aa189d795988564b8f1cfa9633

---
+++ GlobalRename/GlobalRenameRequest.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * @section LICENSE
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Data access object for global rename requests.
+ *
+ * @author Bryan Davis <bd808@wikimedia.org>
+ * @copyright © 2014 Bryan Davis and Wikimedia Foundation.
+ */
+class GlobalRenameRequest {
+
+	const PENDING = 'pending';
+	const APPROVED = 'approved';
+	const REJECTED = 'rejected';
+
+	/**
+	 * Get a DatabaseBase object for the CentralAuth db
+	 *
+	 * @param int $type DB_SLAVE or DB_MASTER
+	 * @return DatabaseBase
+	 */
+	protected static function getDB( $type ) {
+		if ( $type === DB_MASTER ) {
+			return CentralAuthUser::getCentralDB();
+		} else {
+			return CentralAuthUser::getCentralSlaveDB();
+		}
+	}
+
+	/**
+	 * Create a new rename request.
+	 *
+	 * @param string $username User making request
+	 * @param string $wiki Home wiki of requesting user
+	 * @param string $newname Reqested name
+	 * @param string $reason User's explanation for rename request
+	 * @return bool True if successful, false otherwise
+	 */
+	public static function createRequest (
+		$username, $wiki, $newname, $reason
+	) {
+		$dbw = self::getDB( DB_MASTER );
+
+		$dbw->insert(
+			'renameuser_queue',
+			array(
+				'rq_name'         => $username,
+				'rq_wiki'         => $wiki,
+				'rq_newname'      => $newname,
+				'rq_reason'       => $reason,
+				'rq_requested_ts' => $dbw->timestamp(),
+				'rq_status'       => self::PENDING,
+			),
+			__METHOD__
+		);
+
+		return $dbw->affectedRows() === 1;
+	}
+
+	/**
+	 * Check to see if there is a pending rename request for the given user and
+	 * wiki.
+	 *
+	 * @param string $username
+	 * @param string $wiki
+	 * @return int|bool Request id or false if none found
+	 */
+	public static function userHasPendingRequest( $username, $wiki ) {
+		$dbw = self::getDB( DB_SLAVE );
+		$res = $dbw->selectRow(
+			'renameuser_queue',
+			array( 'rq_id' ),
+			array(
+				'rq_name'   => $username,
+				'rq_wiki'   => $wiki,
+				'rq_status' => self::PENDING,
+			),
+			__METHOD__
+		);
+
+		return ( $res === false ) ? false : $res->rq_id;
+	}
+
+	/**
+	 * Get a request record.
+	 *
+	 * @param int $id Request id
+	 * @return array Request
+	 */
+	public static function getRequest( $id ) {
+		$dbw = self::getDB( DB_SLAVE );
+		$res = $dbw->selectRow(
+			'renameuser_queue',
+			array(
+				'id'        => 'rq_id',
+				'name'      => 'rq_name',
+				'wiki'      => 'rq_wiki',
+				'newname'   => 'rq_newname',
+				'reason'    => 'rq_reason',
+				'requested' => 'rq_requested_ts',
+				'status'    => 'rq_status',
+				'completed' => 'rq_completed_ts',
+				'deleted'   => 'rq_deleted',
+				'performer' => 'rq_performer',
+				'comments'  => 'rq_comments',
+			),
+			array( 'rq_id'   => $id, ),
+			__METHOD__
+		);
+		return $res;
+	}
+
+	/**
+	 * Check to see if there is a pending rename request to the given name.
+	 *
+	 * @param string $newname
+	 * @return bool
+	 */
+	public static function nameHasPendingRequest( $newname ) {
+		$dbw = self::getDB( DB_SLAVE );
+		$res = $dbw->selectRow(
+			'renameuser_queue',
+			array( 'rq_id' ),
+			array(
+				'rq_newname' => $newname,
+				'rq_status'  => self::PENDING,
+			),
+			__METHOD__
+		);
+
+		return $res !== false;
+	}
+
+	/**
+	 * Check to see if a given username is available for use via CentralAuth.
+	 *
+	 * Note that this is not a definiative check. It does not include checking
+	 * for AntiSpoof, TitleBlacklist or other AbortNewAccount hook blocks.
+	 *
+	 * @param string $name
+	 * @return Status Canonicalized name
+	 */
+	public static function isNameAvailable ( $name ) {
+		$safe = User::getCanonicalName( $name, 'creatable' );
+		$status = Status::newGood( $safe );
+
+		if ( $safe === false || $safe === '' ) {
+			$status->fatal( 'globalrenamerequest-newname-err-invalid' );
+			return $status;
+		}
+
+		if ( self::nameHasPendingRequest( $safe ) ) {
+			$status->fatal( 'globalrenamerequest-newname-err-taken' );
+			return $status;
+		}
+
+		// New user creation checks against local wiki only using an API request,
+		// but we need to check against te central user table instead
+		$centralUser = new CentralAuthUser( $safe );
+		if (
+			$centralUser->exists() ||
+			$centralUser->renameInProgressOn( wfWikiID() ) ||
+			$centralUser->listUnattached()
+		) {
+			$status->fatal( 'globalrenamerequest-newname-err-taken' );
+			return $status;
+		}
+
+		return $status;
+	}
+
+} //end GlobalRenameRequest
