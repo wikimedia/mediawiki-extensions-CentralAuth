18098d1c62b91c78632dd589b95f1c302a1d374e
Kunal Mehta <legoktm@gmail.com>
2014-10-28 11:02:16 -0700

    Allow for searching the global rename log by previous username
    Requires If8bdbadd88 in core.
    
    TODO: Write a script to back-populate log_search.
    
    Bug: 70850
    Change-Id: I13c39874ff5f8690002658a34b9bcdf523ed0776

--- includes/CentralAuthHooks.php
+++ includes/CentralAuthHooks.php
@@ -1939,4 +1939,37 @@ class CentralAuthHooks {
 		}
 		return true;
 	}
+
+	/**
+	 * @param $type
+	 * @param WebRequest $request
+	 * @param array $qc
+	 * @return bool
+	 */
+	public static function onSpecialLogAddLogSearchRelations( $type, WebRequest $request, array &$qc ) {
+		if ( $type === 'gblrename' ) {
+			$oldname = User::getCanonicalName( $request->getVal( 'oldname' ) );
+			if ( $oldname ) {
+				$qc = array( 'ls_field' => 'oldname', 'ls_value' => $oldname );
+			}
+		}
+
+		return true;
+	}
+
+	public static function onLogEventsListGetExtraInputs( $type, LogEventsList $list, &$input ) {
+		if ( $type === 'gblrename' ) {
+			$value = $list->getRequest()->getVal( 'oldname', '' );
+			if ( $value ) {
+				$value = User::getCanonicalName( $value ) ? : '';
+			}
+			$input = Xml::inputLabel(
+				$list->msg( 'centralauth-log-gblrename-oldname' )->text(),
+				'oldname',
+				'mw-log-gblrename-oldname',
+				20,
+				$value
+			);
+		}
+	}
 }
