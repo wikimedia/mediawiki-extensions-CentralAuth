7cff42a5a9d0fef3221043ad9a5485c5982c6efd
Kunal Mehta <legoktm@gmail.com>
2014-10-28 15:08:51 -0700

    LocalPageMoveJob: Use MovePage
    This probably depends upon Ic502638 in MediaWiki core.
    Change-Id: Ic5026384b92a0d68d628397ffe1de6e5b6183f02

--- includes/LocalRenameJob/LocalPageMoveJob.php
+++ includes/LocalRenameJob/LocalPageMoveJob.php
@@ -5,14 +5,9 @@
  */
 class LocalPageMoveJob extends Job {
 	/**
-	 * @var bool
+	 * @var User
 	 */
-	private $setAFOverride;
-
-	/**
-	 * @var bool
-	 */
-	private $setTBOverride;
+	private $user;
 
 	/**
 	 * @param array $params
@@ -29,7 +24,7 @@ class LocalPageMoveJob extends Job {
 		// Need to set $wgUser so the move log is attributed correctly.
 		global $wgUser;
 		$oldUser = $wgUser;
-		$wgUser = User::newFromName( $this->params['renamer'] );
+		$this->user = User::newFromName( $this->params['renamer'] );
 		foreach ( $this->params['pages'] as $current => $target ) {
 			$this->movePage(
 				Title::newFromText( $current ),
@@ -42,58 +37,21 @@ class LocalPageMoveJob extends Job {
 	}
 
 	protected function movePage( Title $oldPage, Title $newPage ) {
-		if ( $newPage->exists() && !$oldPage->isValidMoveTarget( $newPage ) ) {
-			wfDebugLog( 'CentralAuthRename', "Could not move $oldPage to $newPage" );
-			// @todo log this somewhere publicly?
-		} else {
-			$msg = wfMessage( 'centralauth-rename-movelog' )
-				->params( $this->params['from'], $this->params['to'] )
-				->inContentLanguage()
-				->text();
-			$errors = $oldPage->moveTo( $newPage, false, $msg, !$this->params['suppressredirects'] );
-			if ( is_array( $errors ) ) {
-				// AbuseFilter or TitleBlacklist might be interfering, bug 67875
-				if ( $errors[0][0] === 'hookaborted' ) {
-					wfDebugLog( 'CentralAuthRename', "Page move prevented by hook: {$oldPage->getPrefixedText()} -> {$newPage->getPrefixedText()}" );
-					$this->disableMoveBlockingThings();
-					// Do it again
-					$oldPage->moveTo( $newPage, false, $msg, !$this->params['suppressredirects'] );
-					$this->enableMoveBlockingThings();
-				}
-			}
+		$mp = new MovePage( $oldPage, $newPage );
+		$valid = $mp->isValidMove();
+		if ( !$valid->isOK() ) {
+			wfDebugLog( 'CentralAuthRename', "Invalid page move: {$oldPage->getPrefixedText()} -> {$newPage->getPrefixedText()}" );
+			return;
 		}
 
-	}
+		$msg = wfMessage( 'centralauth-rename-movelog' )
+			->params( $this->params['from'], $this->params['to'] )
+			->inContentLanguage()
+			->text();
 
-	/**
-	 * FIXME: on a of scale one to evil, this is super evil
-	 */
-	private function disableMoveBlockingThings() {
-		global $wgUser;
-		if ( !$wgUser->isAllowed( 'abusefilter-bypass' ) ) {
-			$wgUser->mRights[] = 'abusefilter-bypass';
-			$this->setAFOverride = true;
-		}
-
-		if ( !$wgUser->isAllowed( 'tboverride' ) ) {
-			$wgUser->mRights[] = 'tboverride';
-			$this->setTBOverride = true;
+		$status = $mp->move( $this->user, $msg, true );
+		if ( !$status->isOK() ) {
+			wfDebugLog( 'CentralAuthRename', "Page move failed: {$oldPage->getPrefixedText()} -> {$newPage->getPrefixedText()}" );
 		}
 	}
-
-	private function enableMoveBlockingThings() {
-		global $wgUser;
-		$rights = array();
-		if ( $this->setAFOverride ) {
-			$rights[] = 'abusefilter-bypass';
-			$this->setAFOverride = false;
-		}
-		if ( $this->setTBOverride ) {
-			$rights[] = 'tboverride';
-			$this->setTBOverride = false;
-		}
-
-		$wgUser->mRights = array_diff( $wgUser->mRights, $rights );
-	}
-
 }
