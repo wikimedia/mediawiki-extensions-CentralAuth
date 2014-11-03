d62edf8de449d229d6473d5104145d2fe7c47048
Dan Duvall <dduvall@wikimedia.org>
2014-11-03 13:26:01 -0800

    WIP: QA: Refactored for new features of mediawiki_selenium
    Refactored tests to use new helper methods like `visit_wiki` for
    navigating to configured wikis by name, and `user` and `password` for
    referencing the MediaWiki user/password of the current environmental
    context.
    
    Added an environments.yml file that provides configuration defaults for
    various environments. It still needs the correct URLs for beta and
    test2's CentralAuth login wikis, and "alternative" wikis.
    
    Change-Id: I324776a8e5de08adcd443cf35b3a35d670fed98f

--- tests/browser/features/step_definitions/login_steps.rb
+++ tests/browser/features/step_definitions/login_steps.rb
@@ -4,21 +4,19 @@ Given(/^I am using a global account$/) do
 end
 
 Given(/^I am logged in to the primary wiki domain$/) do
-  visit(LoginPage).login_with(ENV["MEDIAWIKI_USER"], ENV["MEDIAWIKI_PASSWORD"])
+  visit(LoginPage).login_with(user, password)
 end
 
 When(/^I visit the central login wiki domain$/) do
-  @browser.goto ENV["MEDIAWIKI_CENTRALAUTH_LOGINWIKI_URL"]
+  visit_wiki(:login)
 end
 
 When(/^I visit the alternate wiki domain$/) do
-  @browser.goto ENV["MEDIAWIKI_CENTRALAUTH_ALTWIKI_URL"]
+  visit_wiki(:alternative)
 end
 
-Then(/^I should be logged into (.+) also$/) do |site|
-  step "there should be a link to #{ENV['MEDIAWIKI_USER'].gsub('_', ' ')}"
-end
-
-Then(/^there should be a link to (.+)$/) do |text|
-  expect(on(LoginPage).username_displayed_element.when_present.text).to match(text)
+Then(/^I should be logged in$/) do
+  visit(LoginPage) do |page|
+    expect(page.username_displayed_element.when_present.text).to match(user_label)
+  end
 end
--- tests/browser/features/support/env.rb
+++ tests/browser/features/support/env.rb
@@ -2,3 +2,7 @@ require "rubygems"
 require "bundler/setup"
 
 Bundler.require(:default)
+
+require "mediawiki_selenium/support/env"
+require "mediawiki_selenium/support/hooks"
+require "mediawiki_selenium/support/pages"
