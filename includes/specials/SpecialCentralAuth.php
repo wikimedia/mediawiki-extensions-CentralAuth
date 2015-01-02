0d8f2a50ae1ffd88a577d53212e8b98e3b75b2f4
Marius Hoch <hoo@online.de>
2015-01-02 19:40:17 +0100

    Correctly parse links in block reasons on SpecialCentralAuth
    Needs Ibdedf087f85
    
    Change-Id: I7e8109ea7bb8c3180f381263b20f6f1665abb0de

--- includes/specials/SpecialCentralAuth.php
+++ includes/specials/SpecialCentralAuth.php
@@ -502,6 +502,7 @@ class SpecialCentralAuth extends SpecialPage {
 	 * @return String
 	 */
 	private function formatBlockStatus( $row ) {
+		$additionalHtml = '';
 		if ( isset( $row['blocked'] ) && $row['blocked'] ) {
 			if ( $row['block-expiry'] == 'infinity' ) {
 				$text = $this->msg( 'centralauth-admin-blocked2-indef' )->parse();
@@ -514,12 +515,24 @@ class SpecialCentralAuth extends SpecialPage {
 			}
 
 			if ( $row['block-reason'] ) {
-				$text .= ' ' . $this->msg( 'centralauth-admin-blocked-reason', $row['block-reason'] )->parse();
+				$reason = Sanitizer::escapeHtmlAllowEntities( $row['block-reason'] );
+				$reason = Linker::formatLinksInComment(
+					$reason,
+					null,
+					false,
+					$row['wiki']
+				);
+
+				$msg = $this->msg( 'centralauth-admin-blocked-reason' );
+				$msg->rawParams( '<span class="plainlinks">' . $reason . '</span>' );
+
+				$additionalHtml .= ' ' . $msg->parse();
 			}
 
 			if ( $row['block-expiry'] == 'infinity' ) {
-				return $text;
+				return $text . $additionalHtml;
 			}
+
 		} else {
 			$text = $this->msg( 'centralauth-admin-notblocked' )->parse();
 		}
@@ -529,7 +542,7 @@ class SpecialCentralAuth extends SpecialPage {
 			'Special:Log/block',
 			$text,
 			$this->msg( 'centralauth-admin-blocklog' )->text(),
-			'page=User:' . urlencode( $this->mUserName ) );
+			'page=User:' . urlencode( $this->mUserName ) ) . $additionalHtml;
 	}
 
 	/**
@@ -544,6 +557,7 @@ class SpecialCentralAuth extends SpecialPage {
 		}
 		$wikiname = $wiki->getDisplayName();
 		$editCount = $this->getLanguage()->formatNum( intval( $row['editCount'] ) );
+
 		return self::foreignLink(
 			$row['wiki'],
 			'Special:Contributions/' . $this->mUserName,
