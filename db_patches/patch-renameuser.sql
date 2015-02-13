e7544c6cbc0c89416f40020558165c037b978f29
paladox <thomasmulhall410@yahoo.com>
2015-02-13 23:00:09 +0000

    Update sql
    * Update seperate sql files to match changes in centralauth.sql.
    
    * Some changes for sqlite.
    
    Change-Id: I13bc6a821863fbbd403d3823d4445eb1292e3f67

--- db_patches/patch-renameuser.sql
+++ db_patches/patch-renameuser.sql
@@ -1,12 +1,12 @@
-CREATE TABLE renameuser_status (
-  -- Old name being renamed from
-  ru_oldname varchar(255) binary not null,
-  -- New name being renamed to
-  ru_newname varchar(255) binary not null,
-  -- WikiID
-  ru_wiki varchar(255) binary not null,
-  -- current state of the renaming
-  ru_status enum ('queued', 'inprogress', 'failed'),
-
-  UNIQUE KEY `ru_oldname` (`ru_oldname`, `ru_wiki`)
+CREATE TABLE /*_*/renameuser_status (
+    -- Old name being renamed from
+    ru_oldname varchar(255) binary not null,
+    -- New name being renamed to
+    ru_newname varchar(255) binary not null,
+    -- WikiID
+    ru_wiki varchar(255) binary not null,
+    -- current state of the renaming
+    ru_status enum ('queued', 'inprogress', 'failed')
 ) /*$wgDBTableOptions*/;
+
+CREATE UNIQUE INDEX /*i*/ru_oldname ON /*_*/renameuser_status (ru_oldname, ru_wiki);
