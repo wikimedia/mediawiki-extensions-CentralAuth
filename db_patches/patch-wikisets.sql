17a1c8d0007f0e623598f41928b002e73d3df97b
paladox <thomasmulhall410@yahoo.com>
2015-02-13 23:00:09 +0000

    Update sql
    * Update seperate sql files to match changes in centralauth.sql.
    
    * Some changes for sqlite.
    
    Change-Id: I13bc6a821863fbbd403d3823d4445eb1292e3f67

--- db_patches/patch-wikisets.sql
+++ db_patches/patch-wikisets.sql
@@ -1,29 +1,28 @@
 -- Sets of wikis (for things like restricting global groups)
 -- May be defined in two ways: only specified wikis or all wikis except opt-outed
-CREATE TABLE wikiset (
-	-- ID of wikiset
-	ws_id int auto_increment,
-	-- Display name of wikiset
-	ws_name varchar(255) not null,
-	-- Type of set: opt-in or opt-out
-	ws_type enum ('optin', 'optout'),
-	-- Wikis in that set. Why isn't it a separate table?
-	-- Because we can just use such simple list, we don't need complicated queries on it
-	-- Let's suppose that max length of db name is 31 (32 with ","), then we have space for
-	-- 2048 wikis. More than we need
-	ws_wikis blob not null,
-	
-	PRIMARY KEY ws_id (ws_id),
-	UNIQUE ws_name (ws_name)
+CREATE TABLE /*_*/wikiset (
+  -- ID of wikiset
+  ws_id int PRIMARY KEY AUTO_INCREMENT,
+  -- Display name of wikiset
+  ws_name varchar(255) not null,
+  -- Type of set: opt-in or opt-out
+  ws_type enum ('optin', 'optout'),
+  -- Wikis in that set. Why isn't it a separate table?
+  -- Because we can just use such simple list, we don't need complicated queries on it
+  -- Let's suppose that max length of db name is 31 (32 with ","), then we have space for
+  -- 2048 wikis. More than we need
+  ws_wikis blob not null
 ) /*$wgDBTableOptions*/;
+
+CREATE UNIQUE INDEX /*i*/ws_name ON /*_*/wikiset (ws_name);
+
 
 -- Allow certain global groups to have their permissions only on certain wikis
-CREATE TABLE global_group_restrictions (
-	-- Group to restrict
-	ggr_group varchar(255) not null,
-	-- Wikiset to use
-	ggr_set int not null,
-
-	PRIMARY KEY (ggr_group),
-	KEY (ggr_set)
+CREATE TABLE /*_*/global_group_restrictions (
+  -- Group to restrict
+  ggr_group varchar(255) not null PRIMARY KEY,
+  -- Wikiset to use
+  ggr_set int not null
 ) /*$wgDBTableOptions*/;
+
+CREATE INDEX /*i*/ggr_set ON /*_*/global_group_restrictions (ggr_set);
