8e717675c26926f16c841c459dfda63365a5581d
paladox <thomasmulhall410@yahoo.com>
2015-02-13 23:00:09 +0000

    Update sql
    * Update seperate sql files to match changes in centralauth.sql.
    
    * Convert spaces to tabs in sql files.
    
    * Some changes for sqlite.
    
    Change-Id: I13bc6a821863fbbd403d3823d4445eb1292e3f67

--- db_patches/patch-wikisets.sql
+++ db_patches/patch-wikisets.sql
@@ -1,8 +1,8 @@
 -- Sets of wikis (for things like restricting global groups)
 -- May be defined in two ways: only specified wikis or all wikis except opt-outed
-CREATE TABLE wikiset (
+CREATE TABLE /*_*/wikiset (
 	-- ID of wikiset
-	ws_id int auto_increment,
+	ws_id int PRIMARY KEY AUTO_INCREMENT,
 	-- Display name of wikiset
 	ws_name varchar(255) not null,
 	-- Type of set: opt-in or opt-out
@@ -11,19 +11,19 @@ CREATE TABLE wikiset (
 	-- Because we can just use such simple list, we don't need complicated queries on it
 	-- Let's suppose that max length of db name is 31 (32 with ","), then we have space for
 	-- 2048 wikis. More than we need
-	ws_wikis blob not null,
-	
-	PRIMARY KEY ws_id (ws_id),
-	UNIQUE ws_name (ws_name)
+	ws_wikis blob not null
 ) /*$wgDBTableOptions*/;
+
+CREATE UNIQUE INDEX /*i*/ws_name ON /*_*/wikiset (ws_name);
+
 
 -- Allow certain global groups to have their permissions only on certain wikis
-CREATE TABLE global_group_restrictions (
+CREATE TABLE /*_*/global_group_restrictions (
 	-- Group to restrict
-	ggr_group varchar(255) not null,
+	ggr_group varchar(255) not null PRIMARY KEY AUTO_INCREMENT,
 	-- Wikiset to use
-	ggr_set int not null,
+	ggr_set int not null
 
-	PRIMARY KEY (ggr_group),
-	KEY (ggr_set)
 ) /*$wgDBTableOptions*/;
+
+CREATE INDEX /*i*/ggr_set ON /*_*/global_group_restrictions (ggr_set);
