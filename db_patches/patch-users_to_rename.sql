5f6fbe8f61a995662a4f079b865d54d21d681d95
paladox <thomasmulhall410@yahoo.com>
2015-02-13 23:00:09 +0000

    Update sql
    * Update seperate sql files to match changes in centralauth.sql.
    
    * Convert spaces to tabs in sql files.
    
    * Some changes for sqlite.
    
    Change-Id: I13bc6a821863fbbd403d3823d4445eb1292e3f67

--- db_patches/patch-users_to_rename.sql
+++ db_patches/patch-users_to_rename.sql
@@ -2,18 +2,18 @@
 -- who will be renamed in the
 -- glorious finalization.
 CREATE TABLE /*_*/users_to_rename (
-  -- id
-  utr_id int primary key auto_increment,
+	-- id
+	utr_id int PRIMARY KEY AUTO_INCREMENT,
 
-  -- username
-  utr_name varchar(255) binary not null,
+	-- username
+	utr_name varchar(255) binary not null,
 
-  -- wiki the user is on
-  utr_wiki varchar(255) binary not null,
+	-- wiki the user is on
+	utr_wiki varchar(255) binary not null,
 
-  -- bitfield of a user's status
-  -- could be: notified via email, talk page, and finally: renamed
-  utr_status int default 0
+	-- bitfield of a user's status
+	-- could be: notified via email, talk page, and finally: renamed
+	utr_status int default 0
 ) /*$wgDBTableOptions*/;
 
 CREATE UNIQUE INDEX /*i*/utr_user ON /*_*/users_to_rename (utr_name, utr_wiki);
