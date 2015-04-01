5f1704ed9049c479ebfcd76e2d61d7a5334d5885
Kunal Mehta <legoktm@gmail.com>
2015-04-01 01:15:59 -0700

    Log promote to global renames in the global rename log
    Bug: T93235
    Change-Id: I472842bdca2c2490f2a1376819d8f422b63991ea

--- includes/GlobalRename/GlobalRenameUserLogger.php
+++ includes/GlobalRename/GlobalRenameUserLogger.php
@@ -46,4 +46,35 @@ class GlobalRenameUserLogger {
 		$logid = $logEntry->insert();
 		$logEntry->publish( $logid );
 	}
+
+	/**
+	 * Log the promotion of a local unattached to a global
+	 *
+	 * @param string $oldName
+	 * @param string $wiki
+	 * @param string $newName
+	 * @param string $reason
+	 */
+	public function logPromotion( $oldName, $wiki, $newName, $reason ) {
+		$logEntry = new ManualLogEntry( 'gblrename', 'promote' );
+		$logEntry->setPerformer( $this->performingUser );
+
+		$logEntry->setTarget(
+			Title::makeTitleSafe( NS_SPECIAL, 'CentralAuth/' . $newName )
+		);
+
+		$logEntry->setComment( $reason );
+		$logEntry->setParameters( array(
+			'4::olduser' => $oldName,
+			'5::newuser' => $newName,
+			'6::oldwiki' => $wiki,
+		) );
+
+		$logEntry->setRelations( array(
+			'oldname' => $oldName,
+		) );
+
+		$logid = $logEntry->insert();
+		$logEntry->publish( $logid );
+	}
 }
