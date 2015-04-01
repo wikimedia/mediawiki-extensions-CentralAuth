3d7bf89841421bee6c1147f79e4951a4b6672e10
Kunal Mehta <legoktm@gmail.com>
2015-04-01 01:15:59 -0700

    Log promote to global renames in the global rename log
    Bug: T93235
    Change-Id: I472842bdca2c2490f2a1376819d8f422b63991ea
    (cherry picked from commit e4708811a9584665fe0a252fbf7de425f9f5d282)

--- includes/GlobalRename/GlobalRenameLogFormatter.php
+++ includes/GlobalRename/GlobalRenameLogFormatter.php
@@ -1,14 +1,20 @@
 <?php
 
 /**
- * Make the gblrename/rename log entry look pretty
+ * Handles the following log types:
+ *  - gblrename/rename
+ *  - gblrename/promote
  */
 class GlobalRenameLogFormatter extends LogFormatter {
 	protected function getMessageParameters() {
 		parent::getMessageParameters();
 		$params = $this->extractParameters();
 
-		$this->parsedParameters[3] = $this->getCentralAuthLink( $params[3] );
+		if ( $this->entry->getSubtype() === 'promote' ) {
+			$this->parsedParameters[3] = $this->getLocalWikiLink( $params[3], $params[5] );
+		} else { // rename
+			$this->parsedParameters[3] = $this->getCentralAuthLink( $params[3] );
+		}
 		$this->parsedParameters[4] = $this->getCentralAuthLink( $params[4] );
 
 		ksort( $this->parsedParameters );
@@ -26,5 +32,14 @@ class GlobalRenameLogFormatter extends LogFormatter {
 		}
 
 		return Message::rawParam( Linker::link( $title, htmlspecialchars( $name ) ) );
+	}
+
+	protected function getLocalWikiLink( $name, $wiki ) {
+		$text = "User:$name@$wiki";
+		if ( $this->plaintext ) {
+			return "[[$text]]";
+		}
+
+		return Message::rawParam( WikiMap::foreignUserLink( $wiki, $name, $text ) );
 	}
 }
