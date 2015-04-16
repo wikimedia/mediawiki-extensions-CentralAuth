53ecda276fb9b11f6d4b743023d2197165a0c95d
glaisher <glaisher.wiki@gmail.com>
2015-04-16 17:34:26 +0500

    Show block options on Special:CentralAuth
    Bug: T20182
    Change-Id: Id334a2452e5f5f7725700114fdb0fe933b56eda2

--- includes/CentralAuthUser.php
+++ includes/CentralAuthUser.php
@@ -2146,7 +2146,11 @@ class CentralAuthUser extends AuthPluginUser {
 
 		// And while we're in here, look for user blocks :D
 		$result = $db->select( 'ipblocks',
-			array( 'ipb_expiry', 'ipb_reason' ),
+			array(
+				'ipb_expiry', 'ipb_reason', 'ipb_block_email',
+				'ipb_anon_only', 'ipb_create_account',
+				'ipb_enable_autoblock', 'ipb_allow_usertalk',
+			),
 			array( 'ipb_user' => $data['id'] ),
 			__METHOD__ );
 		global $wgLang;
@@ -2154,6 +2158,11 @@ class CentralAuthUser extends AuthPluginUser {
 			if ( $wgLang->formatExpiry( $row->ipb_expiry, TS_MW ) > wfTimestampNow() ) {
 				$data['block-expiry'] = $row->ipb_expiry;
 				$data['block-reason'] = $row->ipb_reason;
+				$data['block-anononly'] = (bool)$row->ipb_anon_only;
+				$data['block-nocreate'] = (bool)$row->ipb_create_account;
+				$data['block-noautoblock'] = !( (bool)$row->ipb_enable_autoblock );
+				$data['block-nousertalk'] = !( (bool)$row->ipb_allow_usertalk ); // Poorly named database column
+				$data['block-noemail'] = (bool)$row->ipb_block_email;
 				$data['blocked'] = true;
 			}
 		}
