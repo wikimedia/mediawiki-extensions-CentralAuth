14a7ac5d751fbed7d93c99bfd5f068eafb0cb6ef
Derk-Jan Hartman <hartman.wiki@gmail.com>
2015-05-15 23:47:12 +0200

    Guard against QUOTA_EXCEEDED_ERR for anon users
    Bug: T96581
    Change-Id: I24dfed652250d72c5d3376fa48eda06f91792dd3

--- modules/inline/anon-set.js
+++ modules/inline/anon-set.js
@@ -1,8 +1,9 @@
 var t = new Date();
 // Set CentralAuthAnon to 1 day in the future
 t.setTime( t.getTime() + 86400000 );
-if ( 'localStorage' in window && window.localStorage ) {
+try {
 	localStorage.setItem( 'CentralAuthAnon', t.getTime() );
-} else {
+} catch ( e ) {
+	/* When no localStorage or when QUOTA reached */
 	document.cookie = 'CentralAuthAnon=1; expires=' + t.toGMTString() + '; path=/';
 }
