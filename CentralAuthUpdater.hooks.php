aa567f5f08836c86dafe73f7b70156e0cec132b8
paladox <thomasmulhall410@yahoo.com>
2015-05-17 22:02:08 +0200

    Add web updater support for sql
    * When running mw-config in web browser it should now update sql.
    
    Change-Id: Icfa9c8327ce8f6d7c28c9525ea9cdce8ac8ca662

---
+++ CentralAuthUpdater.hooks.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Class containing updater functions for a ConfirmAccount environment
+ */
+class CentralAuthUpdaterHooks {
+
+	/**
+	 * @param DatabaseUpdater $updater
+	 * @return bool
+	 */
+	public static function addSchemaUpdates( DatabaseUpdater $updater ) {
+		if ( $updater->getDB()->getType() == 'mysql' || $updater->getDB()->getType() == 'sqlite' ) {
+
+			$updater->addExtensionTable( 'globalnames', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionTable( 'localnames', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionTable( 'globaluser', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionTable( 'localuser', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionTable( 'global_user_groups', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionTable( 'global_group_permissions', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionTable( 'wikiset', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionTable( 'global_group_restrictions', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionTable( 'renameuser_status', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionTable( 'renameuser_queue', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionTable( 'users_to_rename', __DIR__ . "/central-auth.sql" );
+			$updater->addExtensionField( 'globaluser', 'gu_auth_token', __DIR__ . "/db_patches/patch-globaluser_gu_authtoken.sql" );
+			$updater->modifyExtensionField( 'globaluser', 'gu_hidden', __DIR__ . "/db_patches/patch-gu_hidden.sql" );
+			$updater->addExtensionField( 'globaluser', 'gu_locked', __DIR__ . "/db_patches/patch-gu_hidden.sql" );
+			$updater->addExtensionField( 'globaluser', 'gu_hidden', __DIR__ . "/db_patches/patch-gu_hidden.sql" );
+		}
+		return true;
+	}
+}
