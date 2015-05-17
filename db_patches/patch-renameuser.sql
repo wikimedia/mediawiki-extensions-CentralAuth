0539b60a4dd9a987c2117c2ede58020004a1f20f
paladox <thomasmulhall410@yahoo.com>
2015-05-17 22:02:08 +0200

    Convert spaces to TAB in sql files.
    Change-Id: I44fc093fb44b14ca1878eb6f0d1f2e909e4e5ffe

--- db_patches/patch-renameuser.sql
+++ db_patches/patch-renameuser.sql
@@ -1,12 +1,12 @@
 CREATE TABLE renameuser_status (
-  -- Old name being renamed from
-  ru_oldname varchar(255) binary not null,
-  -- New name being renamed to
-  ru_newname varchar(255) binary not null,
-  -- WikiID
-  ru_wiki varchar(255) binary not null,
-  -- current state of the renaming
-  ru_status enum ('queued', 'inprogress', 'failed'),
+	-- Old name being renamed from
+	ru_oldname varchar(255) binary not null,
+	-- New name being renamed to
+	ru_newname varchar(255) binary not null,
+	-- WikiID
+	ru_wiki varchar(255) binary not null,
+	-- current state of the renaming
+	ru_status enum ('queued', 'inprogress', 'failed'),
 
-  UNIQUE KEY `ru_oldname` (`ru_oldname`, `ru_wiki`)
+	UNIQUE KEY `ru_oldname` (`ru_oldname`, `ru_wiki`)
 ) /*$wgDBTableOptions*/;
