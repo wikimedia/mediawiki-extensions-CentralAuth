4595efa1b5d343823c27c16c712f4b7c2b29308f
paladox <thomasmulhall410@yahoo.com>
2015-05-17 22:02:08 +0200

    Add web updater support for sql
    * When running mw-config in web browser it should now update sql.
    
    * Add new settings $wgCentralAuthCluster for when server for sql is on different server it will abort.
    
    Change-Id: Icfa9c8327ce8f6d7c28c9525ea9cdce8ac8ca662

--- CentralAuth.php
+++ CentralAuth.php
@@ -258,6 +258,12 @@ $wgCentralAuthEnableGlobalRenameRequest = false;
 $wgCentralAuthCheckSULMigration = false;
 
 /**
+ * Use the main db if this is set to false, to use a specific external db, just
+ * use any key defined in $wgExternalServers
+ */
+$wgCentralAuthCluster = false;
+
+/**
  * Initialization of the autoloaders, and special extension pages.
  */
 $caBase = __DIR__;
@@ -324,6 +330,8 @@ $wgAutoloadClasses['UsersToRenameDatabaseUpdates'] = "$caBase/includes/UsersToRe
 
 // only used by maintenance/sendConfirmAndMigrateEmail.php
 $wgAutoloadClasses['EmailableUser'] = "$caBase/includes/EmailableUser.php";
+
+$wgAutoloadClasses['CentralAuthUpdaterHooks'] = __DIR__ . "/includes/CentralAuthUpdater.hooks.php";
 
 $wgMessagesDirs['SpecialCentralAuth'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['SpecialCentralAuth'] = "$caBase/CentralAuth.i18n.php";
@@ -405,6 +413,9 @@ $wgHooks['LoadGlobalUserPage'][] = 'CentralAuthHooks::onLoadGlobalUserPage';
 
 // For UserMerge
 $wgHooks['DeleteAccount'][] = 'CentralAuthHooks::onDeleteAccount';
+
+# Schema changes
+$wgHooks['LoadExtensionSchemaUpdates'][] = 'ConfirmAccountUpdaterHooks::onLoadExtensionSchemaUpdates';
 
 $wgAvailableRights[] = 'centralauth-merge';
 $wgAvailableRights[] = 'centralauth-unmerge';
