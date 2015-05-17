ba4fe420992622313f05d9bf89b4734146c1a0af
paladox <thomasmulhall410@yahoo.com>
2015-05-17 22:02:08 +0200

    Add web updater support for sql
    * When running mw-config in web browser it should now update sql.
    
    * Add new settings $wgCentralAuthCluster for when server for sql is on different server it will abort.
    
    Change-Id: Icfa9c8327ce8f6d7c28c9525ea9cdce8ac8ca662

--- db_patches/patch-gu_hidden.sql
+++ db_patches/patch-gu_hidden.sql
@@ -4,6 +4,10 @@
 -- Victor Vasiliev, January 2010.
 
 ALTER TABLE globaluser
+	ADD INDEX gu_locked( gu_name(255), gu_locked ),
+	ADD INDEX gu_hidden( gu_name(255), gu_hidden(255) );
+
+ALTER TABLE globaluser
 	MODIFY COLUMN gu_hidden VARBINARY(255) NOT NULL DEFAULT '';
 
 -- Not hidden (was 0)
@@ -11,7 +15,3 @@ UPDATE globaluser SET gu_hidden = '' WHERE gu_hidden = '0';
 -- Hidden from public lists, but remains visible in Special:CentralSuth log
 UPDATE globaluser SET gu_hidden = 'lists' WHERE gu_hidden = '1';
 -- There's also "suppressed" level, which wasn't used before this schema change
-
-ALTER TABLE globaluser
-	ADD INDEX gu_locked( gu_name(255), gu_locked ),
-	ADD INDEX gu_hidden( gu_name(255), gu_hidden(255) );
