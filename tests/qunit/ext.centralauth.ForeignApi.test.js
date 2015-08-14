29f574556c040676dd8c8b367e00da68714cd3da
Bartosz Dziewoński <matma.rex@gmail.com>
2015-08-15 01:16:46 +0200

    Implement a mediawiki.ForeignApi extension
    Extends mw.ForeignApi with CentralAuth authentication handling,
    amending every request with a 'centralauthtoken' parameter, if
    required.
    
    Every request to the foreign wiki will be preceded by a
    'action=centralauthtoken' request to the local wiki, except in cases
    where we detect that it is unproductive or unnecessary.
    
    Depends on Ic20b9682d28633baa87d22e6e9fb71ce507da58d in MediaWiki core.
    
    Bug: T66636
    Change-Id: I0fd05ef8b9c9db0fdb59c6cb248f364259f80456

---
+++ tests/qunit/ext.centralauth.ForeignApi.test.js
@@ -0,0 +1,74 @@
+( function ( mw, $ ) {
+	QUnit.module( 'ext.centralauth.ForeignApi', QUnit.newMwEnvironment( {
+		setup: function () {
+			this.server = this.sandbox.useFakeServer();
+			this.server.respondImmediately = true;
+			this.clock = this.sandbox.useFakeTimers();
+		},
+		teardown: function () {
+			// https://github.com/jquery/jquery/issues/2453
+			this.clock.tick();
+		},
+		config: {
+			wgUserName: true
+		}
+	} ) );
+
+	QUnit.test( 'Anonymous users do not get centralauthtoken', function ( assert ) {
+		QUnit.expect( 1 );
+		mw.config.set( 'wgUserName', null );
+
+		this.server.respond( function ( request ) {
+			request.respond( 200, { 'Content-Type': 'application/json' }, '[]' );
+		} );
+
+		var api = new mw.ForeignApi( '//localhost:4242/w/api.php' );
+
+		var spy = this.sandbox.spy( api, 'getCentralAuthToken' );
+		api.get( {} );
+		assert.ok( !spy.called, 'Anonymous users do not ask for centralauthtoken' );
+	} );
+
+	QUnit.test( 'Logged in users get centralauthtoken if not logged in remotely', function ( assert ) {
+		QUnit.expect( 1 );
+		mw.config.set( 'wgUserName', 'User' );
+
+		this.sandbox.stub( mw.ForeignApi.prototype, 'checkForeignLogin' ).returns(
+			$.Deferred().reject().promise()
+		);
+
+		this.server.respond( function ( request ) {
+			request.respond( 200, { 'Content-Type': 'application/json' }, '[]' );
+		} );
+
+		var api = new mw.ForeignApi( '//localhost:4242/w/api.php' );
+
+		var spy = this.sandbox.stub( api, 'getCentralAuthToken' ).returns(
+			$.Deferred.resolve( 'CENTRALAUTHTOKEN' ).promise()
+		);
+		api.get( {} );
+		assert.ok( spy.called, 'Logged in users ask for centralauthtoken if not logged in remotely' );
+	} );
+
+	QUnit.test( 'Logged in users do not get centralauthtoken if logged in remotely', function ( assert ) {
+		QUnit.expect( 1 );
+		mw.config.set( 'wgUserName', 'User' );
+
+		this.sandbox.stub( mw.ForeignApi.prototype, 'checkForeignLogin' ).returns(
+			$.Deferred().resolve().promise()
+		);
+
+		this.server.respond( function ( request ) {
+			request.respond( 200, { 'Content-Type': 'application/json' }, '[]' );
+		} );
+
+		var api = new mw.ForeignApi( '//localhost:4242/w/api.php' );
+
+		var spy = this.sandbox.stub( api, 'getCentralAuthToken' ).returns(
+			$.Deferred.resolve( 'CENTRALAUTHTOKEN' ).promise()
+		);
+		api.get( {} );
+		assert.ok( !spy.called, 'Logged in users do not ask for centralauthtoken if logged in remotely' );
+	} );
+
+}( mediaWiki, jQuery ) );
