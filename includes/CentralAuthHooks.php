98a1e2a7ae14eefc9e10ed20714b4fbc07101f1f
Aaron Schulz <aschulz@wikimedia.org>
2015-08-30 15:06:53 -0700

    Converted SUL2 handshake tokens to use CentralAuthUser::getSessionCache()
    Bug: T108253
    Change-Id: I3eda1fe8f84e85e3f38faf87ade5e8d229110c55
    (cherry picked from commit 6bd83005148b4ca04ba4163deceb0b12a73beffa)

--- includes/CentralAuthHooks.php
+++ includes/CentralAuthHooks.php
@@ -706,7 +706,7 @@ class CentralAuthHooks {
 	 * @return bool
 	 */
 	protected static function doCentralLoginRedirect( User $user, CentralAuthUser $centralUser, &$inject_html ) {
-		global $wgCentralAuthLoginWiki, $wgMemc, $wgSecureLogin;
+		global $wgCentralAuthLoginWiki, $wgSecureLogin;
 
 		$context = RequestContext::getMain();
 		$request = $context->getRequest();
@@ -776,7 +776,7 @@ class CentralAuthHooks {
 				'currentProto'  => $request->detectProtocol() // current proto (in case login is https, but final page is http)
 			);
 			Hooks::run( 'CentralAuthLoginRedirectData', array( $centralUser, &$data ) );
-			$wgMemc->set( $key, $data, 60 );
+			CentralAuthUser::getSessionCache()->set( $key, $data, 60 );
 
 			$wiki = WikiMap::getWiki( $wgCentralAuthLoginWiki );
 			// Use WikiReference::getFullUrl(), returns a protocol-relative URL if needed
