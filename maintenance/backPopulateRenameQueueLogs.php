e6de995a5ea2f4b4a34268c3eac5ddba6256fd07
Kunal Mehta <legoktm@gmail.com>
2015-09-09 11:24:47 -0700

    Add script to back-populate promote to global logs from the rename queue
    Bug: T93214
    Change-Id: I739358cae3b8b09b295b22b16f0a7d9355ad605c

---
+++ maintenance/backPopulateRenameQueueLogs.php
@@ -0,0 +1,66 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+/**
+ * Script to back-populate log_search for global rename entries
+ */
+class BackPopulateRenameQueueLogs extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function execute() {
+		$dbr = CentralAuthUser::getCentralSlaveDB();
+		$rows = $dbr->select(
+				array( 'renameuser_queue' ),
+				array( 'rq_name', 'rq_wiki', 'rq_newname', 'rq_completed_ts', 'rq_performer' ),
+				array(
+						'rq_wiki IS NOT NULL',
+						'rq_status' => 'approved',
+						'rq_completed_ts < 20150408153600'
+				),
+				__METHOD__
+		);
+
+		$count = 0;
+		foreach ( $rows as $row ) {
+			$performer = CentralAuthUser::newFromId( $row->rq_performer );
+			$localUser = User::newFromName( $performer->getName() );
+			$logEntry = new ManualLogEntry( 'gblrename', 'promote' );
+			$logEntry->setPerformer( $localUser );
+
+			$logEntry->setTarget(
+					Title::makeTitleSafe( NS_SPECIAL, 'CentralAuth/' . $row->rq_newname )
+			);
+
+			// We didn't store this anywhere :(
+			// $logEntry->setComment( $reason );
+
+			$logEntry->setParameters( array(
+					'4::olduser' => $row->rq_name,
+					'5::newuser' => $row->rq_newname,
+					'6::oldwiki' => $row->rq_wiki,
+			) );
+
+			$logEntry->setRelations( array(
+					'oldname' => $row->rq_name,
+			) );
+			$logEntry->setTimestamp( $row->rq_completed_ts );
+
+			$logid = $logEntry->insert();
+			$count++;
+			if ( $count % 10 == 0 ) {
+				wfWaitForSlaves();
+			}
+		}
+
+	}
+}
+
+$maintClass = 'BackPopulateRenameQueueLogs';
+require_once RUN_MAINTENANCE_IF_MAIN;
