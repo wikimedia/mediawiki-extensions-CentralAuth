376673d5e9090446612b1d63592e875310632fa7
Gergő Tisza <gtisza@wikimedia.org>
2016-04-27 19:09:46 +0000

    Fix suggestLoginUsername
    * fix typo in operator
    * delegate to non-central session provider when the username cookie
      is missing, not just when it is invalid
    
    Change-Id: Ie28146a8e7b80d924e2c6fead7a0b91aa981ccf6

---
+++ tests/phpunit/CentralAuthSessionProviderTest.php
@@ -0,0 +1,32 @@
+<?php
+
+class CentralAuthSessionProviderTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @dataProvider provideSuggestLoginUsername
+	 */
+	public function testSuggestLoginUsername( $cookies, $expectedUsername) {
+		$provider = new CentralAuthSessionProvider( [
+			'priority' => 42,
+			'cookieOptions' => [ 'prefix' => '' ],
+			'centralCookieOptions' => [ 'prefix' => '' ],
+		] );
+		$request = new FauxRequest();
+		$request->setCookies( $cookies, '' );
+		$this->assertSame( $expectedUsername, $provider->suggestLoginUsername( $request ) );
+	}
+
+	public function provideSuggestLoginUsername() {
+		return [
+			[ [ 'Foo' => 'bar' ], null ],
+			[ [ 'User' => 'Foo' ], 'Foo' ],
+			[ [ 'User' => 'foo' ], 'Foo' ],
+			[ [ 'User' => '#&@!' ], null ],
+			[ [ 'UserName' => 'Foo' ], 'Foo' ],
+			[ [ 'UserName' => 'foo' ], 'Foo' ],
+			[ [ 'UserName' => '#&@!' ], null ],
+			[ [ 'User' => 'Foo', 'UserName' => 'Bar' ], 'Foo' ],
+			[ [ 'User' => '0' ], '0' ],
+			[ [ 'UserName' => '0' ], '0' ],
+		];
+	}
+}
