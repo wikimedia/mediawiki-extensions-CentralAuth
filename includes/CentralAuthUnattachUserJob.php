20b29113bdb3a88666d3b8768a5837cc9a0bd88b
Bryan Davis <bd808@wikimedia.org>
2016-07-07 17:24:18 -0600

    Cleanup localuser records when wiki is missing data
    Under some (undefined) circumstances the localuser table can record that
    a user has been attached to a wiki but the wiki's own db is missing
    a record for the user. There is a maintenance script to find and cleanup
    these dangling records, but that's not of much help for a user who is
    effected as they will not be able to authenticate to any wiki until the
    next time someone runs the script.
    
    Add logic to catch this problem in the most likely places and proactively
    clean up the dangling attachment record. The cleanup is done by running
    a job which cleans up the record in a safe manner and clears caches as
    appropriate. Using a job makes this cleanup safe for GET requests.
    
    Bug: T119736
    Change-Id: I3d219cfff0dc835faa72d5fe72a80e57235dcb04

---
+++ includes/CentralAuthUnattachUserJob.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @section LICENSE
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * A job to unattach a user.
+ *
+ * @copyright © 2016 Wikimedia Foundation and contributors.
+ */
+class CentralAuthUnattachUserJob extends Job {
+
+	/**
+	 * Constructor
+	 *
+	 * @param Title $title Associated title
+	 * @param array $params Job parameters
+	 */
+	public function __construct( $title, $params ) {
+		parent::__construct( 'CentralAuthUnattachUserJob', $title, $params );
+	}
+
+	/**
+	 * Execute the job
+	 *
+	 * @return bool
+	 */
+	public function run() {
+		$username = $this->params['username'];
+		$wiki = $this->params['wiki'];
+		$user = User::newFromName( $username );
+		if ( $user->getId() !== 0 ) {
+			// User has been created since this job was queued.
+			// Races are fun!
+			return true;
+		}
+		$causer = CentralAuthUser::getMasterInstanceByName( $username );
+		$causer->removeLocalName( $wiki );
+		if ( $causer->exists() ) {
+			$causer->adminUnattach( array( $wiki ) );
+		}
+		return true;
+	}
+}
