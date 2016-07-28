758c93cc2e0363fc007142067fda2b7e442adb1d
Ariel T. Glenn <ariel@wikimedia.org>
2016-07-28 23:04:12 +0300

    provide the equivalent of getSlaveServer.php for centralauth
    This script will display the name of a slave server for use by
    scripts outside of MediaWiki not necessarily in php, that may
    do queries or dump tables from the db
    
    Bug: T141563
    Change-Id: I24e2ee1d92b993d6e5bd0b0a2df7a7f4e7588a00

---
+++ maintenance/getCentralAuthDBInfo.php
@@ -0,0 +1,25 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once( "$IP/maintenance/Maintenance.php" );
+
+class GetCentralAuthDBInfo extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Display info for slave server hosting the CentralAuth DB';
+	}
+
+	public function execute() {
+		global $wgDBadminuser;
+		global $wgDBadminpassword;
+		$db = CentralAuthUtils::getCentralSlaveDB();
+		$host = $db->getServer();
+		$this->output( "$host\n" );
+	}
+}
+
+$maintClass ="GetCentralAuthDBInfo";
+require_once RUN_MAINTENANCE_IF_MAIN;
