dbdf3f195f285462bfe5b17e85f853c8726fd42c
niharika29 <niharika@wikimedia.org>
2016-08-15 12:55:47 +0000

    [WIP] Add local ID and global ID to localuser table
    Bug: T141951
    Change-Id: I5dd361c0ded1c69dd0109c7cfafdcefe0ba5a94b

---
+++ maintenance/populateLocalAndGlobalIds.php
@@ -0,0 +1,44 @@
+<?php
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once( "$IP/maintenance/Maintenance.php" );
+
+class PopulateLocalAndGlobalIds extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Populate the localuser.lu_local_id and localuser.lu_global_id fields";
+		$this->setBatchSize( 100 );
+	}
+
+	public function execute() {
+		$this->output( "Populating lu_local_id field..." );
+		$dbr = $this->getDB( DB_SLAVE );
+		$dbw = $this->getDB( DB_MASTER );
+		$dbw->selectDB( 'centralauth' );
+		$iterator = new BatchRowIterator(
+			$dbr,
+			'localuser',
+			['lu_wiki', 'lu_name'],
+			$this->mBatchSize
+		);
+		$processed = 0;
+		foreach ( $iterator as $batch ) {
+			foreach ( $batch as $row ) {
+				$lName = $row->lu_name;
+				$lWiki = $row->lu_wiki;
+				$dbr->selectDB( $lWiki );
+				$lId = $dbr->selectField( 'user', 'user_id', ['user_name' => $lName] );
+				$result = $dbw->update( 'localuser', ['lu_local_id' => $lId], ['lu_name' => $lName, 'lu_wiki' => $lWiki] );
+				if ( !$result ) {
+					$this->output( "Update failed for user " . $lName . " for wiki " . $lWiki );
+				}
+				$processed += $dbw->affectedRows();
+			}
+			$this->output( "Updated $processed notifications.\n" );
+		}
+		$this->output( "Done.\n" );
+	}
+}
