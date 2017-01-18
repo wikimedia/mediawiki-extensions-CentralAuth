965549e504fef97b927cd88b1ddf6de5b5a97355
This, that and the other <at.light@live.com.au>
2017-01-18 13:23:55 +1100

    Make CentralAuth work with expiring user groups in core
    This doesn't give CentralAuth expiring user rights; it just keeps
    everything working after the merge of
    I93c955dc7a970f78e32aa503c01c67da30971d1a in core.
    
    Change-Id: I86eb1d5619347ce54a5f33a591417742ebe5d6f8

--- includes/CentralAuthGroupMembershipProxy.php
+++ includes/CentralAuthGroupMembershipProxy.php
@@ -92,12 +92,28 @@ class CentralAuthGroupMembershipProxy {
 	}
 
 	/**
+	 * Replaces User::getGroupMemberships()
+	 * @return array Associative array of (group name => UserGroupMembership object)
+	 */
+	function getGroupMemberships() {
+		$groups = $this->getGroups();
+		return array_combine( $groups, array_map( function( $group ) {
+			return new UserGroupMembership( $this->getId(), $group );
+		}, $groups ) );
+	}
+
+	/**
 	 * replaces addUserGroup
 	 * @param string[]|string $group
+	 * @param string|null $expiry
 	 *
 	 * @return bool
 	 */
-	function addGroup( $group ) {
+	function addGroup( $group, $expiry = null ) {
+		if ( $expiry !== null ) {
+			throw new InvalidArgumentException( __METHOD__ . ' cannot process expiries' );
+		}
+
 		$this->mGlobalUser->addToGlobalGroups( $group );
 		return true;
 	}
