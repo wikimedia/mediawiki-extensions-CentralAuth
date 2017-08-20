4b2de1ad55092a724a24e18ce18af042d865ce32
melos <webmelos@gmail.com>
2017-08-20 21:53:46 +0200

    Mark user page moves as bot on rename user process
    When a user is renamed globalrename tool mark local user page moves as
    a bot if the move user pages option is set.
    
    Bug: T97659
    Change-Id: Id553dc97313270f1ecc8ee2b9d1cbab2f010d93a

--- includes/LocalRenameJob/LocalPageMoveJob.php
+++ includes/LocalRenameJob/LocalPageMoveJob.php
@@ -34,25 +34,41 @@ class LocalPageMoveJob extends Job {
 	}
 
 	public function run() {
-		if ( isset( $this->params['session'] ) ) {
-			$callback = RequestContext::importScopedSession( $this->params['session'] );
-			$this->addTeardownCallback( function () use ( &$callback ) {
-				ScopedCallback::consume( $callback );
-			} );
-		}
-		$this->user = User::newFromName( $this->params['renamer'] );
-		if ( isset( $this->params['pages'] ) ) {
-			// Old calling style for b/c
-			foreach ( $this->params['pages'] as $current => $target ) {
-				$this->movePage(
-					Title::newFromText( $current ),
-					Title::newFromText( $target )
-				);
+		try {
+			if ( isset( $this->params['session'] ) ) {
+				$callback = RequestContext::importScopedSession( $this->params['session'] );
+				$this->addTeardownCallback( function () use ( &$callback ) {
+					ScopedCallback::consume( $callback );
+				} );
 			}
-		} else {
-			$oldTitle = Title::makeTitle( $this->params['old'][0], $this->params['old'][1] );
-			$newTitle = Title::makeTitle( $this->params['new'][0], $this->params['new'][1] );
-			$this->movePage( $oldTitle, $newTitle );
+			$this->user = User::newFromName( $this->params['renamer'] );
+			// Mark user page moves as bot on rename user process T97659
+			if ( !$this->user->isAllowed( 'bot' ) ) {
+				$this->user->mRights[] = 'bot';
+				$safeRemoveBotRight = true;
+			}
+			if ( isset( $this->params['pages'] ) ) {
+				// Old calling style for b/c
+				foreach ( $this->params['pages'] as $current => $target ) {
+					$this->movePage(
+						Title::newFromText( $current ),
+						Title::newFromText( $target )
+					);
+				}
+			} else {
+				$oldTitle = Title::makeTitle( $this->params['old'][0], $this->params['old'][1] );
+				$newTitle = Title::makeTitle( $this->params['new'][0], $this->params['new'][1] );
+				$this->movePage( $oldTitle, $newTitle );
+			}
+		} finally {
+			// We always want to remove the bot right added above.
+			// So we remove it at the end of the job process via addTeardownCallback
+			// otherwise some page move action couldn't be marked as bot
+			if ( isset( $safeRemoveBotRight ) ) {
+				$this->addTeardownCallback( function () {
+					unset( $this->user->mRights[array_search( 'bot', $this->user->mRights )] );
+				} );
+			}
 		}
 		return true;
 	}
