wikimedia/mediawiki-extensions-CentralAuth

commit 6b4dda7de6a2cc587f9db4faf8269b8daa842580

Update for API error i18n
See Iae0e2ce3.

Change-Id: I68bc047b6e8206142be2a5e866a96d78fd1e8ae4

includes/api/ApiSetGlobalAccountStatus.php
@@ -31,32 +31,47 @@
 class ApiSetGlobalAccountStatus extends ApiBase {
 	/* Heavily based on code from SpecialCentralAuth::doSubmit */
 	public function execute() {
-		$globalUser = CentralAuthUser::getMasterInstanceByName( $this->getParameter( 'user' ) );
-		if ( !$this->getUser()->isAllowed( 'centralauth-lock' ) ) {
-			$this->dieUsageMsg( array( 'badaccess-groups' ) );
-		} elseif ( !$globalUser->exists() ) {
-			$this->dieUsageMsg( array( 'nosuchuser', $globalUser->getName() ) );
-		} elseif ( $globalUser->isOversighted() && !$this->getUser()->isAllowed( 'centralauth-oversight' ) ) {
-			$this->dieUsageMsg( array( 'nosuchuser', $globalUser->getName() ) );
-		} elseif ( !$this->getRequest()->getCheck( 'locked' ) && $this->getParameter( 'hidden' ) === null ) {
-			$this->dieUsage( "At least one of the parameters locked, hidden is required", "missingparam" );
+		$params = $this->extractRequestParams();
+		$this->requireAtLeastOneParameter( $params, 'locked', 'hidden' );
+
+		if ( is_callable( [ $this, 'checkUserRightsAny' ] ) ) {
+			$this->checkUserRightsAny( 'centralauth-lock' );
+		} else {
+			if ( !$this->getUser()->isAllowed( 'centralauth-lock' ) ) {
+				$this->dieUsageMsg( [ 'badaccess-groups' ] );
+			}
 		}
 
-		$setLocked = $this->getParameter( 'locked' );
+		$globalUser = CentralAuthUser::getMasterInstanceByName( $params['user'] );
+		if ( !$globalUser->exists() ||
+			$globalUser->isOversighted() && !$this->getUser()->isAllowed( 'centralauth-oversight' )
+		) {
+			if ( is_callable( [ $this, 'dieWithError' ] ) ) {
+				$this->dieWithError(
+					[ 'nosuchusershort', wfEscapeWikiText( $globalUser->getName() ) ], 'nosuchuser'
+				);
+			} else {
+				$this->dieUsageMsg( [ 'nosuchuser', $globalUser->getName() ] );
+			}
+		}
 
-		if ( !$setLocked ) {
+		if ( !$params['locked'] ) {
 			// Don't lock or unlock
 			$setLocked = null;
 		} else {
 			$setLocked = $setLocked === 'lock';
 		}
 
-		$setHidden = $this->getParameter( 'hidden' );
-		$reason = $this->getParameter( 'reason' );
-		$stateCheck = $this->getParameter( 'statecheck' );
+		$setHidden = $params['hidden'];
+		$reason = $params['reason'];
+		$stateCheck = $params['statecheck'];
 
 		if ( $stateCheck && $stateCheck !== $globalUser->getStateHash( true ) ) {
-			$this->dieUsage( 'Edit conflict detected, Aborting.', 'editconflict' );
+			if ( is_callable( [ $this, 'dieWithError' ] ) ) {
+				$this->dieWithError( 'apierror-centralauth-editconflict', 'editconflict' );
+			} else {
+				$this->dieUsage( 'Edit conflict detected, Aborting.', 'editconflict' );
+			}
 		}
 
 		$status = $globalUser->adminLockHide(

includes/api/ApiCentralAuthToken.php
@@ -39,22 +39,42 @@ class ApiCentralAuthToken extends ApiBase {
 
 		// If we're in JSON callback mode, no tokens can be obtained
 		if ( $this->lacksSameOriginSecurity() ) {
-			$this->dieUsage( 'Cannot obtain a centralauthtoken when using a callback', 'hascallback' );
+			if ( is_callable( [ $this, 'dieWithError' ] ) ) {
+				$this->dieWithError( 'apiwarn-tokens-origin', 'hascallback' );
+			} else {
+				$this->dieUsage( 'Cannot obtain a centralauthtoken when using a callback', 'hascallback' );
+			}
 		}
 
 		if ( $user->isAnon() ) {
-			$this->dieUsage( 'Anonymous users cannot obtain a centralauthtoken', 'notloggedin' );
+			if ( is_callable( [ $this, 'dieWithError' ] ) ) {
+				$this->dieWithError( 'apierror-centralauth-notloggedin', 'notloggedin' );
+			} else {
+				$this->dieUsage( 'Anonymous users cannot obtain a centralauthtoken', 'notloggedin' );
+			}
 		}
 
 		$session = MediaWiki\Session\SessionManager::getGlobalSession();
 		if ( !$session->getProvider() instanceof CentralAuthSessionProvider ) {
-			$this->dieUsage( 'Can only obtain a centralauthtoken when using CentralAuth sessions', 'badsession' );
+			if ( is_callable( [ $this, 'dieWithError' ] ) ) {
+				$this->dieWithError( 'apierror-centralauth-badsession', 'badsession' );
+			} else {
+				$this->dieUsage(
+					'Can only obtain a centralauthtoken when using CentralAuth sessions', 'badsession'
+				);
+			}
 		}
 		$id = $session->getId();
 
 		$centralUser = CentralAuthUser::getInstance( $user );
 		if ( !$centralUser->exists() || !$centralUser->isAttached() ) {
-			$this->dieUsage( 'Cannot obtain a centralauthtoken without an attached global account', 'notattached' );
+			if ( is_callable( [ $this, 'dieWithError' ] ) ) {
+				$this->dieWithError( 'apierror-centralauth-notattached', 'notattached' );
+			} else {
+				$this->dieUsage(
+					'Cannot obtain a centralauthtoken without an attached global account', 'notattached'
+				);
+			}
 		}
 
 		$data = array(

includes/api/ApiDeleteGlobalAccount.php
@@ -31,17 +31,25 @@
 class ApiDeleteGlobalAccount extends ApiBase {
 	/* Heavily based on code from SpecialCentralAuth::doSubmit */
 	public function execute() {
-		if ( !$this->getUser()->isAllowed( 'centralauth-unmerge' ) ) {
-			$this->dieUsageMsg( array( 'badaccess-groups' ) );
+		if ( is_callable( [ $this, 'checkUserRightsAny' ] ) ) {
+			$this->checkUserRightsAny( 'centralauth-unmerge' );
+		} else {
+			if ( !$this->getUser()->isAllowed( 'centralauth-unmerge' ) ) {
+				$this->dieUsageMsg( array( 'badaccess-groups' ) );
+			}
 		}
 
 		$params = $this->extractRequestParams();
 
 		$globalUser = CentralAuthUser::getMasterInstanceByName( $params['user'] );
-		if ( !$globalUser->exists() ) {
-			$this->dieUsageMsg( array( 'nosuchuser', $globalUser->getName() ) );
-		} elseif ( $globalUser->isOversighted() && !$this->getUser()->isAllowed( 'centralauth-oversight' ) ) {
-			$this->dieUsageMsg( array( 'nosuchuser', $globalUser->getName() ) );
+		if ( !$globalUser->exists() ||
+			$globalUser->isOversighted() && !$this->getUser()->isAllowed( 'centralauth-oversight' )
+		) {
+			if ( is_callable( [ $this, 'dieWithError' ] ) ) {
+				$this->dieWithError( array( 'nosuchusershort', wfEscapeWikitext( $globalUser->getName() ) ) );
+			} else {
+				$this->dieUsageMsg( array( 'nosuchuser', $globalUser->getName() ) );
+			}
 		}
 
 		$status = $globalUser->adminDelete( $params['reason'] );

includes/api/ApiQueryGlobalAllUsers.php
@@ -79,9 +79,7 @@ class ApiQueryGlobalAllUsers extends ApiQueryBase {
 			);
 		}
 
-		if ( !is_null( $params['group'] ) && !is_null( $params['excludegroup'] ) ) {
-			$this->dieUsage( 'group and excludegroup cannot be used together', 'group-excludegroup' );
-		}
+		$this->requireMaxOneParameter( $params, 'group', 'excludegroup' );
 
 		if ( !empty( $params['group'] ) ) {
 			$this->addTables( 'global_user_groups' );

includes/session/CentralAuthTokenSessionProvider.php
@@ -27,7 +27,7 @@ class CentralAuthTokenSessionProvider extends \MediaWiki\Session\SessionProvider
 	 * Throw an exception, later
 	 *
 	 * @param string $code Error code
-	 * @param string $error Error message
+	 * @param string|array $error Error message key, or key+parameters
 	 * @returns SessionInfo
 	 */
 	private function makeException( $code, $error ) {
@@ -35,7 +35,13 @@ class CentralAuthTokenSessionProvider extends \MediaWiki\Session\SessionProvider
 
 		// First, schedule the throwing of the exception for later when the API
 		// is ready to catch it
-		$exception = new \UsageException( $error, $code );
+		if ( class_exists( \ApiUsageException::class ) ) {
+			$exception = \ApiUsageException::newWithMessage( null, $error, $code );
+		} else {
+			$msg = Message::newFromSpecifier( $error )
+				->inLanguage( 'en' )->useDatabase( false )->text();
+			$exception = new \UsageException( $msg, $code );
+		}
 		$wgHooks['ApiBeforeMain'][] = function () use ( $exception ) {
 			throw $exception;
 		};
@@ -75,7 +81,7 @@ class CentralAuthTokenSessionProvider extends \MediaWiki\Session\SessionProvider
 			!isset( $data['originSessionId'] )
 		) {
 			$this->logger->debug( __METHOD__ . ': centralauthtoken is invalid' );
-			return $this->makeException( 'badtoken', 'The centralauthtoken is not valid' );
+			return $this->makeException( 'badtoken', 'apierror-centralauth-badtoken' );
 		}
 
 		$userName = $data['userName'];
@@ -85,12 +91,12 @@ class CentralAuthTokenSessionProvider extends \MediaWiki\Session\SessionProvider
 		$userName = User::getCanonicalName( $userName, 'valid' );
 		if ( !$userName ) {
 			$this->logger->debug( __METHOD__ . ': invalid username' );
-			return $this->makeException( 'badtoken', 'The centralauthtoken is not valid' );
+			return $this->makeException( 'badtoken', 'apierror-centralauth-badtoken' );
 		}
 		if ( !User::isUsableName( $userName ) ) {
 			$this->logger->debug( __METHOD__ . ': unusable username' );
 			return $this->makeException( 'badusername',
-				"The user name \"$userName\" is not usable on this wiki" );
+				[ 'apierror-centralauth-badusername', wfEscapeWikiText( $userName ) ] );
 		}
 
 		// Try the central user
@@ -99,27 +105,27 @@ class CentralAuthTokenSessionProvider extends \MediaWiki\Session\SessionProvider
 		// Skip if they're being renamed
 		if ( $centralUser->renameInProgress() ) {
 			$this->logger->debug( __METHOD__ . ': rename in progress' );
-			return $this->makeException( 'renameinprogress', 'The user is being renamed, cannot use' );
+			return $this->makeException( 'renameinprogress', 'apierror-centralauth-renameinprogress' );
 		}
 
 		if ( !$centralUser->exists() ) {
 			$this->logger->debug( __METHOD__ . ': global account doesn\'t exist' );
-			return $this->makeException( 'badtoken', 'The centralauthtoken is not valid' );
+			return $this->makeException( 'badtoken', 'apierror-centralauth-badtoken' );
 		}
 		if ( !$centralUser->isAttached() && User::idFromName( $userName ) ) {
 			$this->logger->debug( __METHOD__ . ': not attached and local account exists' );
-			return $this->makeException( 'badtoken', 'The centralauthtoken is not valid' );
+			return $this->makeException( 'badtoken', 'apierror-centralauth-badtoken' );
 		}
 
 		$key = CentralAuthUtils::memcKey( 'api-token-blacklist', $centralUser->getId() );
 		if ( $cache->get( $key ) ) {
 			$this->logger->debug( __METHOD__ . ': user is blacklisted' );
-			return $this->makeException( 'badtoken', 'The centralauthtoken is not valid' );
+			return $this->makeException( 'badtoken', 'apierror-centralauth-badtoken' );
 		}
 
 		if ( $centralUser->authenticateWithToken( $token ) != 'ok' ) {
 			$this->logger->debug( __METHOD__ . ': token mismatch' );
-			return $this->makeException( 'badtoken', 'The centralauthtoken is not valid' );
+			return $this->makeException( 'badtoken', 'apierror-centralauth-badtoken' );
 		}
 
 		$this->logger->debug( __METHOD__ . ': logged in from session' );

commit d10d82cf8aa6fbd8d9378b42ab89de9d8f90f7f1

API: Unbreak setglobalaccountstatus locked=lock
Fixes a regression introduced in 6b4dda7de6a2c.

Bug: T175462
Change-Id: I3b70229107142a80eeeb952865be320d8121d435

includes/api/ApiSetGlobalAccountStatus.php
@@ -59,7 +59,7 @@ class ApiSetGlobalAccountStatus extends ApiBase {
 			// Don't lock or unlock
 			$setLocked = null;
 		} else {
-			$setLocked = $setLocked === 'lock';
+			$setLocked = $params['locked'] === 'lock';
 		}
 
 		$setHidden = $params['hidden'];