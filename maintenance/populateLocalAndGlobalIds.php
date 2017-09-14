d417e6a97b417dc96fcf228fd4b68d27cc23f5af
Reedy <reedy@wikimedia.org>
2017-09-14 20:09:09 +0100

    Remove class_exists() check. Covered by requireExtension
    Change-Id: I6d7e27898b4a67191c6999937fc8fa6b0f138ac5

--- maintenance/populateLocalAndGlobalIds.php
+++ maintenance/populateLocalAndGlobalIds.php
@@ -17,98 +17,93 @@ class PopulateLocalAndGlobalIds extends Maintenance {
 	}
 
 	public function execute() {
-		if ( class_exists( 'CentralAuthUtils' ) ) {
-			$dbr = CentralAuthUtils::getCentralSlaveDB();
-			$dbw = CentralAuthUtils::getCentralDB();
-			$lastGlobalId = -1;
+		$dbr = CentralAuthUtils::getCentralSlaveDB();
+		$dbw = CentralAuthUtils::getCentralDB();
+		$lastGlobalId = -1;
 
-			// Skip people in global rename queue
-			$wiki = wfWikiID();
-			$globalRenames = [];
+		// Skip people in global rename queue
+		$wiki = wfWikiID();
+		$globalRenames = [];
+		$rows = $dbr->select(
+			'renameuser_status',
+			'ru_oldname'
+		);
+		foreach ( $rows as $row ) {
+			$globalRenames[] = $row->ru_oldname;
+		}
+
+		$lb = wfGetLB( $wiki );
+		$ldbr = $lb->getConnection( DB_REPLICA, [], $wiki );
+
+		$this->output( "Populating fields for wiki $wiki... \n" );
+		do {
 			$rows = $dbr->select(
-				'renameuser_status',
-				'ru_oldname'
+				[ 'localuser', 'globaluser' ],
+				[ 'lu_name', 'gu_id' ],
+				[
+					// Start from where we left off in last batch
+					'gu_id >= ' . $lastGlobalId,
+					'lu_wiki' => $wiki,
+					// Only pick records not already populated
+					'lu_local_id' => null,
+					'gu_name = lu_name'
+				],
+				__METHOD__,
+				[ 'LIMIT' => $this->mBatchSize, 'ORDER BY' => 'gu_id ASC' ]
 			);
+			$numRows = $rows->numRows();
+
+			$globalUidToLocalName = [];
 			foreach ( $rows as $row ) {
-				$globalRenames[] = $row->ru_oldname;
-			}
-
-			$lb = wfGetLB( $wiki );
-			$ldbr = $lb->getConnection( DB_REPLICA, [], $wiki );
-
-			$this->output( "Populating fields for wiki $wiki... \n" );
-			do {
-				$rows = $dbr->select(
-					[ 'localuser', 'globaluser' ],
-					[ 'lu_name', 'gu_id' ],
-					[
-						// Start from where we left off in last batch
-						'gu_id >= ' . $lastGlobalId,
-						'lu_wiki' => $wiki,
-						// Only pick records not already populated
-						'lu_local_id' => null,
-						'gu_name = lu_name'
-					],
-					__METHOD__,
-					[ 'LIMIT' => $this->mBatchSize, 'ORDER BY' => 'gu_id ASC' ]
-				);
-				$numRows = $rows->numRows();
-
-				$globalUidToLocalName = [];
-				foreach ( $rows as $row ) {
-					if ( in_array( $row->lu_name, $globalRenames ) ) {
-						$this->output(
-							"User " . $row->lu_name . " not migrated (pending rename)\n"
-						);
-						continue;
-					}
-					$globalUidToLocalName[$row->gu_id] = $row->lu_name;
-				}
-				if ( !$globalUidToLocalName ) {
+				if ( in_array( $row->lu_name, $globalRenames ) ) {
+					$this->output(
+						"User " . $row->lu_name . " not migrated (pending rename)\n"
+					);
 					continue;
 				}
+				$globalUidToLocalName[$row->gu_id] = $row->lu_name;
+			}
+			if ( !$globalUidToLocalName ) {
+				continue;
+			}
 
-				$localNameToUid = [];
-				$localIds = $ldbr->select(
-					'user',
-					[ 'user_id', 'user_name' ],
-					[ 'user_name' => array_values( $globalUidToLocalName ) ]
+			$localNameToUid = [];
+			$localIds = $ldbr->select(
+				'user',
+				[ 'user_id', 'user_name' ],
+				[ 'user_name' => array_values( $globalUidToLocalName ) ]
+			);
+			foreach ( $localIds as $lid ) {
+				$localNameToUid[$lid->user_name] = $lid->user_id;
+			}
+			$updated = 0;
+			foreach ( $globalUidToLocalName as $gid => $uname ) {
+				// Save progress so we know where to start our next batch
+				$lastGlobalId = $gid;
+				$result = $dbw->update(
+					'localuser',
+					[
+						'lu_local_id' => $localNameToUid[$uname],
+						'lu_global_id' => $gid
+					],
+					[ 'lu_name' => $uname, 'lu_wiki' => $wiki ]
 				);
-				foreach ( $localIds as $lid ) {
-					$localNameToUid[$lid->user_name] = $lid->user_id;
-				}
-				$updated = 0;
-				foreach ( $globalUidToLocalName as $gid => $uname ) {
-					// Save progress so we know where to start our next batch
-					$lastGlobalId = $gid;
-					$result = $dbw->update(
-						'localuser',
-						[
-							'lu_local_id' => $localNameToUid[$uname],
-							'lu_global_id' => $gid
-						],
-						[ 'lu_name' => $uname, 'lu_wiki' => $wiki ]
+				if ( !$result ) {
+					$this->output(
+						"Update failed for global user $lastGlobalId for wiki $wiki \n"
 					);
-					if ( !$result ) {
-						$this->output(
-							"Update failed for global user $lastGlobalId for wiki $wiki \n"
-						);
-					} else {
-						// Count number of records actually updated
-						$updated++;
-					}
+				} else {
+					// Count number of records actually updated
+					$updated++;
 				}
-				$this->output(
-					"Updated $updated records. Last user: $lastGlobalId; Wiki: $wiki \n"
-				);
-				CentralAuthUtils::waitForSlaves();
-			} while ( $numRows >= $this->mBatchSize );
-			$lb->reuseConnection( $ldbr );
-			$this->output( "Completed $wiki \n" );
-		} else {
-			$this->error( "This script requires that the CentralAuth extension is enabled. " .
-				"Please enable it and try again.", 1 );
-		}
+			}
+			$this->output(
+				"Updated $updated records. Last user: $lastGlobalId; Wiki: $wiki \n"
+			);
+			CentralAuthUtils::waitForSlaves();
+		} while ( $numRows >= $this->mBatchSize );
+		$lb->reuseConnection( $ldbr );
+		$this->output( "Completed $wiki \n" );
 	}
 
 }
