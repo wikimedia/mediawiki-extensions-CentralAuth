a7844652b83f06aec5dc1cdfca8d0e4f24bac2ac
James D. Forrester <jforrester@wikimedia.org>
2017-11-02 11:41:50 -0700

    Replace OutputPage::setSquidMaxage with ::setCdnMaxage
    Renamed and deprecated in MediaWiki in f606fd8d since 1.27.
    
    Change-Id: Ibba2f486f0ecb684ded7efb09f9942f5e0f5fd7a

--- includes/specials/SpecialCentralAutoLogin.php
+++ includes/specials/SpecialCentralAutoLogin.php
@@ -225,7 +225,7 @@ class SpecialCentralAutoLogin extends UnlistedSpecialPage {
 		case 'start': // Main entry point
 			// Note this is safe to cache, because the cache already varies on
 			// the session cookies.
-			$this->getOutput()->setSquidMaxage( 1200 );
+			$this->getOutput()->setCdnMaxage( 1200 );
 
 			if ( !$this->checkIsLocalWiki() ) {
 				return;
@@ -244,7 +244,7 @@ class SpecialCentralAutoLogin extends UnlistedSpecialPage {
 		case 'checkLoggedIn': // Check if we're logged in centrally
 			// Note this is safe to cache, because the cache already varies on
 			// the session cookies.
-			$this->getOutput()->setSquidMaxage( 1200 );
+			$this->getOutput()->setCdnMaxage( 1200 );
 
 			if ( !$this->checkIsCentralWiki( $wikiid ) ) {
 				return;
