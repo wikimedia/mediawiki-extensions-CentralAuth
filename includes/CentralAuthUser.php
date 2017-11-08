91802a3729a52f8c3aa68c60f2c5f4a7eaf30076
Aaron Schulz <aschulz@wikimedia.org>
2017-11-07 18:53:10 -0800

    Use the proper cache key method in loadFromCache()
    Bug: T179999
    Change-Id: I9199ad964377dd3d079cb2d55f0a2ea88acdad56

--- includes/CentralAuthUser.php
+++ includes/CentralAuthUser.php
@@ -492,7 +492,7 @@ class CentralAuthUser extends AuthPluginUser implements IDBAccessObject {
 	protected function loadFromCache() {
 		$cache = ObjectCache::getMainWANInstance();
 		$data = $cache->getWithSetCallback(
-			$this->getCacheKey(),
+			$this->getCacheKey( $cache ),
 			$cache::TTL_DAY,
 			function ( $oldValue, &$ttl, array &$setOpts ) {
 				$dbr = CentralAuthUtils::getCentralSlaveDB();
@@ -569,10 +569,11 @@ class CentralAuthUser extends AuthPluginUser implements IDBAccessObject {
 
 	/**
 	 * Generate a valid memcached key for caching the object's data.
+	 * @param WANObjectCache $cache
 	 * @return string
 	 */
-	protected function getCacheKey() {
-		return "centralauth-user-" . md5( $this->mName );
+	protected function getCacheKey( WANObjectCache $cache ) {
+		return $cache->makeGlobalKey( 'centralauth-user', md5( $this->mName ) );
 	}
 
 	/**
