17cb710a4e2b5deb4e3abaec278c925f816fc5db
rxy <wikitech.rxy@rxy.jp>
2018-08-05 17:57:59 +0900

    Restrict global actions to specified wikis
    Bug: T194232
    Change-Id: I20fd26e2915e43a2fc3de70d16c1204c8d110afb

--- includes/specials/SpecialGlobalRenameQueue.php
+++ includes/specials/SpecialGlobalRenameQueue.php
@@ -50,6 +50,19 @@ class SpecialGlobalRenameQueue extends SpecialPage {
 		return true;
 	}
 
+	public function checkPermissions() {
+		parent::checkPermissions();
+
+		// T194232
+		if ( !CentralAuthUtils::isPermittedGlobalActionWiki() ) {
+			throw new ErrorPageError(
+				'permissionserrors',
+				'centralauth-globalactiondenied',
+				$this->getUser()->getName()
+			);
+		}
+	}
+
 	/**
 	 * @param string $par Subpage string if one was specified
 	 */
