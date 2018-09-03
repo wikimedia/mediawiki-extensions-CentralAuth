24e8db8c97e44571851c16327d98929945d7a18c
MR70 <mr7060@mail.com>
2018-09-03 15:54:31 +0000

    Display global rename log in "Previous global account changes"
    Bug: T202262
    Change-Id: I2faa7b80c11c60593471863d9b2e54bce2ec6043

--- includes/specials/SpecialCentralAuth.php
+++ includes/specials/SpecialCentralAuth.php
@@ -850,22 +850,28 @@ class SpecialCentralAuth extends SpecialPage {
 	private function showLogExtract() {
 		$user = $this->mGlobalUser->getName();
 		$title = Title::newFromText( MWNamespace::getCanonicalName( NS_USER ) . ":{$user}@global" );
+		$caTitle = SpecialPage::getTitleValueFor( 'CentralAuth', $user );
 		if ( !$title ) {
 			// Don't fatal even if a Title couldn't be generated
 			// because we've invalid usernames too :/
 			return;
 		}
-		$logTypes = [ 'globalauth' ];
+		$logTypes = [ 'globalauth', 'gblrename' ];
 		if ( $this->mCanOversight ) {
 			$logTypes[] = 'suppress';
 		}
 		$text = '';
+		$dbr = wfGetDB( DB_REPLICA );
 		$numRows = LogEventsList::showLogExtract(
 			$text,
 			$logTypes,
-			$title->getPrefixedText(),
 			'',
-			[ 'showIfEmpty' => true ] );
+			'',
+			[ 'showIfEmpty' => true,
+				'conds' => [ 'log_title ='. $dbr->addQuotes( $title->getText() )
+				.'OR log_title ='. $dbr->addQuotes( $caTitle->getText() ) ]
+			]
+		);
 		if ( $numRows ) {
 			$this->getOutput()->addHTML( Xml::fieldset(
 				$this->msg( 'centralauth-admin-logsnippet' )->text(),
