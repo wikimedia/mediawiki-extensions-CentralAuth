136213b09dfd582b5e9bff0acdbe963ea7f38e97
James D. Forrester <jforrester@wikimedia.org>
2018-09-05 10:03:48 -0700

    build: Wrap anon-set in a closure
    Change-Id: Ic5a62b95f3a9fa2e68131a7c5458a54358a0c9c7

--- modules/inline/anon-set.js
+++ modules/inline/anon-set.js
@@ -1,9 +1,13 @@
-var t = new Date();
-// Set CentralAuthAnon to 1 day in the future
-t.setTime( t.getTime() + 86400000 );
-try {
-	localStorage.setItem( 'CentralAuthAnon', t.getTime() );
-} catch ( e ) {
-	/* When no localStorage or when QUOTA reached */
-	document.cookie = 'CentralAuthAnon=1; expires=' + t.toGMTString() + '; path=/';
-}
+( function () {
+
+	var t = new Date();
+	// Set CentralAuthAnon to 1 day in the future
+	t.setTime( t.getTime() + 86400000 );
+	try {
+		localStorage.setItem( 'CentralAuthAnon', t.getTime() );
+	} catch ( e ) {
+		/* When no localStorage or when QUOTA reached */
+		document.cookie = 'CentralAuthAnon=1; expires=' + t.toGMTString() + '; path=/';
+	}
+
+}() );
