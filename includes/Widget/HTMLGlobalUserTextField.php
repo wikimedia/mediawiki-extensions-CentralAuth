de74cb380c67ae51b700f0826e8528c9131163c5
Jayprakash12345 <0freerunning@gmail.com>
2018-10-31 12:49:37 +0530

    Introduce HTMLGlobalUserTextField
    Bug: T204411
    Change-Id: Ie02c9df8e1189472d1d94c3d293ddefa669ed8b4

---
+++ includes/Widget/HTMLGlobalUserTextField.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Implements a text input field for user names.
+ * Automatically auto-completes if using the OOUI display format.
+ *
+ * @since 1.33
+ */
+
+class HTMLGlobalUserTextField extends HTMLUserTextField {
+
+	public function __construct( $params ) {
+		parent::__construct( $params );
+	}
+
+	protected function getInputWidget( $params ) {
+		return new GlobalUserInputWidget( $params );
+	}
+
+	protected function getOOUIModules() {
+		return [ 'ext.widgets.GlobalUserInputWidget' ];
+	}
+}
