b1002a6c6e108c2afeef71bfe27b8084e0d4ec42
MarcoAurelio <maurelio@tools.wmflabs.org>
2019-02-10 19:44:21 +0100

    Split api.* messages to a separate folder
    Bug: T214983
    Bug: T189982
    Change-Id: Id33af49b0d80ca5a0b324d988b2edfb1e0c5d67a

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,14 @@
 /* eslint-env node */
 module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
-		banana: {
-			all: 'i18n/'
-		},
+		banana: conf.MessagesDirs,
 		jsonlint: {
 			all: [
 				'**/*.json',
