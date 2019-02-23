fc380228bc2e2ea89c8745866a3974f58fc87aab
Umherirrender <umherirrender_de.wp@web.de>
2019-02-23 22:56:46 +0100

    Remove call of undeclared methods in Special:MergeAccount
    Change-Id: I21df478668b96407c4c534e9d02903909c5968c0

--- includes/specials/SpecialMergeAccount.php
+++ includes/specials/SpecialMergeAccount.php
@@ -103,12 +103,6 @@ class SpecialMergeAccount extends SpecialPage {
 			case "attach":
 				$this->doAttachMerge();
 				break;
-			case "remove":
-				$this->doUnattach(); // FIXME: Method is undefined
-				break;
-			default:
-				$this->invalidAction(); // FIXME: Method is undefined
-				break;
 			}
 			return;
 		}
