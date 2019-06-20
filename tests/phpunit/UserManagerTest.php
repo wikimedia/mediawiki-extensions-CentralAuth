ccd65595b18f25315e98a49f9a8f961556e34529
David Barratt <dbarratt@wikimedia.org>
2019-06-20 13:24:51 -0400

    Add blocking details for partial blocks to Special:CentralAuth.
    The Special:CentralAuth page does not give users details on the nature of
    the blocks a user has. To give a better picture, the user should be shown the
    details of the block (where possible).
    
    Moved the block parameter formatting to a new method:
    SpecialCentralAuth::formatBlockParams().
    
    Moved CentralAuthUser::localUserData() to a new CentralAuthUserManager service
    as UserManager::getLocalUserData().
    
    Bug: T200938
    Change-Id: Ia6ab39ef10a423e9709ae626f333765de30cdc9e

---
+++ tests/phpunit/UserManagerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace MediaWiki\CentralAuth\Tests\Block;
+
+use MediaWiki\Block\BlockRestrictionStore;
+use MediaWiki\CentralAuth\UserManager;
+use Psr\Log\LoggerInterface;
+use Wikimedia\Rdbms\DBConnRef;
+use Wikimedia\Rdbms\ILBFactory;
+use Wikimedia\Rdbms\ILoadBalancer;
+
+class UserManagerTest extends \MediaWikiTestCase {
+
+	/**
+	 * @covers MediaWiki\CentralAuth\UserManager::getLocalUserData()
+	 */
+	public function testGetLocalUserData() {
+		$wikiId = 'test';
+
+		$language = $this->createMock( \Language::class );
+
+		$db = $this->createMock( DBConnRef::class );
+		$db->method( 'selectRow' )
+			->willReturn( new \stdClass );
+		$db->method( 'select' )
+			->willReturn( [] );
+
+		$loadBalancer = $this->createMock( ILoadBalancer::class );
+		$loadBalancer->method( 'getConnectionRef' )
+			->willReturn( $db );
+
+		$lbFactory = $this->createMock( ILBFactory::class );
+		$lbFactory->method( 'getMainLB' )
+			->with( $wikiId )
+			->willReturn( $loadBalancer );
+
+		$actorMigraiton = $this->createMock( \ActorMigration::class );
+		$actorMigraiton->method( 'getWhere' )
+			->willReturn( [
+				'orconds' => [],
+			] );
+
+		$commentStore = $this->createMock( \CommentStore::class );
+		$commentStore->method( 'getJoin' )
+			->willReturn( [
+				'tables' => [],
+				'fields' => [],
+			] );
+
+		$logger = $this->createMock( LoggerInterface::class );
+		$blockRestrictions = $this->createMock( BlockRestrictionStore::class );
+
+		$userManager = new UserManager(
+			$language,
+			$lbFactory,
+			$actorMigraiton,
+			$commentStore,
+			$logger,
+			$blockRestrictions
+		);
+
+		$data = $userManager->getLocalUserData( $wikiId, 'Test' );
+		$this->assertInternalType( 'array', $data );
+		$this->assertArrayHasKey( 'wiki', $data );
+		$this->assertSame( $wikiId, $data['wiki'] );
+	}
+}
