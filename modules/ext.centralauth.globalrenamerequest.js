73a74e7aca88a2064ad9057aa42487dd8a671aa3
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-08-05 19:00:00 +0200

    [wip] Create "vanish" option in Special:GlobalRenameRequest
    Bug: T195888
    Change-Id: I85efdfbcb2950bd8d902740ab6b613fb8f51c792

---
+++ modules/ext.centralauth.globalrenamerequest.js
@@ -0,0 +1,14 @@
+( function () {
+	'use strict';
+	/* eslint-disable no-alert, no-jquery/no-global-selector */
+	$( function () {
+		$('#mw-renamerequest-vanish>input').change(function() {
+			if (this.checked) {
+				$('.mw-globalrenamerequest-newname').hide();
+			} else {
+				$('.mw-globalrenamerequest-newname').show();
+			}
+		});
+	} );
+	/* eslint-enable no-alert, no-jquery/no-global-selector */
+}() );
