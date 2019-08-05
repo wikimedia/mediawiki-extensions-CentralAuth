c2485f8410be7fb2cb571344be61744b474513e6
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-08-05 19:00:00 +0200

    Create "vanish" option in Special:GlobalRenameRequest
    Bug: T195888
    Change-Id: I85efdfbcb2950bd8d902740ab6b613fb8f51c792

--- includes/specials/SpecialGlobalRenameRequest.php
+++ includes/specials/SpecialGlobalRenameRequest.php
@@ -83,6 +83,7 @@ class SpecialGlobalRenameRequest extends FormSpecialPage {
 			default:
 				// Request form
 				$out = $this->getOutput();
+				$out->addModules( 'ext.centralauth.globalrename' );
 				$user = $this->getUser();
 				$wiki = $this->isGlobalUser() ? null : wfWikiID();
 
@@ -148,19 +149,30 @@ class SpecialGlobalRenameRequest extends FormSpecialPage {
 				'type'          => 'info',
 			],
 			'newname' => [
-				'cssclass'     => 'mw-globalrenamerequest-field',
+				# mw-globalrenamerequest-newname added for ext.centralauth.globalrenamerequest
+				# see T195888#5393782
+				'cssclass'     => 'mw-globalrenamerequest-field mw-globalrenamerequest-newname',
 				'csshelpclass' => 'mw-globalrenamerequest-help',
 				'default'      => $this->getRequest()->getVal( 'newname', $this->par ),
 				'id'            => 'mw-renamerequest-newname',
 				'label-message' => 'globalrenamerequest-newname-label',
 				'name'          => 'newname',
-				'required'      => true,
+				# newname isn't required only if vanish is checked, see T195888#5393782 for context
+				'required'      => false,
 				'type'          => 'text',
 				'help-message'  => [
 					'globalrenamerequest-newname-help',
 					$this->suggestedUsername(),
 				],
 				'validation-callback' => [ $this, 'validateNewname' ],
+			],
+			'vanish' => [
+				'cssclass'      => 'mw-globalrenamerequest-field',
+				'csshelpclass'  => 'mw-globalrenamerequest-help',
+				'default'       => false,
+				'id'            => 'mw-renamerequest-vanish',
+				'label-message' => 'globalrenamerequest-vanish-label',
+				'type'          => 'check',
 			],
 		];
 
@@ -233,6 +245,9 @@ class SpecialGlobalRenameRequest extends FormSpecialPage {
 			// Not submitted yet
 			return true;
 		}
+		if ( $alldata['vanish'] === true ) {
+			return true;
+		}
 		$check = GlobalRenameRequest::isNameAvailable( $value );
 		return $check->isGood() ? true : $check->getMessage();
 	}
@@ -260,7 +275,11 @@ class SpecialGlobalRenameRequest extends FormSpecialPage {
 	public function onSubmit( array $data ) {
 		$wiki = $this->isGlobalUser() ? null : wfWikiID();
 		$reason = $data['reason'] ?? null;
-		$safeName = User::getCanonicalName( $data['newname'], 'creatable' );
+		if ( $data['vanish'] ) {
+			$safeName = uniqid( "Renamed user " );
+		} else {
+			$safeName = User::getCanonicalName( $data['newname'], 'creatable' );
+		}
 
 		$request = new GlobalRenameRequest;
 		$request->setName( $this->getUser()->getName() );
