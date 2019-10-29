cc59ee6dd78f1318f559cd838cc39fa3f0d2b0b1
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-10-29 15:19:49 +0100

    [wip] Make notification about renamed user account an Echo notification
    Bug: T231395
    Change-Id: Icf68fe4eba113d3141bc83ad01a7769dd503bac9

--- .phan/config.php
+++ .phan/config.php
@@ -14,6 +14,7 @@ $cfg['directory_list'] = array_merge(
 		'../../extensions/Renameuser',
 		'../../extensions/TitleBlacklist',
 		'../../extensions/UserMerge',
+		'../../extensions/Echo',
 	]
 );
 
@@ -28,6 +29,7 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 		'../../extensions/Renameuser',
 		'../../extensions/TitleBlacklist',
 		'../../extensions/UserMerge',
+		'../../extensions/Echo',
 	]
 );
 
