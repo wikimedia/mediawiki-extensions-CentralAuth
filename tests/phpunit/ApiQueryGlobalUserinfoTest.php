a92f8c182b247f0468807cb3c41a8b52152ab548
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-11-06 10:06:46 +0100

    API: Allow multiple values in 'user' and 'id' parameters to globaluserinfo
    The action=query&meta=globaluserinfo endpoint now supports multiple values
    in its guiuser and guiid parameters.
    
    Bug: T237505
    Change-Id: I1405f3df5933ff451f5cc8aeaf92093ae265cc0d

---
+++ tests/phpunit/ApiQueryGlobalUserinfoTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @group API
+ * @group Database
+ * @group medium
+ */
+class ApiQueryGlobalUserinfoTest extends ApiTestCase {
+	private $user = null;
+
+	protected function setUp() {
+		parent::setUp();
+		$this->user = $this->getTestUser()->getUser();
+	}
+
+	private function getParams( $additional = [] ) {
+		$base = [
+			'action' => 'query',
+			'meta' => 'globaluserinfo',
+			'guiprop' => 'editcount|groups|merged|rights|unattached'
+		];
+		return array_merge( $base, $additional );
+	}
+
+	/**
+	 * @covers ApiQueryGlobalUserInfo::execute
+	 */
+	public function testExecuteOneUser() {
+		$res = $this->doApiRequest(
+			$this->getParams( [
+				'guiuser' => $this->user->getName()
+			] )
+		);
+		$centralUser = CentralAuthUser::getInstance( $this->user );
+		$data = $res['query']['globaluserinfo'][$this->user->getName()];
+		$this->assertSame( $centralUser->getHomeWiki(), $data['home'] );
+		$this->assertSame( $centralUser->getId(), $data['id'] );
+		$this->assertSame( $centralUser->getRegistration(), $data['registration'] );
+		$this->assertSame( $centralUser->getGlobalEditCount(), $data['editcount'] );
+	}
+}
