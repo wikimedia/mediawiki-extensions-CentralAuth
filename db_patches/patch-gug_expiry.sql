f87f46c1d2037d3ddee01e9d0c59c4d8d3198bbd
Melos <webmelos@gmail.com>
2020-02-17 12:23:32 +0100

    Add expiry column in global_user_groups table
    Add a new gug_expiry column to the global_user_groups table,
    a timestamp giving a date when the global user group expires.
    Set default value to null for permanent groups.
    
    Bug: T245413
    Change-Id: I866f7917fb4dedbd7b52c67d9ff3932ccc913b1a

---
+++ db_patches/patch-gug_expiry.sql
@@ -0,0 +1,5 @@
+-- Add expiry column in global_user_groups table
+
+ALTER TABLE /*_*/global_user_groups
+  ADD COLUMN gug_expiry varbinary(14) NULL default NULL,
+  ADD INDEX gug_expiry (gug_expiry);
