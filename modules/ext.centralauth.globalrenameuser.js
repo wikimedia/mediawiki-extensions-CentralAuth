10008609be1dc299e0cc33f49104ca2b4cd6b997
Ed Sanders <esanders@wikimedia.org>
2020-02-22 16:20:52 +0000

    eslint: Add /mediawiki rules
    Change-Id: Ic84607cb3529ae7117ba91d2c440f778efef2000

--- modules/ext.centralauth.globalrenameuser.js
+++ modules/ext.centralauth.globalrenameuser.js
@@ -1,14 +1,12 @@
 $( function () {
 	// Confirm renames
 	mw.util.$content.find( 'form' ).first().on( 'submit', function () {
-		var msgKey;
-		if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'GlobalUserMerge' ) {
-			msgKey = 'centralauth-usermerge-confirm';
-		} else {
-			msgKey = 'centralauth-rename-confirm';
-		}
 		// TODO: Convert this to OOUI.
 		// eslint-disable-next-line no-alert
-		return confirm( mw.msg( msgKey ) );
+		return confirm( mw.msg(
+			mw.config.get( 'wgCanonicalSpecialPageName' ) === 'GlobalUserMerge' ?
+				'centralauth-usermerge-confirm' :
+				'centralauth-rename-confirm'
+		) );
 	} );
 } );
