43b7ffe3eec8d6f86d7a5f076e322a21abfbb68a
Reedy <reedy@wikimedia.org>
2020-02-22 17:15:04 +0000

    Make CentralAuth schema patches consistent to canonical file
    Bug: T245924
    Change-Id: I94559918ceb0bf12ccc25211e25160c5c62c64f8

--- db_patches/patch-gu_cas_token.sql
+++ db_patches/patch-gu_cas_token.sql
@@ -1,1 +1,1 @@
-ALTER TABLE globaluser ADD COLUMN gu_cas_token integer unsigned NOT NULL default 1;
+ALTER TABLE /*_*/globaluser ADD COLUMN gu_cas_token integer unsigned NOT NULL default 1;
--- db_patches/patch-gu_hidden.sql
+++ db_patches/patch-gu_hidden.sql
@@ -3,15 +3,15 @@
 -- get a list of hidden or locked users.
 -- Victor Vasiliev, January 2010.
 
-ALTER TABLE globaluser
+ALTER TABLE /*_*/globaluser
 	MODIFY COLUMN gu_hidden VARBINARY(255) NOT NULL DEFAULT '';
 
 -- Not hidden (was 0)
-UPDATE globaluser SET gu_hidden = '' WHERE gu_hidden = '0';
+UPDATE /*_*/globaluser SET gu_hidden = '' WHERE gu_hidden = '0';
 -- Hidden from public lists, but remains visible in Special:CentralAuth log
-UPDATE globaluser SET gu_hidden = 'lists' WHERE gu_hidden = '1';
+UPDATE /*_*/globaluser SET gu_hidden = 'lists' WHERE gu_hidden = '1';
 -- There's also "suppressed" level, which wasn't used before this schema change
 
-ALTER TABLE globaluser
-	ADD INDEX gu_locked( gu_name(255), gu_locked ),
-	ADD INDEX gu_hidden( gu_name(255), gu_hidden(255) );
+ALTER TABLE /*_*/globaluser
+	ADD INDEX /*i*/gu_locked(gu_name(255), gu_locked),
+	ADD INDEX /*i*/gu_hidden(gu_name(255), gu_hidden(255));
