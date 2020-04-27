723fc7de68086718e482ee773116a2e8c5d8bc7e
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-04-27 23:57:59 +0200

    tests: Introduce basic ApiQueryGlobaluserinfoTest
    Change-Id: I92074475c4fde242aade56f24a855352717bf6b1

---
+++ tests/phpunit/ApiQueryGlobalUserinfoTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @group API
+ * @group Database
+ * @group medium
+ */
+class ApiQueryGlobalUserinfoTest extends CentralAuthUsingDatabaseApiTestCase {
+	private $usernames = [];
+
+	protected function setUp() : void {
+		parent::setUp();
+		for ( $i = 0; $i < 5; $i++ ) {
+			$username = 'GlobalUser ' . $i;
+			$user = new CentralAuthTestUser(
+				$username,
+				'GUP@ssword',
+				[ 'gu_id' => '1001' ],
+				[
+					[ wfWikiID(), 'primary' ],
+					[ 'enwiki', 'primary' ],
+					[ 'dewiki', 'login' ],
+					[ 'metawiki', 'password' ],
+				]
+			);
+			$user->save( $this->db );
+			$this->usernames[] = $username;
+		}
+	}
+
+	private function getParams( $additional = [] ) {
+		$base = [
+			'action' => 'query',
+			'meta' => 'globaluserinfo',
+			'guiprop' => 'editcount|groups|merged|rights|unattached'
+		];
+		return array_merge( $base, $additional );
+	}
+
+	/**
+	 * @covers ApiQueryGlobalUserInfo::execute
+	 */
+	public function testExecuteOneUser() {
+		$centralUser = CentralAuthUser::getInstanceByName( $this->usernames[0] );
+		$res = $this->doApiRequest(
+			$this->getParams( [
+				'guiuser' => $centralUser->getName()
+			] )
+		);
+		$data = $res[0]['query']['globaluserinfo'];
+		$this->assertSame( $centralUser->getHomeWiki(), $data['home'] );
+		$this->assertSame( $centralUser->getId(), $data['id'] );
+		$this->assertSame( $centralUser->getRegistration(), $data['registration'] );
+		$this->assertSame( $centralUser->getGlobalEditCount(), $data['editcount'] );
+	}
+}
