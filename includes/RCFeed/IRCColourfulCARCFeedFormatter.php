f2daf315e55aa341d0d99e20baad3f0f405e8869
Taavi Väänänen <hi@taavi.wtf>
2021-12-04 18:38:01 +0200

    Namespace RCFeed
    Backwards compability is preserved for the formatter classes so that
    usage in operations/mediawiki-config can be changed easily.
    
    Bug: T297047
    Change-Id: I735dfda80f98274aad42a8164ba6818cdc074cc5

--- includes/RCFeed/IRCColourfulCARCFeedFormatter.php
+++ includes/RCFeed/IRCColourfulCARCFeedFormatter.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,15 +18,31 @@
  * @file
  */
 
+namespace MediaWiki\Extension\CentralAuth\RCFeed;
+
+use IRCColourfulRCFeedFormatter;
+use Title;
+
 /**
- * Interface for CentralAuth RC feed formatters
+ * Generates a colourful notification intended for humans on IRC.
  */
-interface CARCFeedFormatter {
+class IRCColourfulCARCFeedFormatter implements CARCFeedFormatter {
 	/**
 	 * Generate an IRC line corresponding to user unification/creation
 	 * @param Title $userpage
 	 * @param string $wikiID
 	 * @return string
 	 */
-	public function getLine( $userpage, $wikiID );
+	public function getLine( $userpage, $wikiID ) {
+		$title = IRCColourfulRCFeedFormatter::cleanupForIRC( $userpage->getPrefixedText() );
+		$wikiID = IRCColourfulRCFeedFormatter::cleanupForIRC( $wikiID );
+		$url = $userpage->getCanonicalURL();
+		$user = IRCColourfulRCFeedFormatter::cleanupForIRC( $userpage->getText() );
+		# see http://www.irssi.org/documentation/formats for some colour codes. prefix is \003,
+		# no colour (\003) switches back to the term default
+		return "\00314[[\00307$title\00314]]\0034@$wikiID\00310 " .
+		"\00302$url\003 \0035*\003 \00303$user\003 \0035*\003\n";
+	}
 }
+
+class_alias( IRCColourfulCARCFeedFormatter::class, 'IRCColourfulCARCFeedFormatter' );
--- includes/RCFeed/JSONCARCFeedFormatter.php
+++ includes/RCFeed/JSONCARCFeedFormatter.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,24 +18,27 @@
  * @file
  */
 
-/**
- * Generates a colourful notification intended for humans on IRC.
- */
-class IRCColourfulCARCFeedFormatter implements CARCFeedFormatter {
+namespace MediaWiki\Extension\CentralAuth\RCFeed;
+
+use FormatJson;
+use Title;
+
+class JSONCARCFeedFormatter implements CARCFeedFormatter {
 	/**
-	 * Generate an IRC line corresponding to user unification/creation
+	 * Generates a user unification/creation entry that can be easily interpreted by a machine.
 	 * @param Title $userpage
 	 * @param string $wikiID
 	 * @return string
 	 */
 	public function getLine( $userpage, $wikiID ) {
-		$title = IRCColourfulRCFeedFormatter::cleanupForIRC( $userpage->getPrefixedText() );
-		$wikiID = IRCColourfulRCFeedFormatter::cleanupForIRC( $wikiID );
-		$url = $userpage->getCanonicalURL();
-		$user = IRCColourfulRCFeedFormatter::cleanupForIRC( $userpage->getText() );
-		# see http://www.irssi.org/documentation/formats for some colour codes. prefix is \003,
-		# no colour (\003) switches back to the term default
-		return "\00314[[\00307$title\00314]]\0034@$wikiID\00310 " .
-		"\00302$url\003 \0035*\003 \00303$user\003 \0035*\003\n";
+		$packet = [
+			'title' => $userpage->getPrefixedText(),
+			'wikiid' => $wikiID,
+			'user' => $userpage->getText(),
+			'url' => $userpage->getCanonicalURL()
+		];
+		return FormatJson::encode( $packet );
 	}
 }
+
+class_alias( JSONCARCFeedFormatter::class, 'JSONCARCFeedFormatter' );
