b49cf5dded73e4181d46c111c8e159ba671518bd
Matěj Suchánek <matejsuchanek97@gmail.com>
2021-12-12 13:24:24 +0100

    Migrate to modern logging system (last part)
    This migrates only very old entries, which were last
    inserted in 2009. There isn't large benefit from
    migrating them, so if the risks and translators' work
    outweight better i18n and post-insert transformation
    (T139495), we don't have to do this and can close all
    the tasks right away.
    
    Bug: T41045
    Bug: T138728
    Bug: T153322
    Change-Id: Ifc4a2f207b051bcb68d61bc64613e038c97fd97f

--- includes/LogFormatter/PermissionChangeLogFormatter.php
+++ includes/LogFormatter/PermissionChangeLogFormatter.php
@@ -6,7 +6,11 @@ use LogFormatter;
 
 /**
  * Handles the following log types:
+ * - gblrights/groupperms
  * - gblrights/groupprms2
+ * Messages used here:
+ * - logentry-gblrights-groupperms
+ * - logentry-gblrights-groupprms2
  */
 class PermissionChangeLogFormatter extends LogFormatter {
 
@@ -17,20 +21,30 @@ class PermissionChangeLogFormatter extends LogFormatter {
 	}
 
 	/** @inheritDoc */
-	protected function getMessageKey() {
-		return 'logentry-gblrights-groupprms2';
-	}
-
 	protected function extractParameters() {
-		if ( $this->entry->isLegacy() ) {
-			return parent::extractParameters();
-		}
+		if ( $this->entry->getSubtype() === 'groupprms2' ) {
+			if ( $this->entry->isLegacy() ) {
+				return parent::extractParameters();
+			}
 
-		[ 'addRights' => $addRights, 'removeRights' => $removeRights ] = $this->entry->getParameters();
-		return [
-			3 => $this->makeRightsList( $addRights ),
-			4 => $this->makeRightsList( $removeRights ),
-		];
+			[ 'addRights' => $addRights, 'removeRights' => $removeRights ] = $this->entry->getParameters();
+			return [
+				3 => $this->makeRightsList( $addRights ),
+				4 => $this->makeRightsList( $removeRights ),
+			];
+		} else {
+			$params = parent::extractParameters();
+			$oldRights = $params[3] === '(none)' ? [] : explode( ', ', $params[3] );
+			$newRights = $params[4] === '(none)' ? [] : explode( ', ', $params[4] );
+			return [
+				// False positive
+				// @phan-suppress-next-line SecurityCheck-XSS
+				3 => $this->makeRightsList( $oldRights ),
+				// False positive
+				// @phan-suppress-next-line SecurityCheck-XSS
+				4 => $this->makeRightsList( $newRights ),
+			];
+		}
 	}
 
 }
