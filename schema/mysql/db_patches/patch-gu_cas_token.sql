28c891a3162ee758ea328eb3b611f9a58ea26223
Umherirrender <umherirrender_de.wp@web.de>
2022-06-11 00:20:58 +0200

    Bump minimum required version for upgrade to 1.31
    Remove sql patches, mention the gerrit change to check version
    - patch-gu_cas_token.sql - I04c7b1476bd460933a6391891c811c2166a88361
    - patch-lu_local_id.sql - I5dd361c0ded1c69dd0109c7cfafdcefe0ba5a94b
    - patch-lu_wiki.sql - 1896fc65bb03e30bd1a04f8366a19ba5125abeea
    
    Change-Id: I8e41bea00edac1b77ee79754c74044d2b4c16125

--- schema/mysql/db_patches/patch-gu_cas_token.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE /*_*/globaluser ADD COLUMN gu_cas_token integer unsigned NOT NULL default 1;
--- schema/mysql/db_patches/patch-lu_wiki.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE /*_*/localnames CHANGE ln_dbname ln_wiki varchar(255) binary not null;
-ALTER TABLE /*_*/localuser CHANGE lu_dbname lu_wiki varchar(255) binary not null;
