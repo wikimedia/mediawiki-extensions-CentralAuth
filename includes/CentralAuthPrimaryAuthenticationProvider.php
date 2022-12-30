90c62bccfd65de780a245e4baba079cd4f6c19bb
Taavi Väänänen <hi@taavi.wtf>
2022-12-30 20:57:02 +0200

    Fix authentication provider ID during auto creation
    The provider ID comes from extension.json, not the class name directly.
    This fact was missed when namespacing the authentication provider
    classes.
    
    Change-Id: I92d2da0abe5f468d7311860918b650df508ea2b1

--- includes/CentralAuthPrimaryAuthenticationProvider.php
+++ includes/CentralAuthPrimaryAuthenticationProvider.php
@@ -49,6 +49,8 @@ use WikiMap;
 class CentralAuthPrimaryAuthenticationProvider
 	extends AbstractPasswordPrimaryAuthenticationProvider
 {
+	/** @var string The internal ID of this provider. */
+	public const ID = 'CentralAuthPrimaryAuthenticationProvider';
 
 	/** @var CentralAuthDatabaseManager */
 	private $databaseManager;
