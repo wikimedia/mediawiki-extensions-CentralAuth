wikimedia/mediawiki-extensions-CentralAuth

commit bb89a133615563ec78842d5a500987f7aa68354d

Add a GlobalRenameFactory
Adds a new factory to construct GlobalRenameUser objects so that we can
use DI and otherwise clean up the code.

The name is GlobalRenameFactory, not GlobalRenameUserFactory, on
purpose, so that it can be extended to construct other GlobalRename
related objects later on.

Change-Id: I973b0a092e58b720a5e9f9cf9214c82bfbb61640

includes/GlobalRename/GlobalRenameFactory.php
@@ -1,0 +1,94 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\Extension\CentralAuth\GlobalRename;
+
+use InvalidArgumentException;
+use MediaWiki\Extension\CentralAuth\CentralAuthDatabaseManager;
+use MediaWiki\Extension\CentralAuth\User\CentralAuthAntiSpoofManager;
+use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
+use MediaWiki\JobQueue\JobQueueGroupFactory;
+use MediaWiki\User\UserFactory;
+use MediaWiki\User\UserIdentity;
+use MediaWiki\User\UserRigorOptions;
+
+/**
+ * Constructs global renaming related command objects.
+ *
+ * @author Taavi Väänänen <hi@taavi.wtf>
+ */
+class GlobalRenameFactory {
+
+	private JobQueueGroupFactory $jobQueueGroupFactory;
+	private UserFactory $userFactory;
+
+	private CentralAuthAntiSpoofManager $caAntiSpoofManager;
+	private CentralAuthDatabaseManager $databaseManager;
+
+	/**
+	 * @param JobQueueGroupFactory $jobQueueGroupFactory
+	 * @param UserFactory $userFactory
+	 * @param CentralAuthAntiSpoofManager $caAntiSpoofManager
+	 * @param CentralAuthDatabaseManager $databaseManager
+	 */
+	public function __construct(
+		JobQueueGroupFactory $jobQueueGroupFactory,
+		UserFactory $userFactory,
+		CentralAuthAntiSpoofManager $caAntiSpoofManager,
+		CentralAuthDatabaseManager $databaseManager
+	) {
+		$this->jobQueueGroupFactory = $jobQueueGroupFactory;
+		$this->userFactory = $userFactory;
+		$this->caAntiSpoofManager = $caAntiSpoofManager;
+		$this->databaseManager = $databaseManager;
+	}
+
+	public function newGlobalRenameUser(
+		UserIdentity $performer,
+		CentralAuthUser $userToRename,
+		string $newName
+	): GlobalRenameUser {
+		$userOld = $this->userFactory->newFromName( $userToRename->getName() );
+		if ( !$userOld ) {
+			throw new InvalidArgumentException( 'Name of the old user is not valid' );
+		}
+
+		$userNew = $this->userFactory->newFromName( $newName, UserRigorOptions::RIGOR_CREATABLE );
+		if ( !$userNew ) {
+			throw new InvalidArgumentException( 'Name old the new user is not creatable' );
+		}
+
+		return new GlobalRenameUser(
+			$performer,
+			$userOld,
+			$userToRename,
+			$userNew,
+			// TODO: this is only used for caching, maybe we can create a Factory service
+			// for CAUsers and related caching and inject that instead
+			CentralAuthUser::getInstance( $userNew ),
+			new GlobalRenameUserStatus( $newName ),
+			$this->jobQueueGroupFactory,
+			new GlobalRenameUserDatabaseUpdates( $this->databaseManager ),
+			new GlobalRenameUserLogger( $performer ),
+			$this->caAntiSpoofManager
+		);
+	}
+
+}

includes/GlobalRename/GlobalRenameUser.php
@@ -3,8 +3,8 @@
 namespace MediaWiki\Extension\CentralAuth\GlobalRename;
 
 use Job;
-use MediaWiki\Extension\CentralAuth\CentralAuthServices;
 use MediaWiki\Extension\CentralAuth\GlobalRename\LocalRenameJob\LocalRenameUserJob;
+use MediaWiki\Extension\CentralAuth\User\CentralAuthAntiSpoofManager;
 use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
 use MediaWiki\JobQueue\JobQueueGroupFactory;
 use MediaWiki\User\UserIdentity;
@@ -61,10 +61,12 @@ class GlobalRenameUser {
 	 */
 	private $logger;
 
+	private CentralAuthAntiSpoofManager $caAntiSpoofManager;
+
 	/**
 	 * @var array|null
 	 */
-	private $session;
+	private ?array $session = null;
 
 	/**
 	 * @param UserIdentity $performingUser
@@ -76,7 +78,7 @@ class GlobalRenameUser {
 	 * @param JobQueueGroupFactory $jobQueueGroupFactory
 	 * @param GlobalRenameUserDatabaseUpdates $databaseUpdates
 	 * @param GlobalRenameUserLogger $logger
-	 * @param array|null $session output of RequestContext::exportSession()
+	 * @param CentralAuthAntiSpoofManager $caAntiSpoofManager
 	 */
 	public function __construct(
 		UserIdentity $performingUser,
@@ -88,7 +90,7 @@ class GlobalRenameUser {
 		JobQueueGroupFactory $jobQueueGroupFactory,
 		GlobalRenameUserDatabaseUpdates $databaseUpdates,
 		GlobalRenameUserLogger $logger,
-		?array $session
+		CentralAuthAntiSpoofManager $caAntiSpoofManager
 	) {
 		$this->performingUser = $performingUser;
 		$this->oldUser = $oldUser;
@@ -99,7 +101,18 @@ class GlobalRenameUser {
 		$this->jobQueueGroupFactory = $jobQueueGroupFactory;
 		$this->databaseUpdates = $databaseUpdates;
 		$this->logger = $logger;
+		$this->caAntiSpoofManager = $caAntiSpoofManager;
+	}
+
+	/**
+	 * Set session data to use with this rename.
+	 *
+	 * @param array $session
+	 * @return GlobalRenameUser
+	 */
+	public function withSession( array $session ): GlobalRenameUser {
 		$this->session = $session;
+		return $this;
 	}
 
 	/**
@@ -137,9 +150,9 @@ class GlobalRenameUser {
 		);
 
 		// Update CA's AntiSpoof
-		// TODO: inject services
-		$spoof = CentralAuthServices::getAntiSpoofManager()->getSpoofUser( $this->newUser->getName() );
-		$spoof->update( $this->oldUser->getName() );
+		$this->caAntiSpoofManager
+			->getSpoofUser( $this->newUser->getName() )
+			->update( $this->oldUser->getName() );
 
 		// From this point on all code using CentralAuthUser
 		// needs to use the new username, except for

includes/Hooks/Handlers/SpecialPageInitHookHandler.php
@@ -61,6 +61,7 @@ class SpecialPageInitHookHandler implements
 					'CentralAuth.GlobalRenameRequestStore',
 					'JobQueueGroupFactory',
 					'CentralAuth.CentralAuthAntiSpoofManager',
+					'CentralAuth.GlobalRenameFactory',
 				],
 			];
 		}

includes/ServiceWiring.php
@@ -5,6 +5,7 @@ namespace MediaWiki\Extension\CentralAuth;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\Extension\CentralAuth\GlobalGroup\GlobalGroupLookup;
 use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameDenylist;
+use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameFactory;
 use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameRequestStore;
 use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserValidator;
 use MediaWiki\Extension\CentralAuth\User\CentralAuthAntiSpoofManager;
@@ -117,6 +118,14 @@ return [
 	},
 	'CentralAuth.GlobalGroupLookup' => static function ( MediaWikiServices $services ): GlobalGroupLookup {
 		return new GlobalGroupLookup(
+			CentralAuthServices::getDatabaseManager( $services )
+		);
+	},
+	'CentralAuth.GlobalRenameFactory' => static function ( MediaWikiServices $services ): GlobalRenameFactory {
+		return new GlobalRenameFactory(
+			$services->getJobQueueGroupFactory(),
+			$services->getUserFactory(),
+			CentralAuthServices::getAntiSpoofManager( $services ),
 			CentralAuthServices::getDatabaseManager( $services )
 		);
 	},

includes/Special/SpecialGlobalRenameQueue.php
@@ -30,12 +30,10 @@ use LogEventsList;
 use MailAddress;
 use MediaWiki\Extension\CentralAuth\CentralAuthDatabaseManager;
 use MediaWiki\Extension\CentralAuth\CentralAuthUIService;
+use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameFactory;
 use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameRequest;
 use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameRequestStore;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUser;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserDatabaseUpdates;
 use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserLogger;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserStatus;
 use MediaWiki\Extension\CentralAuth\GlobalRename\LocalRenameJob\LocalRenameUserJob;
 use MediaWiki\Extension\CentralAuth\User\CentralAuthAntiSpoofManager;
 use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
@@ -83,6 +81,8 @@ class SpecialGlobalRenameQueue extends SpecialPage {
 
 	private CentralAuthAntiSpoofManager $caAntiSpoofManager;
 
+	private GlobalRenameFactory $globalRenameFactory;
+
 	/** @var \Psr\Log\LoggerInterface */
 	private $logger;
 
@@ -100,7 +100,8 @@ class SpecialGlobalRenameQueue extends SpecialPage {
 		CentralAuthUIService $uiService,
 		GlobalRenameRequestStore $globalRenameRequestStore,
 		JobQueueGroupFactory $jobQueueGroupFactory,
-		CentralAuthAntiSpoofManager $caAntiSpoofManager
+		CentralAuthAntiSpoofManager $caAntiSpoofManager,
+		GlobalRenameFactory $globalRenameFactory
 	) {
 		parent::__construct( 'GlobalRenameQueue', 'centralauth-rename' );
 		$this->userNameUtils = $userNameUtils;
@@ -110,6 +111,7 @@ class SpecialGlobalRenameQueue extends SpecialPage {
 		$this->globalRenameRequestStore = $globalRenameRequestStore;
 		$this->jobQueueGroupFactory = $jobQueueGroupFactory;
 		$this->caAntiSpoofManager = $caAntiSpoofManager;
+		$this->globalRenameFactory = $globalRenameFactory;
 		$this->logger = LoggerFactory::getInstance( 'CentralAuth' );
 	}
 
@@ -642,20 +644,14 @@ class SpecialGlobalRenameQueue extends SpecialPage {
 			if ( $request->userIsGlobal() ) {
 				// Trigger a global rename job
 
-				$globalRenameUser = new GlobalRenameUser(
-					$this->getUser(),
-					$oldUser,
-					CentralAuthUser::getInstance( $oldUser ),
-					$newUser,
-					CentralAuthUser::getInstance( $newUser ),
-					new GlobalRenameUserStatus( $newUser->getName() ),
-					$this->jobQueueGroupFactory,
-					new GlobalRenameUserDatabaseUpdates( $this->databaseManager ),
-					new GlobalRenameUserLogger( $this->getUser() ),
-					$session
-				);
-
-				$status = $globalRenameUser->rename( $data );
+				$status = $this->globalRenameFactory
+					->newGlobalRenameUser(
+						$this->getUser(),
+						CentralAuthUser::getInstanceByName( $request->getName() ),
+						$request->getNewName()
+					)
+					->withSession( $session )
+					->rename( $data );
 			} else {
 				// If the user is local-only:
 				// * rename the local user using LocalRenameUserJob

includes/Special/SpecialGlobalRenameUser.php
@@ -4,19 +4,14 @@ namespace MediaWiki\Extension\CentralAuth\Special;
 
 use ExtensionRegistry;
 use FormSpecialPage;
-use MediaWiki\Extension\CentralAuth\CentralAuthDatabaseManager;
 use MediaWiki\Extension\CentralAuth\CentralAuthUIService;
 use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameDenylist;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUser;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserDatabaseUpdates;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserLogger;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserStatus;
+use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameFactory;
 use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserValidator;
 use MediaWiki\Extension\CentralAuth\User\CentralAuthAntiSpoofManager;
 use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
 use MediaWiki\Extension\TitleBlacklist\TitleBlacklist;
 use MediaWiki\Extension\TitleBlacklist\TitleBlacklistEntry;
-use MediaWiki\JobQueue\JobQueueGroupFactory;
 use Message;
 use Status;
 use Title;
@@ -49,19 +44,15 @@ class SpecialGlobalRenameUser extends FormSpecialPage {
 	 */
 	private $overrideTitleBlacklist = false;
 
-	/** @var JobQueueGroupFactory */
-	private $jobQueueGroupFactory;
-
 	private CentralAuthAntiSpoofManager $caAntiSpoofManager;
-
-	/** @var CentralAuthDatabaseManager */
-	private $databaseManager;
 
 	/** @var CentralAuthUIService */
 	private $uiService;
 
 	/** @var GlobalRenameDenylist */
 	private $globalRenameDenylist;
+
+	private GlobalRenameFactory $globalRenameFactory;
 
 	/** @var GlobalRenameUserValidator */
 	private $globalRenameUserValidator;
@@ -72,27 +63,24 @@ class SpecialGlobalRenameUser extends FormSpecialPage {
 	private const EDITCOUNT_THRESHOLD = 100000;
 
 	/**
-	 * @param JobQueueGroupFactory $jobQueueGroupFactory
 	 * @param CentralAuthAntiSpoofManager $caAntiSpoofManager
-	 * @param CentralAuthDatabaseManager $databaseManager
 	 * @param CentralAuthUIService $uiService
 	 * @param GlobalRenameDenylist $globalRenameDenylist
+	 * @param GlobalRenameFactory $globalRenameFactory
 	 * @param GlobalRenameUserValidator $globalRenameUserValidator
 	 */
 	public function __construct(
-		JobQueueGroupFactory $jobQueueGroupFactory,
 		CentralAuthAntiSpoofManager $caAntiSpoofManager,
-		CentralAuthDatabaseManager $databaseManager,
 		CentralAuthUIService $uiService,
 		GlobalRenameDenylist $globalRenameDenylist,
+		GlobalRenameFactory $globalRenameFactory,
 		GlobalRenameUserValidator $globalRenameUserValidator
 	) {
 		parent::__construct( 'GlobalRenameUser', 'centralauth-rename' );
-		$this->jobQueueGroupFactory = $jobQueueGroupFactory;
 		$this->caAntiSpoofManager = $caAntiSpoofManager;
-		$this->databaseManager = $databaseManager;
 		$this->uiService = $uiService;
 		$this->globalRenameDenylist = $globalRenameDenylist;
+		$this->globalRenameFactory = $globalRenameFactory;
 		$this->globalRenameUserValidator = $globalRenameUserValidator;
 	}
 
@@ -294,24 +282,15 @@ class SpecialGlobalRenameUser extends FormSpecialPage {
 
 		$this->newUsername = $data['newname'];
 		$this->oldUsername = $data['oldname'];
-		$oldUser = User::newFromName( $this->oldUsername );
-		$newUser = User::newFromName( $this->newUsername, 'creatable' );
 
-		$session = $this->getContext()->exportSession();
-		$globalRenameUser = new GlobalRenameUser(
-			$this->getUser(),
-			$oldUser,
-			CentralAuthUser::getInstance( $oldUser ),
-			$newUser,
-			CentralAuthUser::getInstance( $newUser ),
-			new GlobalRenameUserStatus( $newUser->getName() ),
-			$this->jobQueueGroupFactory,
-			new GlobalRenameUserDatabaseUpdates( $this->databaseManager ),
-			new GlobalRenameUserLogger( $this->getUser() ),
-			$session
-		);
-
-		return $globalRenameUser->rename( $data );
+		return $this->globalRenameFactory
+			->newGlobalRenameUser(
+				$this->getUser(),
+				CentralAuthUser::getInstanceByName( $this->oldUsername ),
+				$this->newUsername
+			)
+			->withSession( $this->getContext()->exportSession() )
+			->rename( $data );
 	}
 
 	public function onSuccess() {

maintenance/renameUsersMatchingPattern.php
@@ -5,13 +5,9 @@ namespace MediaWiki\Extension\CentralAuth\Maintenance;
 use Maintenance;
 use MediaWiki\Extension\CentralAuth\CentralAuthDatabaseManager;
 use MediaWiki\Extension\CentralAuth\CentralAuthServices;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUser;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserDatabaseUpdates;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserLogger;
-use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserStatus;
+use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameFactory;
 use MediaWiki\Extension\CentralAuth\GlobalRename\GlobalRenameUserValidator;
 use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
-use MediaWiki\JobQueue\JobQueueGroupFactory;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\User\TempUser\Pattern;
 use MediaWiki\User\UserFactory;
@@ -31,8 +27,7 @@ class RenameUsersMatchingPattern extends Maintenance {
 	/** @var UserFactory */
 	private $userFactory;
 
-	/** @var JobQueueGroupFactory */
-	private $jobQueueGroupFactory;
+	private GlobalRenameFactory $globalRenameFactory;
 
 	/** @var GlobalRenameUserValidator */
 	private $validator;
@@ -75,7 +70,7 @@ class RenameUsersMatchingPattern extends Maintenance {
 		$services = MediaWikiServices::getInstance();
 		$this->dbManager = CentralAuthServices::getDatabaseManager();
 		$this->userFactory = $services->getUserFactory();
-		$this->jobQueueGroupFactory = $services->getJobQueueGroupFactory();
+		$this->globalRenameFactory = $services->get( 'CentralAuth.GlobalRenameFactory' );
 		$this->validator = $services->get( 'CentralAuth.GlobalRenameUserValidator' );
 	}
 
@@ -164,8 +159,6 @@ class RenameUsersMatchingPattern extends Maintenance {
 		}
 
 		$oldCaUser = new CentralAuthUser( $oldName, CentralAuthUser::READ_LATEST );
-		// @phan-suppress-next-line SecurityCheck-SQLInjection -- T290563 importLocalNames
-		$newCaUser = new CentralAuthUser( $newUser->getName() );
 
 		$data = [
 			'movepages' => !$this->skipPageMoves,
@@ -174,18 +167,12 @@ class RenameUsersMatchingPattern extends Maintenance {
 			'force' => true,
 		];
 
-		$globalRenameUser = new GlobalRenameUser(
+		$globalRenameUser = $this->globalRenameFactory->newGlobalRenameUser(
 			$this->performer,
-			$oldUser,
 			$oldCaUser,
-			$newUser,
-			$newCaUser,
-			new GlobalRenameUserStatus( $newUser->getName() ),
-			$this->jobQueueGroupFactory,
-			new GlobalRenameUserDatabaseUpdates( $this->dbManager ),
-			new GlobalRenameUserLogger( $this->performer ),
-			null
+			$newName
 		);
+
 		if ( $this->dryRun ) {
 			$this->output( "Would rename \"$oldName\" to \"$newName\"\n" );
 			return true;

commit 31f7f1a1ca2c93bf550f37711adef86cbe025a1f

GlobalRename: Ensure status database rows use the normalized name
This fixes a bug I introduced in bb89a133 ("Add a GlobalRenameFactory"),
namely that the GlobalRenameUserStatus object, responsible for adding
the database rows to keep track of the rename, was not using the
normalized user name.

Bug: T343956
Change-Id: I76d2665045bd7a46a55b569f533964fc7c34c3a8

includes/GlobalRename/GlobalRenameFactory.php
@@ -83,7 +83,7 @@ class GlobalRenameFactory {
 			// TODO: this is only used for caching, maybe we can create a Factory service
 			// for CAUsers and related caching and inject that instead
 			CentralAuthUser::getInstance( $userNew ),
-			new GlobalRenameUserStatus( $newName ),
+			new GlobalRenameUserStatus( $userNew->getName() ),
 			$this->jobQueueGroupFactory,
 			new GlobalRenameUserDatabaseUpdates( $this->databaseManager ),
 			new GlobalRenameUserLogger( $performer ),