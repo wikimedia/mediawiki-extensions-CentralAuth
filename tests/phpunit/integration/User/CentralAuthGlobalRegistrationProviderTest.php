e5f59396b9b429ef3b5564f99b9dd41125e6343b
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-09-16 01:56:49 +0200

    Add CentralAuthGlobalRegistrationProviderTest
    Change-Id: Ic8fd397b71842aa85e44c971bcc43a9b3991b59c

---
+++ tests/phpunit/integration/User/CentralAuthGlobalRegistrationProviderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use MediaWiki\Extension\CentralAuth\User\CentralAuthGlobalRegistrationProvider;
+use MediaWiki\User\UserIdentityValue;
+
+/**
+ * @covers \MediaWiki\Extension\CentralAuth\User\CentralAuthGlobalRegistrationProvider
+ * @group Database
+ */
+class CentralAuthGlobalRegistrationProviderTest extends CentralAuthUsingDatabaseTestCase {
+
+	public function testUnregistered() {
+		$this->assertFalse(
+			$this->getServiceContainer()->getUserRegistrationLookup()->getRegistration(
+				new UserIdentityValue( 0, '127.0.0.1' ),
+				CentralAuthGlobalRegistrationProvider::TYPE
+			)
+		);
+	}
+
+	public function testValid() {
+		$user = new CentralAuthTestUser(
+			'GlobalUser',
+			'GUP@ssword',
+			[ 'gu_id' => 1001 ],
+			[ [ WikiMap::getCurrentWikiId(), 'primary' ] ]
+		);
+		$user->save( $this->db );
+
+		$this->assertSame(
+			'20130627183537',
+			$this->getServiceContainer()->getUserRegistrationLookup()->getRegistration(
+				new UserIdentityValue( 1, 'GlobalUser' ),
+				CentralAuthGlobalRegistrationProvider::TYPE
+			)
+		);
+	}
+}
