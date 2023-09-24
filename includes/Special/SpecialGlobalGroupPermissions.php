c24f95da58868ce5d5fbc4c9275174c209c9db92
Matěj Suchánek <matejsuchanek97@gmail.com>
2023-09-24 21:23:12 +0200

    Drop reportedly unneeded suppressions
    They make phan complain.
    
    Change-Id: Ic9a03bb96ade08bca8509714b38782671594d657

--- includes/Special/SpecialGlobalGroupPermissions.php
+++ includes/Special/SpecialGlobalGroupPermissions.php
@@ -142,7 +142,6 @@ class SpecialGlobalGroupPermissions extends SpecialPage {
 
 			$fields = [ 'centralauth-globalgroupperms-newgroupname' => Xml::input( 'wpGroup' ) ];
 
-			// @phan-suppress-next-line SecurityCheck-DoubleEscaped taint-check tracks keys and values together
 			$html .= Xml::buildForm( $fields, 'centralauth-globalgroupperms-creategroup-submit' );
 			$html .= Xml::closeElement( 'form' );
 			$html .= Xml::closeElement( 'fieldset' );
@@ -347,7 +346,6 @@ class SpecialGlobalGroupPermissions extends SpecialPage {
 			$fields['centralauth-editgroup-reason'] = Xml::input( 'wpReason', 60 );
 		}
 
-		// @phan-suppress-next-line SecurityCheck-DoubleEscaped taint-check tracks keys and values together
 		$html .= Xml::buildForm( $fields,  $editable ? 'centralauth-editgroup-submit' : null );
 
 		if ( $editable ) {
