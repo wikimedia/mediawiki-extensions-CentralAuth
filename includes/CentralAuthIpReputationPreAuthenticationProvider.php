b32218797eb6b137c5cc9dd983305d9c76101eef
Kosta Harlan <kharlan@wikimedia.org>
2024-01-11 10:51:53 +0100

    PreAuthenticationProvider: Deny account creation based on ipoid data
    Why:
    
    - ipoid (https://wikitech.wikimedia.org/wiki/Service/IPoid)
      contains a database of IP reputation data. We can compare
      requests for account creations against ipoid's database and prevent
      unwanted account creations.
    
    What:
    
    - Add config for ipoid URL, a feature flag, configuration for the
      request timeout, and the ability to fine tune which risks and tunnel
      types we want to block
      - Also provide ability to only log account prevention actions, instead
        of actually enforcing them
    - Exempt users with local or global IP block exemption from the checks
    - Cache calls to ipoid
    
    Local testing:
    
    Set the following:
    
    ```
    $wgCentralAuthIpoidUrl = 'http://localhost:6927';
    $wgCentralAuthIpoidCheckAtAccountCreation = true;
    ```
    
    Set up your ipoid instance and add an entry for your local IP address,
    then test creating an account on your wiki.
    
    To test integration with the production ipoid instance, you can set:
    
    ```
    $wgCentralAuthIpoidUrl = 'http://localhost:6035';
    ```
    
    and run `ssh -NL 6035:localhost:6035 mwmaint2002.codfw.wmnet` to tunnel
    requests to production ipoid. But your local IP address is (one hopes)
    not going to be found in the production ipoid DB.
    
    Bug: T354928
    Co-authored-by: Taavi Väänänen <taavi@wikimedia.org>
    Change-Id: I4af3d0fc1e0de8acfe2b8dc25e34b2850975581b

---
+++ includes/CentralAuthIpReputationPreAuthenticationProvider.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace MediaWiki\Extension\CentralAuth;
+
+use ApiMessage;
+use Liuggio\StatsdClient\Factory\StatsdDataFactoryInterface;
+use MediaWiki\Auth\AbstractPreAuthenticationProvider;
+use MediaWiki\Http\HttpRequestFactory;
+use MediaWiki\Language\FormatterFactory;
+use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\Permissions\PermissionManager;
+use MediaWiki\User\UserIdentity;
+use RequestContext;
+use StatusValue;
+use WANObjectCache;
+use Wikimedia\IPUtils;
+
+/**
+ * PreAuthentication provider that checks if an IP address is known to ipoid
+ * @see https://wikitech.wikimedia.org/wiki/Service/IPoid
+ */
+class CentralAuthIpReputationPreAuthenticationProvider extends AbstractPreAuthenticationProvider {
+
+	private HttpRequestFactory $httpRequestFactory;
+	private StatsdDataFactoryInterface $statsDataFactory;
+	private WANObjectCache $cache;
+	private FormatterFactory $formatterFactory;
+	private PermissionManager $permissionManager;
+
+	public function __construct(
+		FormatterFactory $formatterFactory,
+		HttpRequestFactory $httpRequestFactory,
+		WANObjectCache $cache,
+		StatsdDataFactoryInterface $statsDataFactory,
+		PermissionManager $permissionManager
+	) {
+		$this->formatterFactory = $formatterFactory;
+		$this->httpRequestFactory = $httpRequestFactory;
+		$this->cache = $cache;
+		$this->statsDataFactory = $statsDataFactory;
+		$this->permissionManager = $permissionManager;
+	}
+
+	/** @inheritDoc */
+	public function testForAccountCreation( $user, $creator, array $reqs ) {
+		// If feature flag is off, don't do any checks, let the user proceed.
+		if ( !$this->config->get( 'CentralAuthIpoidCheckAtAccountCreation' ) ) {
+			return StatusValue::newGood();
+		}
+
+		if (
+			$this->permissionManager->userHasAnyRight(
+				$creator,
+				'ipblock-exempt',
+				'global-ipblock-exempt'
+			)
+		) {
+			return StatusValue::newGood();
+		}
+
+		$this->logger = LoggerFactory::getInstance( 'IpReputation' );
+
+		$ip = $this->manager->getRequest()->getIP();
+
+		$data = $this->getIpoidDataFor( $user, $ip );
+		if ( !$data ) {
+			// ipoid doesn't know anything about this IP, so let the authentication request proceed.
+			return StatusValue::newGood();
+		}
+
+		$shouldLogOnly = $this->config->get( 'CentralAuthIpoidCheckAtAccountCreationLogOnly' );
+
+		if ( !isset( $data['risks'] ) || !isset( $data['tunnels'] ) ) {
+			// 'risks' and 'tunnels' should always be set, but just in case, check and deny
+			// without considering the risk/tunnel type config later on.
+			// IP exists in ipoid.
+			$this->logger->warning(
+				'Got iPoid data for {ip} without "risks" or "tunnels", preventing user {user} from creating an account',
+				[
+					'user' => $user->getName(),
+					'ip' => $ip,
+					'ipoidData' => json_encode( $data ),
+				]
+			);
+
+			$statsdKey = $shouldLogOnly ? 'DenyCreationLogOnly' : 'DenyCreation';
+			$this->statsDataFactory->increment( "CentralAuth.IpReputation.$statsdKey.UNKNOWN" );
+			if ( $shouldLogOnly ) {
+				return StatusValue::newGood();
+			}
+			return StatusValue::newFatal( 'apierror-autoblocked' );
+		}
+
+		$risksToBlock = $this->config->get( 'CentralAuthIpoidDenyAccountCreationRiskTypes' );
+		$tunnelTypesToBlock = $this->config->get( 'CentralAuthIpoidDenyAccountCreationTunnelTypes' );
+
+		$risks = $data['risks'];
+		sort( $risks );
+
+		$tunnels = $data['tunnels'];
+		sort( $tunnels );
+
+		// Allow for the possibility to exclude VPN users from having account
+		// creation denied, if the only risk type known for the IP is that it's a VPN,
+		// and if config is set up to allow VPN tunnel types.
+		// That would be done with:
+		// $wgCentralAuthIpoidDenyAccountCreationRiskTypes = [ 'TUNNEL', 'CALLBACK_PROXY', ... ];
+		// $wgCentralAuthIpoidDenyAccountCreationTunnelTypes = [ 'PROXY', 'UNKNOWN' ];
+		// If the only risk type is a TUNNEL...
+		if (
+			$risks === [ 'TUNNEL' ]
+			// and there are tunnels listed for the IP
+			&& count( $tunnels )
+			// and we have configured TUNNEL as a risk type to block
+			&& in_array( 'TUNNEL', $risksToBlock )
+			// and the configured tunnel types to block are *not* present in the data
+			&& !array_intersect( $tunnelTypesToBlock, $tunnels )
+		) {
+			$this->logger->debug(
+				'Allowing account creation for user {user} as IP {ip} is known to iPoid '
+				. 'with only non-blocked tunnels ({tunnelTypes})',
+				[
+					'user' => $user->getName(),
+					'ip' => $ip,
+					'tunnelTypes' => implode( ' ', $tunnels ),
+					'ipoidData' => json_encode( $data ),
+				]
+			);
+			return StatusValue::newGood();
+		}
+
+		// Otherwise, check for other risks.
+		$blockedRisks = array_intersect( $risksToBlock, $risks );
+		if ( $blockedRisks ) {
+			$this->logger->warning(
+				'Blocking account creation for user {user} as IP {ip} is known to iPoid '
+				. 'with risks {riskTypes} (blocked due to {blockedRiskTypes})',
+				[
+					'user' => $user->getName(),
+					'ip' => $ip,
+					'riskTypes' => implode( ' ', $risks ),
+					'blockedRiskTypes' => implode( ' ', $blockedRisks ),
+					'ipoidData' => json_encode( $data ),
+				]
+			);
+
+			$statsdKey = $shouldLogOnly ? 'DenyCreationLogOnly' : 'DenyCreation';
+			$this->statsDataFactory->increment( "CentralAuth.IpReputation.$statsdKey." . implode( '_', $risks ) );
+
+			if ( $shouldLogOnly ) {
+				return StatusValue::newGood();
+			}
+
+			// Not a completely accurate message, but it is close enough, and
+			// also exists in translation on all wikis.
+			return StatusValue::newFatal( ApiMessage::create( 'centralauth-blocked-ip-reputation', 'autoblocked' ) );
+		}
+
+		return StatusValue::newGood();
+	}
+
+	/**
+	 * @param UserIdentity $user
+	 * @param string $ip
+	 *
+	 * @return array|null iPoid data for the specific address, or null if there is no data
+	 */
+	private function getIpoidDataFor( UserIdentity $user, string $ip ): ?array {
+		$data = $this->cache->getWithSetCallback(
+			$this->cache->makeGlobalKey( 'centralauth-ipoid', $ip ),
+			// ipoid data is refreshed every 24 hours and roughly 10% of its IPs drop out
+			// of the database each 24-hour cycle. A one hour TTL seems reasonable to allow
+			// no longer problematic IPs to get evicted from the cache relatively quickly,
+			// and also means that IPs for e.g. residential proxies are updated in our cache
+			// relatively quickly.
+			$this->cache::TTL_HOUR,
+			function () use ( $ip, $user ) {
+				// If ipoid URL isn't configured, don't do any checks, let the user proceed.
+				$baseUrl = $this->config->get( 'CentralAuthIpoidUrl' );
+				if ( !$baseUrl ) {
+					$this->logger->warning(
+						'Configured to check IP reputation on signup, but no iPoid URL configured'
+					);
+					// Don't cache this.
+					return false;
+				}
+
+				$timeout = $this->config->get( 'CentralAuthIpoidRequestTimeoutSeconds' );
+				// Convert IPv6 to lowercase, to match ipoid storage format.
+				$url = $baseUrl . '/feed/v1/ip/' . IPUtils::prettifyIP( $ip );
+				$request = $this->httpRequestFactory->create( $url, [
+					'method' => 'GET',
+					'timeout' => $timeout,
+					'connectTimeout' => 1,
+				] );
+				$response = $request->execute();
+				if ( !$response->isOK() ) {
+					// Probably a 404, which means ipoid doesn't know about the IP.
+					// If not a 404, log it, so we can figure out what happened.
+					if ( $request->getStatus() !== 404 ) {
+						$statusFormatter = $this->formatterFactory->getStatusFormatter( RequestContext::getMain() );
+						[ $errorText, $context ] = $statusFormatter->getPsr3MessageAndContext( $response );
+						$this->logger->error( $errorText, $context );
+					}
+
+					return null;
+				}
+
+				$data = json_decode( $request->getContent(), true );
+
+				if ( !$data ) {
+					// Malformed data.
+					$this->logger->error(
+						'Got invalid JSON data while checking user {user} with IP {ip}',
+						[
+							'ip' => $ip,
+							'user' => $user->getName(),
+							'response' => $request->getContent()
+						]
+					);
+					return null;
+				}
+
+				// ipoid will return the IP in lower case format, and we are searching for the
+				// indexed value in the returned array.
+				if ( !isset( $data[IPUtils::prettifyIP( $ip )] ) ) {
+					// IP should always be set in the data array, but just to be safe.
+					$this->logger->error(
+						'Got JSON data with no IP {ip} present while checking user {user}',
+						[
+							'ip' => $ip,
+							'user' => $user->getName(),
+							'response' => $request->getContent()
+						]
+					);
+					return null;
+				}
+
+				// We have a match and valid data structure;
+				// return the values for this IP for storage in the cache.
+				return $data[$ip];
+			}
+		);
+
+		// Unlike null, false tells cache not to cache something. Normalize both to null before returning.
+		if ( $data === false ) {
+			return null;
+		}
+
+		return $data;
+	}
+}
