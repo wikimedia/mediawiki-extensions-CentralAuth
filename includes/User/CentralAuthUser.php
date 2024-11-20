b6fb5c47c7183e2cd300f45557701e1c9cd2c10d
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-11-20 23:36:48 +0000

    Prevent locking of temporary accounts
    Why:
    * Temporary accounts do not have a password and cannot be logged
      into.
    * Locking a temporary account will therefore not prevent login,
      but instead only force the user from their temporary account.
    * However, doing this will mean that the user on their next edit
      will be given a new temporary account username without the
      locked user having any indication that this has occured.
    * Instead, the GlobalBlocking extension is intended to prevent
      editing by temporary accounts on multiple wikis and has been
      updated to properly support this.
    
    What:
    * Update CentralAuthUser::adminLock to return a fatal status for
      a call if the username of the global account matches the temporary
      account name format.
    * Add a test to verify this behaviour.
    
    Bug: T373370
    Change-Id: I6ea8dc6d794e4431a530bcec1872e64e5e98a811

--- includes/User/CentralAuthUser.php
+++ includes/User/CentralAuthUser.php
@@ -1875,7 +1875,13 @@ class CentralAuthUser implements IDBAccessObject {
 	 */
 	public function adminLock() {
 		$this->checkWriteMode();
-		$dbw = CentralAuthServices::getDatabaseManager()->getCentralPrimaryDB();
+
+		$services = MediaWikiServices::getInstance();
+		if ( $services->getTempUserConfig()->isTempName( $this->mName ) ) {
+			return Status::newFatal( 'centralauth-admin-cannot-lock-temporary-account' );
+		}
+
+		$dbw = CentralAuthServices::getDatabaseManager( $services )->getCentralPrimaryDB();
 		$dbw->newUpdateQueryBuilder()
 			->update( 'globaluser' )
 			->set( [ 'gu_locked' => 1 ] )
@@ -1895,7 +1901,7 @@ class CentralAuthUser implements IDBAccessObject {
 		SessionManager::singleton()->invalidateSessionsForUser( $user );
 
 		// T375870: Track rate of locks for monitoring of anti-abuse tool usage.
-		MediaWikiServices::getInstance()->getStatsFactory()->withComponent( 'CentralAuth' )
+		$services->getStatsFactory()->withComponent( 'CentralAuth' )
 			->getCounter( 'account_lock' )
 			->increment();
 
