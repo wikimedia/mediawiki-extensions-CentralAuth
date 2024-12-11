22122188e6b9021844bff53158887ab93352ca72
Thalia <thalia.e.chan@googlemail.com>
2024-12-11 18:45:07 +0000

    Update automatic global group membership on local group change
    Why:
    
    * As part of the temporary accounts project, some users
      automatically gain and lose the right to belong to a global
      group, depending on their local group memberships.
    * Automatic global groups were introduced in Icbd225193672
      so that these groups could be updated automatically.
    
    What:
    
    * Add a hook handler for `UserGroupsChangedHook`, which adds or
      removes global groups depending on the configured automatic
      global groups.
    * The hook runs whenever a user's local groups are updated.
    * A future commit will handle updating automatic groups when
      global groups are changed.
    * Update the global groups even if the performer doesn't have
      the right to change global group membership. The action is
      logged with the same performer and a note that the change
      was automatic.
    
    Bug: T376315
    Change-Id: I98a2abc1a31998fbad191c8979fd518fe8d6f92d

---
+++ tests/phpunit/integration/Hooks/Handlers/UserGroupsHookHandlerTest.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace MediaWiki\Extension\CentralAuth\Tests\Phpunit\Integration\Hooks\Handlers;
+
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Extension\CentralAuth\CentralAuthAutomaticGlobalGroupManager;
+use MediaWiki\Extension\CentralAuth\CentralAuthServices;
+use MediaWiki\Extension\CentralAuth\Hooks\Handlers\UserGroupsHookHandler;
+use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
+use MediaWiki\Tests\MockWikiMapTrait;
+use MediaWiki\WikiMap\WikiMap;
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @group Database
+ * @covers \MediaWiki\Extension\CentralAuth\Hooks\Handlers\UserGroupsHookHandler
+ */
+class UserGroupsHookHandlerTest extends MediaWikiIntegrationTestCase {
+	use MockWikiMapTrait;
+
+	public function addDBDataOnce() {
+		// Set up defined global groups
+		$this->getDb()->newInsertQueryBuilder()
+			->insertInto( 'global_group_permissions' )
+			->rows( [
+				[ 'ggp_group' => 'automatic-global-group', 'ggp_permission' => 'read' ],
+				[ 'ggp_group' => 'existing-global-group', 'ggp_permission' => 'read' ],
+			] )
+			->caller( __METHOD__ )
+			->execute();
+	}
+
+	private function getHandler( $globalGroupManager = null ) {
+		return new UserGroupsHookHandler(
+			$this->getServiceContainer()->getMainConfig(),
+			$this->getServiceContainer()->getTitleFactory(),
+			$this->getServiceContainer()->getUserNamePrefixSearch(),
+			$this->getServiceContainer()->getUserNameUtils(),
+			$globalGroupManager ??
+				CentralAuthServices::getAutomaticGlobalGroupManager( $this->getServiceContainer() ),
+			CentralAuthServices::getGlobalGroupLookup( $this->getServiceContainer() )
+		);
+	}
+
+	private function getTargetUser() {
+		// Set up a global user that has an attached local user
+		$testUser = $this->getTestUser();
+		$user = $testUser->getUser();
+		$globalUser = CentralAuthUser::getPrimaryInstance( $user );
+		$globalUser->register( $testUser->getPassword(), null );
+		$globalUser->attach( WikiMap::getCurrentWikiId() );
+
+		return $user;
+	}
+
+	/** @dataProvider provideGlobalGroupMembershipPermission */
+	public function testOnUserGroupsChanged( $hasPermission ) {
+		$this->overrideConfigValue( 'CentralAuthAutomaticGlobalGroups', [
+			'new-local-group' => [ 'automatic-global-group' ],
+			'existing-global-group' => [ 'automatic-global-group' ],
+		] );
+
+		$this->mockWikiMap();
+
+		$user = $this->getTargetUser();
+		$handler = $this->getHandler();
+
+		$this->setGroupPermissions( 'sysop', 'globalgroupmembership', $hasPermission );
+		$performer = $this->getTestSysop()->getUser();
+		RequestContext::getMain()->setUser( $performer );
+
+		// Add the user to the local group, then run the hook.
+		// The user should be added to the automatic global group.
+
+		$this->getServiceContainer()->getUserGroupManager()
+			->addUserToGroup( $user, 'new-local-group' );
+
+		$handler->onUserGroupsChanged(
+			$user,
+			[ 'new-local-group' ],
+			[],
+			$performer,
+			'Test reason',
+			[],
+			[]
+		);
+
+		// Re-fetch the global user with the new groups
+		CentralAuthUser::clearUserCache();
+		$globalUser = CentralAuthUser::getPrimaryInstanceByName( $user->getName() );
+
+		$this->assertSame( [ 'automatic-global-group' ], $globalUser->getGlobalGroups() );
+
+		$globalUser->addToGlobalGroup( 'existing-global-group' );
+
+		// Remove the user from the local group, then run the hook.
+		// The user should not be removed from the automatic global group, because of the
+		// other global group.
+
+		$this->getServiceContainer()->getUserGroupManager()
+			->removeUserFromGroup( $user, 'new-local-group' );
+
+		$handler->onUserGroupsChanged(
+			$user,
+			[],
+			[ 'new-local-group' ],
+			$performer,
+			'Test reason',
+			[],
+			[]
+		);
+
+		// Re-fetch the global user with the new groups
+		CentralAuthUser::clearUserCache();
+		$globalUser = CentralAuthUser::getPrimaryInstanceByName( $user->getName() );
+
+		$this->assertSame(
+			[ 'automatic-global-group', 'existing-global-group' ],
+			$globalUser->getGlobalGroups()
+		);
+	}
+
+	public function provideGlobalGroupMembershipPermission() {
+		return [
+			'Groups are updated when the performer has permission' => [ true ],
+			'Groups are updated when the performer does not have permission' => [ false ],
+		];
+	}
+
+	public function testOnUserGroupsChangedNonexistentUser() {
+		$handler = $this->getHandler(
+			$this->createNoOpMock( CentralAuthAutomaticGlobalGroupManager::class )
+		);
+
+		// Simulate running the hook with a local user without a global user
+		$handler->onUserGroupsChanged(
+			$this->getTestUser()->getUser(),
+			[],
+			[],
+			$this->getTestUser()->getUser(),
+			'Test reason',
+			[],
+			[]
+		);
+	}
+}
