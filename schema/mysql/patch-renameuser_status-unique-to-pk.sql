f86b33936f6df66dcabf28b87933fa08258f7a03
Daimona Eaytoy <daimona.wiki@gmail.com>
2024-12-14 03:51:24 +0100

    tests: Add structure test for abstract schema
    Use the new base class introduced in T381981.
    
    Also regenerate all the schema changes to get rid of drifts, as done in
    I3a3acc0935409d4601eb172664580b13d5fe2340.
    
    Depends-On: I8ae59ce6854922c2804d12986c870d3f0e0c3299
    Change-Id: I2922f04ad1ffa463a96cebe0a14d75a5d2d0bc42

--- schema/mysql/patch-renameuser_status-unique-to-pk.sql
+++ schema/mysql/patch-renameuser_status-unique-to-pk.sql
@@ -2,6 +2,6 @@
 -- Source: schema/abstractSchemaChanges/patch-renameuser_status-unique-to-pk.json
 -- Do not modify this file directly.
 -- See https://www.mediawiki.org/wiki/Manual:Schema_changes
-DROP  INDEX ru_oldname ON  /*_*/renameuser_status;
-ALTER TABLE  /*_*/renameuser_status
-ADD  PRIMARY KEY (ru_oldname, ru_wiki);
+DROP INDEX ru_oldname ON /*_*/renameuser_status;
+ALTER TABLE /*_*/renameuser_status
+  ADD PRIMARY KEY (ru_oldname, ru_wiki);
