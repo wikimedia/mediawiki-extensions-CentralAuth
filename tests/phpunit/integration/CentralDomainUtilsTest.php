b10a647882cdce2d6d9c45e82b06887c258a1fe8
Gergő Tisza <tgr.huwiki@gmail.com>
2025-02-18 17:36:13 +0100

    Add passive central domain to edge login list
    When doing edge login, do an edge login on the "wrong" central
    domain (the SUL2 domain if we are in SUL3 mode, the SUL3 domain
    if we are in SUL2 mode). This helps keeping the two domains in
    sync.
    
    Also clean up CentralDomainUtils tests so it's clearer what they
    are testing.
    
    Bug: T375796
    Change-Id: Iddc22e2dcfc2a2af213b24133d5937491f716968

--- tests/phpunit/integration/CentralDomainUtilsTest.php
+++ tests/phpunit/integration/CentralDomainUtilsTest.php
@@ -2,7 +2,6 @@
 
 namespace MediaWiki\Extension\CentralAuth\Tests\Phpunit\Integration;
 
-use Generator;
 use MediaWiki\Config\SiteConfiguration;
 use MediaWiki\Extension\CentralAuth\CentralDomainUtils;
 use MediaWiki\Extension\CentralAuth\Config\CAMainConfigNames;
@@ -23,19 +22,23 @@ class CentralDomainUtilsTest extends MediaWikiIntegrationTestCase {
 		$conf = new SiteConfiguration();
 		$conf->settings = [
 			'wgServer' => [
-				'foowiki' => 'http://foowiki.example.org',
+				'foowiki' => 'https://foowiki.example.org',
+				'loginwiki' => 'https://login.example.org',
 			],
 			'wgArticlePath' => [
-				'foowiki' => '/w/$1',
+				'foowiki' => '/wiki/$1',
+				'loginwiki' => '/wiki/$1',
 			],
 		];
 		$conf->suffixes = [ 'wiki' ];
 		$this->setMwGlobals( 'wgConf', $conf );
 		$this->overrideConfigValues( [
-			MainConfigNames::LocalDatabases => [ 'foowiki' ],
-			MainConfigNames::CanonicalServer => '//foowiki.example.org',
+			MainConfigNames::LocalDatabases => [ 'foowiki', 'loginwiki' ],
+			MainConfigNames::CanonicalServer => 'https://foowiki.example.org',
 			MainConfigNames::DBname => 'foowiki',
-			MainConfigNames::DBprefix => ''
+			MainConfigNames::DBprefix => null,
+			CAMainConfigNames::CentralAuthLoginWiki => 'loginwiki',
+			CAMainConfigNames::CentralAuthSharedDomainPrefix => 'https://auth.example.org',
 		] );
 
 		TestSites::insertIntoDb();
@@ -50,161 +53,48 @@ class CentralDomainUtilsTest extends MediaWikiIntegrationTestCase {
 		);
 	}
 
-	public function testDefaultGetUrlForSul3Enabled(): void {
-		$wikiId = CentralDomainUtils::CENTRAL_DOMAIN_ID;
-		$page = 'Main_Page';
-		$params = [];
-
-		$this->overrideConfigValues( [
-			CAMainConfigNames::CentralAuthEnableSul3 => [ 'always' ],
-			CAMainConfigNames::CentralAuthSharedDomainPrefix => 'https://auth0.wikimedia.org',
-		] );
-
-		$centralDomainUtils = $this->getCentralDomainUtils();
-
-		// Let's make sure the URL points to the right page.
-		$this->assertStringContainsString(
-			$page, $centralDomainUtils->getUrl( $wikiId, $page, new FauxRequest(), $params )
-		);
-
-		// The default behaviour is that we're on desktop.
-		$this->assertStringContainsString(
-			'useformat=desktop', $centralDomainUtils->getUrl( $wikiId, $page, new FauxRequest(), $params )
-		);
-
-		// Check that the URL has the shared domain prefix.
-		$this->assertStringContainsString(
-			'https://auth0.wikimedia.org',
-			$centralDomainUtils->getUrl( $wikiId, $page, new FauxRequest(), $params )
-		);
-	}
-
-	private function applyWikiConfigOverrides( string $wikiId, array $sul3Enabled ): void {
-		$this->overrideConfigValues( [
-			MainConfigNames::DBname => $wikiId,
-			MainConfigNames::DBprefix => null,
-			MainConfigNames::Server => "//$wikiId.wikimedia.org",
-			MainConfigNames::CanonicalServer => "https://$wikiId.wikimedia.org",
-			CAMainConfigNames::CentralAuthSharedDomainPrefix => 'https://auth0.wikimedia.org',
-			CAMainConfigNames::CentralAuthEnableSul3 => $sul3Enabled,
-		] );
-	}
-
-	public static function provideGetUrlDataForCentralDomain(): Generator {
-		yield 'central domain' => [
-			'wikiId' => CentralDomainUtils::CENTRAL_DOMAIN_ID,
-			'page' => 'Main_Page',
-			'params' => [],
-			'sul3Enabled' => [ 'always' ],
-		];
-
-		yield 'central domain on Bar page with mobile param' => [
-			'wikiId' => CentralDomainUtils::CENTRAL_DOMAIN_ID,
-			'page' => 'Bar',
-			'params' => [ 'useformat' => 'mobile' ],
-			'sul3Enabled' => [ 'always' ],
-		];
-
-		yield 'central domain on Bar page with desktop param' => [
-			'wikiId' => CentralDomainUtils::CENTRAL_DOMAIN_ID,
-			'page' => 'Bar',
-			'params' => [ 'useformat' => 'desktop' ],
-			'sul3Enabled' => [ 'always' ],
-		];
-
-		yield 'central domain with special page Foo with desktop param' => [
-			'wikiId' => CentralDomainUtils::CENTRAL_DOMAIN_ID,
-			'page' => 'Special:Foo',
-			'params' => [ 'useformat' => 'desktop' ],
-			'sul3Enabled' => [ 'always' ],
-		];
-	}
-
 	/**
-	 * @dataProvider provideGetUrlDataForCentralDomain
+	 * @dataProvider provideGetUrlData
 	 */
-	public function testGetUrlForCentralDomain( $wikiId, $page, $params, $sul3Enabled ): void {
-		$this->applyWikiConfigOverrides( $wikiId, $sul3Enabled );
+	public function testGetUrl(
+		bool $isSul3Enabled,
+		string $wikiId,
+		string $page,
+		array $params,
+		string $expectedUrl
+	) {
+		$this->overrideConfigValue( CAMainConfigNames::CentralAuthEnableSul3, $isSul3Enabled ? [ 'always' ] : [] );
 		$centralDomainUtils = $this->getCentralDomainUtils();
-
-		// Check that the URL has the shared domain prefix.
-		$this->assertStringContainsString(
-			'auth0.wikimedia.org',
-			$centralDomainUtils->getUrl( $wikiId, $page, new FauxRequest(), $params )
-		);
-
-		// Let's make sure the URL points to the right page.
-		$this->assertStringContainsString(
-			$page, $centralDomainUtils->getUrl( $wikiId, $page, new FauxRequest(), $params )
-		);
-
-		// The default behaviour is that we're on the correct device.
-		$this->assertStringContainsString(
-			$params['useformat'] ?? 'desktop', $centralDomainUtils->getUrl( $wikiId, $page, new FauxRequest(), $params )
-		);
+		$actualUrl = $centralDomainUtils->getUrl( $wikiId, $page, new FauxRequest(), $params );
+		$this->assertSame( $expectedUrl, $actualUrl );
 	}
 
-	public static function provideGetUrlDataForNonCentralDomain(): Generator {
-		yield 'non-central domain' => [
-			'wikiId' => 'foowiki',
-			'page' => 'Main_Page',
-			'params' => [],
-			'sul3Enabled' => [ 'always' ],
+	public function provideGetUrlData() {
+		$centralDomain = CentralDomainUtils::CENTRAL_DOMAIN_ID;
+		$passiveDomain = CentralDomainUtils::PASSIVE_CENTRAL_DOMAIN_ID;
+		return [
+			// $isSul3Enabled, $wikiId, $page, $params, $expectedUrl
+			[ false, $centralDomain, 'Main Page', [],
+				'https://login.example.org/wiki/Main_Page?useformat=desktop' ],
+			[ false, $centralDomain, 'Special:UserLogin', [ 'foo' => 'bar' ],
+				'https://login.example.org/wiki/Special:UserLogin?useformat=desktop&foo=bar' ],
+			[ false, $passiveDomain, 'Special:UserLogin', [ 'foo' => 'bar' ],
+				'https://auth.example.org/wiki/Special:UserLogin?useformat=desktop&foo=bar' ],
+			[ false, 'enwiktionary', 'Special:UserLogin', [ 'foo' => 'bar' ],
+				'https://en.wiktionary.org/wiki/Special:UserLogin?useformat=desktop&foo=bar' ],
+			[ true, $centralDomain, 'Special:UserLogin', [ 'foo' => 'bar' ],
+				'https://auth.example.org/wiki/Special:UserLogin?useformat=desktop&foo=bar' ],
+			[ true, $passiveDomain, 'Special:UserLogin', [ 'foo' => 'bar' ],
+				'https://login.example.org/wiki/Special:UserLogin?useformat=desktop&foo=bar' ],
+			[ true, 'enwiktionary', 'Special:UserLogin', [ 'foo' => 'bar' ],
+				'https://en.wiktionary.org/wiki/Special:UserLogin?useformat=desktop&foo=bar' ],
 		];
-
-		yield 'non-central domain on Bar page with mobile param' => [
-			'wikiId' => 'foowiki',
-			'page' => 'Bar',
-			'params' => [ 'useformat' => 'mobile' ],
-			'sul3Enabled' => [ 'always' ],
-		];
-
-		yield 'non-central domain on Bar page with desktop param' => [
-			'wikiId' => 'foowiki',
-			'page' => 'Bar',
-			'params' => [ 'useformat' => 'desktop' ],
-			'sul3Enabled' => [ 'always' ],
-		];
-
-		yield 'non-central domain with special page Foo with desktop param' => [
-			'wikiId' => 'foowiki',
-			'page' => 'Special:Foo',
-			'params' => [ 'useformat' => 'desktop' ],
-			'sul3Enabled' => [ 'always' ],
-		];
-	}
-
-	/**
-	 * @dataProvider provideGetUrlDataForNonCentralDomain
-	 */
-	public function testGetUrlForNonCentralDomain( $wikiId, $page, $params, $sul3Enabled ): void {
-		$this->applyWikiConfigOverrides( $wikiId, $sul3Enabled );
-
-		$centralDomainUtils = $this->getCentralDomainUtils();
-
-		// Check that the URL has the shared domain prefix.
-		$this->assertStringNotContainsString(
-			'auth0.wikimedia.org',
-			$centralDomainUtils->getUrl( $wikiId, $page, new FauxRequest(), $params )
-		);
-
-		// Let's make sure the URL points to the right page.
-		$this->assertStringContainsString(
-			$page, $centralDomainUtils->getUrl( $wikiId, $page, new FauxRequest(), $params )
-		);
-
-		// The default behaviour is that we're on the correct device.
-		$this->assertStringContainsString(
-			$params['useformat'] ?? 'desktop', $centralDomainUtils->getUrl( $wikiId, $page, new FauxRequest(), $params )
-		);
 	}
 
 	public function testIsCentralDomain(): void {
 		// SUL2 setup for central domain.
 		$this->overrideConfigValues( [
 			MainConfigNames::DBname => 'loginwiki',
-			MainConfigNames::DBprefix => null,
-			CAMainConfigNames::CentralAuthLoginWiki => 'loginwiki',
 			CAMainConfigNames::CentralAuthEnableSul3 => [ 'query-flag' ],
 		] );
 		$centralDomainUtils = $this->getCentralDomainUtils();
