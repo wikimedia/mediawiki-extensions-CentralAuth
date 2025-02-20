947d0f7c66942dff9e75ddef1ccbb8c7d00befda
Tim Starling <tstarling@wikimedia.org>
2025-02-20 15:57:12 +1100

    Implement CentralIdLookup::lookupUserNamesWithFilter
    Allow batch lookups of isOwned() or isAttached().
    
    Integrate with the user cache to avoid a performance regression.
    
    Bug: T386584
    Depends-On: If7ee55e0a26546429cc92ec3adf9ba86325bc49d
    Change-Id: Ie63ed1f14c9ba4f9cefcceef05d5585a6676f111

--- includes/User/CentralAuthIdLookup.php
+++ includes/User/CentralAuthIdLookup.php
@@ -23,8 +23,9 @@ namespace MediaWiki\Extension\CentralAuth\User;
 use MediaWiki\Config\Config;
 use MediaWiki\DAO\WikiAwareEntity;
 use MediaWiki\Extension\CentralAuth\CentralAuthDatabaseManager;
+use MediaWiki\Extension\CentralAuth\CentralAuthUserCache;
 use MediaWiki\Extension\CentralAuth\Config\CAMainConfigNames;
-use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\Permissions\Authority;
 use MediaWiki\User\CentralId\CentralIdLookup;
 use MediaWiki\User\UserIdentity;
 use MediaWiki\WikiMap\WikiMap;
@@ -40,10 +41,19 @@ class CentralAuthIdLookup extends CentralIdLookup {
 
 	private Config $config;
 	private CentralAuthDatabaseManager $databaseManager;
+	private CentralAuthUserCache $userCache;
 
-	public function __construct( Config $config, CentralAuthDatabaseManager $databaseManager ) {
+	/** @var array<string,bool> Names that cause a NormalizedException */
+	private $badNameCache = [];
+
+	public function __construct(
+		Config $config,
+		CentralAuthDatabaseManager $databaseManager,
+		CentralAuthUserCache $userCache
+	) {
 		$this->config = $config;
 		$this->databaseManager = $databaseManager;
+		$this->userCache = $userCache;
 	}
 
 	/** @inheritDoc */
@@ -81,55 +91,204 @@ class CentralAuthIdLookup extends CentralIdLookup {
 	public function lookupUserNames(
 		array $nameToId, $audience = self::AUDIENCE_PUBLIC, $flags = IDBAccessObject::READ_NORMAL
 	): array {
+		return $this->lookupUserNamesWithFilter( $nameToId, self::FILTER_NONE,
+			$audience, $flags );
+	}
+
+	/** @inheritDoc */
+	protected function lookupUserNamesWithFilter(
+		array $nameToId, $filter, $audience = self::AUDIENCE_PUBLIC,
+		$flags = IDBAccessObject::READ_NORMAL, $wikiId = UserIdentity::LOCAL
+	): array {
+		$audience = $this->checkAudience( $audience );
+		$filter = $this->simplifyFilter( $filter );
+		if ( $wikiId === UserIdentity::LOCAL
+			|| $wikiId === WikiMap::getCurrentWikiId()
+			|| $filter === self::FILTER_NONE
+		) {
+			return $this->lookupUserNamesWithLocalFilter( $nameToId, $filter, $audience, $flags );
+		} else {
+			return $this->lookupUserNamesWithForeignFilter( $nameToId, $filter, $wikiId, $audience, $flags );
+		}
+	}
+
+	/**
+	 * Look up central IDs for a list of usernames while checking attachment status
+	 * on a wiki that is not the local wiki.
+	 *
+	 * @param array $nameToId
+	 * @param string $filter The simplified (config-independent) filter, either
+	 *   FILTER_ATTACHED or FILTER_OWNED
+	 * @param string|false $wikiId
+	 * @param Authority|null $audience The authority for viewing rows, or null for unauthenticated
+	 * @param int $flags
+	 * @return array
+	 */
+	private function lookupUserNamesWithForeignFilter(
+		array $nameToId,
+		$filter,
+		$wikiId,
+		?Authority $audience,
+		int $flags
+	): array {
 		if ( !$nameToId ) {
 			return [];
 		}
 
-		$audience = $this->checkAudience( $audience );
-		$fromPrimaryDb = $this->shouldUsePrimary( $flags );
+		$db = $this->databaseManager->getCentralDBFromRecency( $flags );
+		$qb = $db->newSelectQueryBuilder()
+			->select( [ 'gu_name', 'gu_id', 'gu_hidden_level' ] )
+			->from( 'globaluser' )
+			->where( [ 'gu_name' => array_map( 'strval', array_keys( $nameToId ) ) ] );
 
-		$centralUserArray = [];
-		if ( count( $nameToId ) === 1 ) {
-			// Use cache in the common case of looking up a single user
-			$name = array_key_first( $nameToId );
-			try {
-				$centralUser = $fromPrimaryDb
-					? CentralAuthUser::getPrimaryInstanceByName( $name )
-					: CentralAuthUser::getInstanceByName( $name );
-				// The batch path ignores non-canonical usernames, do the same for consistency
-				if ( $centralUser->getName() === $name ) {
-					$centralUserArray[$name] = $centralUser;
-				}
-			} catch ( NormalizedException $e ) {
-				// Usernames are supposed to be valid, but in the past this has not been enforced
-				// and CentralAuthUser throws on invalid usernames, so log and swallow the exception
-				LoggerFactory::getInstance( 'CentralAuth' )->warning(
-					__METHOD__ . ': invalid username: {name}',
-					[ 'name' => $name ]
+		if ( $filter === self::FILTER_ATTACHED ) {
+			$qb->join( 'localuser', null, 'lu_name=gu_name' )
+				->where( [ 'lu_wiki' => $wikiId ] );
+		} elseif ( $filter === self::FILTER_OWNED ) {
+			// In strict mode, the user is owned if it is either attached or
+			// if it is locally nonexistent.
+			$qb->leftJoin( 'localnames', null,
+					[ 'ln_name=gu_name', 'ln_wiki' => $wikiId ] )
+				->leftJoin( 'localuser', null,
+					[ 'lu_name=gu_name', 'lu_wiki' => $wikiId ] )
+				->where(
+					$db->expr( 'ln_name', '=', null )
+						->or( 'lu_name', '!=', null )
 				);
-			}
-		} else {
-			$db = $this->getCentralDB( $flags );
-			$res = $db->newSelectQueryBuilder()
-				->queryInfo( CentralAuthUser::selectQueryInfo() )
-				->where( [ 'gu_name' => array_map( 'strval', array_keys( $nameToId ) ) ] )
-				->caller( __METHOD__ )
-				->fetchResultSet();
-			foreach ( $res as $row ) {
-				$centralUser = CentralAuthUser::newFromRow( $row, [], $fromPrimaryDb );
-				$centralUserArray[$centralUser->getName()] = $centralUser;
-			}
 		}
 
-		foreach ( $centralUserArray as $name => $centralUser ) {
-			if ( $centralUser->exists()
-				&& ( $centralUser->getHiddenLevelInt() === CentralAuthUser::HIDDEN_LEVEL_NONE
-					|| $audience === null || $audience->isAllowed( 'centralauth-suppress' ) )
-			) {
-				$nameToId[$name] = $centralUser->getId();
+		$res = $qb->caller( __METHOD__ )->fetchResultSet();
+
+		foreach ( $res as $row ) {
+			if ( $this->canView( $audience, $row->gu_hidden_level ) ) {
+				$nameToId[$row->gu_name] = $row->gu_id;
 			}
 		}
 		return $nameToId;
+	}
+
+	/**
+	 * Look up central IDs for a list of usernames, either with no filter or
+	 * with an attachment filter on the local wiki. Use the CentralAuthUserCache
+	 * for caching.
+	 *
+	 * @param array $nameToId
+	 * @param string $filter The simplified config-independent filter
+	 * @param Authority|null $audience The authority for viewing rows, or null for unauthenticated
+	 * @param int $flags
+	 * @return array
+	 */
+	private function lookupUserNamesWithLocalFilter(
+		array $nameToId,
+		$filter,
+		?Authority $audience,
+		int $flags
+	): array {
+		$fromPrimaryDb = $this->shouldUsePrimary( $flags );
+
+		$namesToLoad = [];
+		foreach ( $nameToId as $name => $_ ) {
+			$centralUser = $this->userCache->get( $name, $fromPrimaryDb );
+			if ( $centralUser ) {
+				if ( $filter === self::FILTER_ATTACHED ) {
+					$filterPass = $centralUser->isAttached();
+				} elseif ( $filter === self::FILTER_OWNED ) {
+					if ( $centralUser->getId() === 0 ) {
+						// Absent from globaluser
+						$filterPass = false;
+					} elseif ( $centralUser->isAttached() ) {
+						// If it's attached, it's definitely owned, that's the normal case
+						$filterPass = true;
+					} elseif ( $centralUser->isLocalExistenceLoaded() ) {
+						$filterPass = !$centralUser->existsLocally();
+					} else {
+						// Have to do a DB query
+						$filterPass = false;
+						$namesToLoad[] = (string)$name;
+					}
+				} else {
+					$filterPass = true;
+				}
+				if ( $filterPass && $this->canView( $audience, $centralUser->getHiddenLevelInt() ) ) {
+					if ( $centralUser->getId() ) {
+						$nameToId[$name] = $centralUser->getId();
+					}
+				}
+			} elseif ( isset( $this->badNameCache[$name] ) ) {
+				// Do not try to load bad names
+			} else {
+				$namesToLoad[] = (string)$name;
+			}
+		}
+
+		if ( !$namesToLoad ) {
+			return $nameToId;
+		}
+
+		$db = $this->databaseManager->getCentralDBFromRecency( $flags );
+		$res = $db->newSelectQueryBuilder()
+			->queryInfo( CentralAuthUser::selectQueryInfo() )
+			->where( [ 'gu_name' => $namesToLoad ] )
+			->caller( __METHOD__ )->fetchResultSet();
+
+		$loadedNames = [];
+		foreach ( $res as $row ) {
+			$centralUser = CentralAuthUser::newFromRow( $row, [], $fromPrimaryDb );
+			$this->userCache->set( $centralUser );
+			if ( $filter === self::FILTER_ATTACHED ) {
+				$filterPass = $centralUser->isAttached();
+			} elseif ( $filter === self::FILTER_OWNED ) {
+				$filterPass = $centralUser->isAttached() || !$centralUser->existsLocally();
+			} else {
+				$filterPass = true;
+			}
+			if ( $filterPass && $this->canView( $audience, $centralUser->getHiddenLevelInt() ) ) {
+				$nameToId[$centralUser->getName()] = $centralUser->getId();
+			}
+			$loadedNames[] = $row->gu_name;
+		}
+
+		$missingNames = array_diff( $namesToLoad, $loadedNames );
+		foreach ( $missingNames as $name ) {
+			try {
+				$centralUser = CentralAuthUser::newUnattached( $name, $fromPrimaryDb );
+				$this->userCache->set( $centralUser );
+			} catch ( NormalizedException $e ) {
+				$this->badNameCache[$name] = true;
+			}
+		}
+
+		return $nameToId;
+	}
+
+	/**
+	 * Determine if the authority can view a row with a given hidden level
+	 *
+	 * @param Authority|null $authority
+	 * @param int $hiddenLevel
+	 * @return bool
+	 */
+	private function canView( ?Authority $authority, int $hiddenLevel ): bool {
+		return $hiddenLevel === CentralAuthUser::HIDDEN_LEVEL_NONE
+			|| !$authority || $authority->isAllowed( 'centralauth-suppress' );
+	}
+
+	/**
+	 * Resolve a filter type to a config-independent type.
+	 * FILTER_OWNED in non-strict mode is just an attachment check, so we
+	 * return FILTER_ATTACHED in that case. FILTER_OWNED is only returned
+	 * in strict mode to indicate that unattached users are allowed.
+	 *
+	 * @param string $filter
+	 * @return string
+	 */
+	private function simplifyFilter( $filter ) {
+		// FILTER_OWNED in non-strict mode is just an attachment check
+		$strictMode = $this->config->get( CAMainConfigNames::CentralAuthStrict );
+		if ( !$strictMode && $filter === self::FILTER_OWNED ) {
+			$filter = self::FILTER_ATTACHED;
+		}
+		return $filter;
 	}
 
 	/** @inheritDoc */
