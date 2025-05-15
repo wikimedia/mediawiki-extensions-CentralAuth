0258f27593d2a138c0ddfeb25391da17b8823cea
Thalia <thalia.e.chan@googlemail.com>
2025-05-15 13:03:56 +0100

    Handle expiries in automatic global groups
    Why:
    
    * As part of the temporary accounts project, some users
      automatically gain and lose the right to belong to a global
      group, depending on their other group memberships.
    * Automatic global groups were introduced in Icbd225193672
      so that these groups could be updated automatically.
    * Before this commit, automatic global groups are always
      added without an expiry date; however the groups that
      determine whether the automatic groups should be assigned
      may have expiries. This could result in an automatic global
      group being assigned even after all the promoting groups
      have expired.
    
    What:
    
    * Update CentralAuthAutomaticGlobalGroupManager to give any
      automatic global group the expiry of the latest promoting
      group. (This is indefinite if any of the promoting groups
      are assigned indefinitely.)
    * Update SpecialGlobalGroupMembership::doSaveUserGroups to
      pass through the group expiry information to the
      CentralAuthAutomaticGlobalGroupManager.
    
    Bug: T376315
    Change-Id: Id3d5756da604a365e9ca0fcef4c6020d5769d772

--- tests/phpunit/integration/Special/SpecialGlobalGroupMembershipTest.php
+++ tests/phpunit/integration/Special/SpecialGlobalGroupMembershipTest.php
@@ -31,7 +31,9 @@ use MediaWiki\Session\SessionManager;
 use MediaWiki\Tests\MockWikiMapTrait;
 use MediaWiki\Tests\Unit\Permissions\MockAuthorityTrait;
 use MediaWiki\Tests\User\TempUser\TempUserTestTrait;
+use MediaWiki\User\UserGroupMembership;
 use MediaWiki\WikiMap\WikiMap;
+use ReflectionClass;
 use SpecialPageTestBase;
 use Wikimedia\TestingAccessWrapper;
 
@@ -403,6 +405,54 @@ class SpecialGlobalGroupMembershipTest extends SpecialPageTestBase {
 				[]
 			],
 		];
+	}
+
+	public function testAdjustForAutomaticGlobalGroups() {
+		$localGroup = 'localGroup';
+		$this->overrideConfigValue( 'CentralAuthAutomaticGlobalGroups', [
+			$localGroup => [ 'group-one' ],
+		] );
+
+		$specialPage = TestingAccessWrapper::newFromObject( $this->newSpecialPage() );
+
+		$localGroupMembership = $this->createMock( UserGroupMembership::class );
+		$localGroupMembership->method( 'getGroup' )
+			->willReturn( $localGroup );
+		$localGroupMembership->method( 'getExpiry' )
+			->willReturn( '20230405060707' );
+
+		$externalGroupMembership = $this->createMock( UserGroupMembership::class );
+		$externalGroupMembership->method( 'getGroup' )
+			->willReturn( $localGroup );
+		$externalGroupMembership->method( 'getExpiry' )
+			->willReturn( '20240405060707' );
+
+		$user = $this->createMock( CentralAuthUser::class );
+		$user->method( 'queryAttached' )
+			->willReturn( [
+				'localWiki' => [
+					'groupMemberships' => [ $localGroupMembership ],
+				],
+				'externalWiki' => [
+					'groupMemberships' => [ $externalGroupMembership ],
+				]
+			] );
+
+		$assignedGroups = [];
+		$add = [];
+		$remove = [];
+		$groupExpiries = [];
+		// T287318 - TestingAccessWrapper::__call does not support pass-by-reference
+		$classReflection = new ReflectionClass( $specialPage->object );
+		$methodReflection = $classReflection->getMethod( 'adjustForAutomaticGlobalGroups' );
+		$methodReflection->setAccessible( true );
+		$methodReflection->invokeArgs( $specialPage->object, [
+			$user, $assignedGroups, &$add, &$remove, &$groupExpiries,
+		] );
+
+		$this->assertEquals( [ 'group-one' ], $add );
+		$this->assertEquals( [], $remove );
+		$this->assertEquals( [ 'group-one' => '20240405060707' ], $groupExpiries );
 	}
 
 	/** @dataProvider provideGetLogReason */
