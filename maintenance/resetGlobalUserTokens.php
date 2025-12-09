487de13f0ef13e87dae73a78c6aa3240e667f7c5
Bartosz Dziewoński <dziewonski@fastmail.fm>
2025-12-09 13:03:51 +0000

    Update copy of GPL license and licensing headers in files
    The postal address of the FSF has changed, and the internet address
    is more practical these days.
    
    * Update COPYING file to the current text of the license copied from
      https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
    
    * Replace licensing headers in files with a Doxygen @license tag.
    
    Bug: T400642
    Change-Id: I4de00f60a34ba37f759e5075feb45200cc5b8fa5

--- maintenance/resetGlobalUserTokens.php
+++ maintenance/resetGlobalUserTokens.php
@@ -3,20 +3,7 @@
  * Reset the user_token for all users on the wiki. Useful if you believe
  * that your user table was acidentally leaked to an external source.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
+ * @license GPL-2.0-or-later
  *
  * This is mostly taken from core's maintenance/resetUserTokens.php
  *
