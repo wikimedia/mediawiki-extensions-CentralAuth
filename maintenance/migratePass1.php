9c2f1d60d91bfbe21a4d4f88d7fa0ea4a60e8e11
Sam Reed <sam@reedyboy.net>
2026-01-29 00:38:51 +0000

    migratePass1.php: Minor updates and improvements
    * Define member variables inline
    * Define start time in execute()
    * Display the number of rows and progress % in migratePassOneReport
    
    Change-Id: Ie4b55181118540d9de16250d57d2378e822b20e3

--- maintenance/migratePass1.php
+++ maintenance/migratePass1.php
@@ -21,26 +21,20 @@ use Wikimedia\Rdbms\IDBAccessObject;
 
 class MigratePass1 extends Maintenance {
 
-	/** @var float */
-	protected $start;
+	protected float $start;
 
-	/** @var int */
-	protected $migrated;
+	protected int $migrated = 0;
 
-	/** @var int */
-	protected $total;
+	protected int $total = 0;
 
-	/** @var string */
-	protected $fromPrefix;
+	protected string $fromPrefix = '';
+
+	protected int $totalRowCount;
 
 	public function __construct() {
 		parent::__construct();
 		$this->requireExtension( 'CentralAuth' );
 		$this->addDescription( 'Migrates local users to global users where possible' );
-		$this->start = microtime( true );
-		$this->migrated = 0;
-		$this->total = 0;
-		$this->fromPrefix = '';
 	}
 
 	public function execute() {
@@ -49,12 +43,14 @@ class MigratePass1 extends Maintenance {
 
 		$dbBackground = CentralAuthServices::getDatabaseManager()->getCentralReplicaDB();
 
+		$this->start = microtime( true );
 		$result = $dbBackground->newSelectQueryBuilder()
 			->select( 'gn_name' )
 			->from( 'globalnames' )
 			->caller( __METHOD__ )
 			->fetchResultSet();
 
+		$this->totalRowCount = $result->numRows();
 		foreach ( $result as $row ) {
 			$this->fromPrefix = $row->gn_name;
 			$central = new CentralAuthUser( $row->gn_name, IDBAccessObject::READ_LATEST );
@@ -72,9 +68,11 @@ class MigratePass1 extends Maintenance {
 	private function migratePassOneReport() {
 		$delta = microtime( true ) - $this->start;
 		$this->output( sprintf(
-			"%s processed %d usernames (%.1f/sec), %d (%.1f%%) fully migrated (@ %s)\n",
+			"%s: processed %d of %d usernames (%.1f%%; %.1f/sec) , %d (%.1f%%) processed fully migrated (@ %s)\n",
 			wfTimestamp( TS_DB ),
 			$this->total,
+			$this->totalRowCount,
+			$this->total / $this->totalRowCount * 100.0,
 			$this->total / $delta,
 			$this->migrated,
 			$this->migrated / $this->total * 100.0,
