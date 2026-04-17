37ded2542d09f2f7db943c77c2e8729c065b0171
Bartosz Dziewoński <dziewonski@fastmail.fm>
2026-04-17 19:59:00 +0200

    Use sha256 instead of md5 to generate user cache key
    This prevents existing cache entries from being used, but that's fine,
    we have invalidated them a couple times in the past (most recently
    c3fdadafb91d323f5bbb7aa61c9cccd827e0393d) and they expire in 24 hours.
    
    It also increases the length of the key (by 32 bytes), which is also
    fine, since the length of the key is not limited (if it would exceed
    internal limits, the caching code will hash the whole thing again).
    We use comparably long keys in other code.
    
    Bug: T419336
    Change-Id: I87e5a0f7a27e3e1a0cffc113d4783d232910f8fe
    (cherry picked from commit b6705a5d33b674d2d5100c8dbdaa8e124f508ca9)

--- includes/User/CentralAuthUser.php
+++ includes/User/CentralAuthUser.php
@@ -721,7 +721,7 @@ class CentralAuthUser implements IDBAccessObject {
 	 * @return string
 	 */
 	protected function getCacheKey( WANObjectCache $cache ) {
-		return $cache->makeGlobalKey( 'centralauth-user', md5( $this->mName ) );
+		return $cache->makeGlobalKey( 'centralauth-user', hash( 'sha256', $this->mName ) );
 	}
 
 	/**
